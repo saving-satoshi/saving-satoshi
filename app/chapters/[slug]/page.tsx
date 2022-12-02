7727ffe7ed39f4f58075be2592fb4a7fa23292cf
Aurèle Oulès <aurele@oules.com>
2022-12-02 11:30:53 +0100

    Responsive chapter page

--- app/chapters/[slug]/page.tsx
+++ app/chapters/[slug]/page.tsx
@@ -21,33 +21,33 @@ export default async function Page({ params }) {
     const intro = await getIntro(params.slug)
     return (
         <div className="w-screen">
-        <div className='grid grid-cols-1 md:grid-cols-2 justify-center'>
-            <div className='flex justify-start w-full text-white '>
-                <div className='content-center justify-items-start sm:p-10 px-1 py-6 sm:py-36'>
-                    <div
-                        dangerouslySetInnerHTML={{ __html: intro.body.html }}
-                        className='pt-8 text-xl'
-                    ></div>
-                    <div className='pt-8'>
-                        <Link
-                            href='/chapters/chapter-1/genesis'
-                            className='py-2.5 px-12 w-full md:w-auto justify-center bg-white text-base-blue text-2xl'>
-                            Start
-                        </Link>
+            <div className='grid grid-cols-1 md:grid-cols-2 justify-center px-6'>
+                <div className='flex justify-start w-full text-white '>
+                    <div className='content-center justify-items-start sm:p-10 px-1 py-6 sm:py-36'>
+                        <div
+                            dangerouslySetInnerHTML={{ __html: intro.body.html }}
+                            className='pt-8 text-xl'
+                        ></div>
+                        <div className='pt-8'>
+                            <Link
+                                href='/chapters/chapter-1/genesis'
+                                className='py-2.5 px-12 w-full md:w-auto justify-center bg-white text-base-blue text-2xl'>
+                                Start
+                            </Link>
+                        </div>
                     </div>
                 </div>
+                <div className='flex justify-center'>
+                    <Image
+                        src={intro.image}
+                        alt={intro.title}
+                        width={1440}
+                        height={715}
+                        layout="responsive"
+                        objectFit="cover"
+                    />
+                </div>
             </div>
-            <div className='flex justify-center'>
-                <Image
-                    src={intro.image}
-                    alt={intro.title}
-                    width={1440}
-                    height={715}
-                    layout="responsive"
-                    objectFit="cover"
-                />
-            </div>
-        </div>
         </div>
     )
 
