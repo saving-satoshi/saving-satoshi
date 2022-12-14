c95225b40328c4fd9807b4b962de97627df2a15c
GBKS <chri@sto.ph>
2022-12-14 10:43:24 +0100

    Made challenge links on the chapters page clickable
    This is part of issue #10. The Challenges component previously only received and showed a list of lesson IDs. With these changes it finds the respective challenge data to show the title and a link to the respective challenge content.
    
    Note that the links don't work right now. Contentlayer is not producing the correct links we need. This can be handled in a separate PR. Maybe a good task for Juan.

--- components/partials/Challenges.tsx
+++ components/partials/Challenges.tsx
@@ -1,13 +1,17 @@
+
+import { allLessons } from 'contentlayer/generated'
 import Link from "next/link"
 import { ListItem } from "./ListItem"
 
 export const ChallengeList = ({challenges}) => {
+    const challengesData = challenges ? allLessons.filter((challenge: Lesson) => challenges.indexOf(challenge.slugAsParams) !== -1) : null
+
     return (
         <div className='flex grow justify-stretch items-start mt-6 font-nunito text-white w-full'>
-            {challenges ? 
+            {challengesData ? 
                 <ul className='grid w-full items-start'>
-                    {challenges.map((ch, idx) => (
-                        <ListItem key={idx+1} pos={idx+1} title={ch}/>
+                    {challengesData.map((challenge, index) => (
+                        <ListItem key={index+1} position={index+1} title={challenge.title} slug={challenge.slug} />
                     ))}
                 </ul> :
                 <p>Coming soon. Hang tight.</p>
--- components/partials/ListItem.tsx
+++ components/partials/ListItem.tsx
@@ -1,12 +1,14 @@
 import clsx from "clsx";
+import Link from "next/link"
 
-export const ListItem = ({pos, title}) => {
+export const ListItem = ({position, title, slug}) => {
     return (
-        <li className={clsx('flex justify-left w-full px-[15px] py-[11px] text-xl font-cbrush hover:bg-black/20 transition ease-in-out duration-150', {
-            'border-t border-white/25': pos!==1
-        })}
-    >
-            <span className="opacity-50 pr-1">{pos+ '. '}</span>{title}
-        </li>
+        <Link className={clsx('flex justify-left w-full px-[15px] py-[11px] text-xl font-cbrush hover:bg-black/20 transition ease-in-out duration-150', {
+                'border-t border-white/25': position!==1
+            })}
+            href={slug}
+        >
+            <span className="opacity-50 pr-1">{position+ '. '}</span>{title}
+        </Link>
     );
 }
