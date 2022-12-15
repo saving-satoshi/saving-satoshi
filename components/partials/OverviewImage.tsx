bb4838758c7671f8bf1615b9ec4410f1331987d9
Gwilin <js@kumuly.dev>
2022-12-15 10:05:23 -0500

    chore: refactor chapter overview

--- components/partials/OverviewImage.tsx
+++ components/partials/OverviewImage.tsx
@@ -1,15 +1,17 @@
-import { Chapter } from "contentlayer/generated"
-import Image from "next/image"
+import { Chapter } from 'contentlayer/generated'
+import Image from 'next/image'
 
 export const OverviewImage = (chapter: Chapter) => {
-    return(
-        <Image
-            src={chapter.image}
-            alt={chapter.title}
-            width={1440}
-            height={715}
-            layout="responsive"
-            objectFit="contain"
-        />
-    )
+  return (
+    <Image
+      src={chapter.image}
+      alt={chapter.title}
+      width={1440}
+      height={715}
+      style={{
+        maxWidth: '100%',
+        height: 'auto',
+      }}
+    />
+  )
 }
