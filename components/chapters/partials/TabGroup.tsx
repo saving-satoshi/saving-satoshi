d6d1139ded3c5d88680e405df3d1ef47ec8ff441
GBKS <chri@sto.ph>
2022-12-21 17:11:11 +0100

    Fix to add Item type back to TabGroup

--- components/chapters/partials/TabGroup.tsx
+++ components/chapters/partials/TabGroup.tsx
@@ -1,6 +1,11 @@
 import { Tab } from './Tab'
 import { allLessons, Lesson } from 'contentlayer/generated'
 
+export type Item = {
+  slug: string
+  id: number
+}
+
 export const TabGroup = ({ path, items }: { path: string; items: Item[] }) => {
   const itemSlugs = items.map(item => item.slug)
   const challengesData = items ? allLessons.filter((challenge: Lesson) => itemSlugs.indexOf(challenge.slugAsParams) !== -1) : null
