057962df3eb2a55024eecc7899dbefdd5742ca64
vinayaksh42 <vinayaksh42@gmail.com>
2023-01-18 02:15:32 +0530

    Keeping chapter height consistent in medium-large screen when tabbing

--- components/partials/OverviewInfo.tsx
+++ components/partials/OverviewInfo.tsx
@@ -3,11 +3,14 @@
 import { Chapter } from '.contentlayer/generated/types'
 import { Overview } from './Overview'
 import { Tabs } from 'components/ui/Tabs'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ChallengeList } from './ChallengeList'
+import { useMediaQuery } from 'react-responsive'
 
 export const OverviewInfo = (chapter: Chapter) => {
   const [activeTab, setActiveTab] = useState('info')
+  const [hydrated, setHydrated] = useState(false);
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 1023px)' })
   const tabData = [
     {
       id: 'info',
@@ -19,7 +22,11 @@ export const OverviewInfo = (chapter: Chapter) => {
     }
   ]
 
-  return (
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  return (hydrated && (
     <div className="w-full ml-3.5 mr-3.5 content-center justify-items-start px-1">
       <h2 className="text-left mt-6 font-nunito text-xl font-bold text-white text-opacity-75 md:text-3xl">
         Chapter {chapter.position}
@@ -33,14 +40,29 @@ export const OverviewInfo = (chapter: Chapter) => {
         onChange={setActiveTab}
         classes="mt-8"
       />
-      <div className="flex grow">
-        {activeTab == 'info' &&
-          <Overview chapter={chapter}></Overview>
-        }
-        {activeTab == 'challenges' && (
-          <ChallengeList challenges={chapter.lessons} path={chapter.slug} ></ChallengeList>
+        {isSmallScreen ? (
+          <div className="flex grow">
+            {activeTab == 'info' &&
+              <Overview chapter={chapter}></Overview>
+            }
+            {activeTab == 'challenges' && (
+              <ChallengeList challenges={chapter.lessons} path={chapter.slug} ></ChallengeList>
+            )}
+          </div>
+        ) : (
+          <div className='flex'>
+            <div 
+              aria-hidden={(activeTab != 'info' ? 'true' : 'false')} 
+              className={'tab-content ' + (activeTab != 'info' ? '' : 'tab-content-active')}>
+              <Overview chapter={chapter}></Overview>
+            </div>
+            <div 
+              aria-hidden={(activeTab != 'challenges' ? 'true' : 'false')} 
+              className={'tab-content ' + (activeTab != 'challenges' ? '' : 'tab-content-active')}>
+              <ChallengeList challenges={chapter.lessons} path={chapter.slug} ></ChallengeList>
+            </div>
+          </div>
         )}
-      </div>
     </div>
-  )
+  ))
 }
