5815304a7ee4a7a7decc1b8eff96b9b1e8270103
Ben Allen <benalleng@gmail.com>
2023-02-01 09:38:32 -0500

    prevent spacebar in textarea

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -3,71 +3,66 @@
 import { useState } from 'react';
 
 interface UserInputProps {
-    inputAmount: number;
     value: string;
     userInput: (value: string) => void;
     answer: string;
     hints?: boolean;
 }
 
-export const UserInput: React.FC<UserInputProps> = ({ inputAmount, value, userInput, answer, hints }) => {
+export const UserInput: React.FC<UserInputProps> = ({ value, userInput, answer, hints }) => {
     const [textAreaValue, setTextAreaValue] = useState(value);
-    const [matching, setMatching] = useState(false);
+    const [correctAns, setCorrectAns] = useState(false);
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+      if (event.keyCode === 32) {
+        event.preventDefault();
+      }
+    };
 
     const blankSpace = () => {
-      let underscores = '_'.repeat(inputAmount);
-      underscores = underscores.substring(0, inputAmount - value.length);
+      let underscores = '_'.repeat(answer.length);
+      underscores = underscores.substring(0, answer.length - value.length);
       return (
         <>
-          {Array.from({length: inputAmount}, (_, i) => {
-            if(i >= inputAmount - underscores.length) return <span className='underscore' key={i}>_</span>
+          {Array.from({length: answer.length}, (_, i) => {
+            if(i >= answer.length - underscores.length) return <span className='underscore' key={i}>_</span>
             if(textAreaValue[i] === answer[i]) {
               return <span className='overlay correct' key={i}>{textAreaValue[i]}</span>
-            } else if(textAreaValue[i] !== answer[i]) {
+            } else if((textAreaValue[i] !== answer[i]) && !!hints) {
               return <span className='overlay incorrect' key={i}>{textAreaValue[i]}</span>
-            } else if(textAreaValue[i] === answer[i] && textAreaValue.length === answer.length) {
-              return <span className='overlay correct answer' key={i}>{textAreaValue[i]}</span>
+            } else {
+              return <span className='overlay' key={i}>{textAreaValue[i]}</span>
             }
           })}
         </>
       );
     }
 
-    const hintColorer = () => {
-      if((hints === true && ((value !== answer) && (value.length === inputAmount)))) {
-        // TODO add hints
-        console.log(answer);
-      } else {
-        return
-      }
-    }
-
-    hintColorer();
-
     return (
         <form className='relative'>
             <textarea 
+              onKeyDown={handleKeyDown}
                 onChange={(e) => {setTextAreaValue(e.target.value)
                     userInput(e.target.value);
                     // setMatching(e.target.value.split('').every((val, i) => (val === answer[i]) && (value.length === inputAmount - 1)));
-                    if(((e.target.value === answer) && (e.target.value.length === inputAmount))) {
-                      setMatching(true)
+                    if(((e.target.value === answer) && (e.target.value.length === answer.length))) {
+                      setCorrectAns(true)
                     } else {
-                      setMatching(false)
+                      setCorrectAns(false)
                     }
                 }}
                 value={textAreaValue}  
-                maxLength={inputAmount}
+                maxLength={answer.length}
                 spellCheck='false'
                 className={
-                  `${matching ? 'text-green/100' : 'text-transparent'} w-full h-full overflow-hidden text-[18px] md:text-[30px] tracking-[5px] bg-transparent font-space-mono absolute top-0 left-0 outline-none resize-none leading-[180%]`
+                  `text-transparent w-full h-full overflow-hidden text-[18px] md:text-[30px] tracking-[5px] bg-transparent font-space-mono absolute top-0 left-0 outline-none resize-none leading-[180%]`
                 }
                 style={{
-                  caretColor: 'white'
+                  caretColor: '#6e7d92'
                 }}
                 />
               <p
-              className='w-full h-full text-inherit text-[18px] md:text-[30px] tracking-[5px] pointer-events-none font-space-mono leading-[180%]'
+              className={`${correctAns ? 'overlay-complete' : 'overlay-incomplete'} w-full h-full text-inherit text-[18px] md:text-[30px] tracking-[5px] pointer-events-none font-space-mono leading-[180%]`}
               style={{
                 lineBreak: 'anywhere',
               }}>
