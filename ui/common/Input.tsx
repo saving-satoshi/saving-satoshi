af1fc666407310038fd39246ead98e430e082425
Ben Allen <benalleng@gmail.com>
2023-02-01 10:25:28 -0500

    move userInput

--- ui/common/Input.tsx
+++ ui/common/Input.tsx
@@ -1,39 +1,72 @@
 'use client'
 
-import RICIBs from 'react-individual-character-input-boxes'
+import { useState } from 'react';
 
-export default function Input({
-  amount,
-  onChange,
-}: {
-  amount: number
-  onChange: Function
-}) {
-  const handleChange = (value: string) => {
-    onChange(value)
-  }
+interface UserInputProps {
+    onChange: (value: string) => void;
+    answer: string;
+    hints?: boolean;
+}
 
-  return (
-    <RICIBs
-      amount={amount}
-      autoFocus
-      handleOutputString={handleChange}
-      inputProps={{
-        className: 'bg-transparent',
-        placeholder: '_',
-        style: {
-          fontSize: '20px',
-          width: '20px',
-          height: '20px',
-          margin: '0px',
-          borderRadius: '0px',
-          textAlign: 'center',
-          justifyContent: 'space-evenly',
-          outline: 'none',
-          fontFamily: 'var(--space-mono-font)',
-        },
-      }}
-      inputRegExp={/^[a-zA-Z0-9_.-]*$/}
-    />
-  )
+export default function Input({ onChange, answer, hints }) {
+    const [textAreaValue, setTextAreaValue] = useState('');
+    const [correctAns, setCorrectAns] = useState(false);
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+      if (event.key === ' ') {
+        event.preventDefault();
+      }
+    };
+
+    const blankSpace = () => {
+      let underscores = '_'.repeat(answer.length);
+      underscores = underscores.substring(0, answer.length - textAreaValue.length);
+      return (
+        <>
+          {Array.from({length: answer.length}, (_, i) => {
+            if(i >= answer.length - underscores.length) return <span className='underscore' key={i}>_</span>
+            if(textAreaValue[i] === answer[i]) {
+              return <span className='overlay correct' key={i}>{textAreaValue[i]}</span>
+            } else if((textAreaValue[i] !== answer[i]) && !!hints) {
+              return <span className='overlay incorrect' key={i}>{textAreaValue[i]}</span>
+            } else {
+              return <span className='overlay' key={i}>{textAreaValue[i]}</span>
+            }
+          })}
+        </>
+      );
+    }
+
+    return (
+        <form className='relative'>
+            <textarea 
+              onKeyDown={handleKeyDown}
+                onChange={(e) => {setTextAreaValue(e.target.value)
+                    onChange(e.target.value);
+                    // setMatching(e.target.value.split('').every((val, i) => (val === answer[i]) && (value.length === inputAmount - 1)));
+                    if(((e.target.value === answer) && (e.target.value.length === answer.length))) {
+                      setCorrectAns(true)
+                    } else {
+                      setCorrectAns(false)
+                    }
+                }}
+                value={textAreaValue}  
+                maxLength={answer.length}
+                spellCheck='false'
+                className={
+                  `text-transparent w-full h-full overflow-hidden text-[18px] md:text-[30px] tracking-[5px] bg-transparent font-space-mono absolute top-0 left-0 outline-none resize-none leading-[180%]`
+                }
+                style={{
+                  caretColor: '#6e7d92'
+                }}
+                />
+              <p
+              className={`${correctAns ? 'overlay-complete' : 'overlay-incomplete'} w-full h-full text-inherit text-[18px] md:text-[30px] tracking-[5px] pointer-events-none font-space-mono leading-[180%]`}
+              style={{
+                lineBreak: 'anywhere',
+              }}>
+                {blankSpace()}
+              </p>
+            </form>
+    )
 }
