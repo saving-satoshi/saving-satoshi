fd29b7222b4c92fb41eac2b93220b87d5d6dd11b
Ben Allen <benalleng@gmail.com>
2023-02-02 17:37:04 -0500

    review changes

--- ui/common/Input.tsx
+++ ui/common/Input.tsx
@@ -10,27 +10,20 @@ interface UserInputProps {
 
 export default function Input({ onChange, answer, hints }: UserInputProps) {
   const [textAreaValue, setTextAreaValue] = useState('')
-  const [correctAns, setCorrectAns] = useState(false)
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === ' ') {
-      event.preventDefault()
-    }
-  }
+  const [correctAnswer, setCorrectAnswer] = useState(false)
 
   const blankSpace = () => {
-    let underscores = '_'.repeat(answer.length)
-    underscores = underscores.substring(0, answer.length - textAreaValue.length)
+    const underscores = '_'.repeat(Math.max(0, answer.length - textAreaValue.length))
     return (
       <>
         {Array.from({ length: answer.length }, (_, i) => {
-          if (i >= answer.length - underscores.length)
+          {if (i >= answer.length - underscores.length)
             return (
               <span className="underscore" key={i}>
                 _
               </span>
-            )
-          if (textAreaValue[i] === answer[i]) {
+            )}
+          {if (textAreaValue[i] === answer[i]) {
             return (
               <span className="overlay correct" key={i}>
                 {textAreaValue[i]}
@@ -48,29 +41,42 @@ export default function Input({ onChange, answer, hints }: UserInputProps) {
                 {textAreaValue[i]}
               </span>
             )
-          }
+          }}
         })}
       </>
     )
+  }
+
+  const formatOnPaste = (clipboard) => {
+    const formattedText = clipboard.replace(/[\s\u00A0]+/g, '').replace(/\r?\n|\r/g, '').slice(0, answer.length).toLowerCase()
+    return formattedText
+  }
+  
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === ' ') {
+      event.preventDefault()
+    }
+  }
+
+  const handleChange = (e) => {
+    let formattedText = formatOnPaste(e.target.value)
+    setTextAreaValue(formattedText)
+    onChange(formattedText)
+    if (
+      formattedText === answer &&
+      formattedText.length === answer.length
+    ) {
+      setCorrectAnswer(true)
+    } else {
+      setCorrectAnswer(false)
+    }
   }
 
   return (
     <form className="relative">
       <textarea
         onKeyDown={handleKeyDown}
-        onChange={(e) => {
-          let noSpace = e.target.value.replace(/[\s\u00A0]+/g, '').replace(/\r?\n|\r/g, '').slice(0, answer.length).toLowerCase()
-          setTextAreaValue(noSpace)
-          onChange(noSpace)
-          if (
-            noSpace === answer &&
-            noSpace.length === answer.length
-          ) {
-            setCorrectAns(true)
-          } else {
-            setCorrectAns(false)
-          }
-        }}
+        onChange={handleChange}
         value={textAreaValue}
         spellCheck="false"
         className={`absolute top-0 left-0 h-full w-full resize-none overflow-hidden bg-transparent font-space-mono text-[18px] leading-[180%] tracking-[5px] text-transparent outline-none md:text-[30px]`}
@@ -80,7 +86,7 @@ export default function Input({ onChange, answer, hints }: UserInputProps) {
       />
       <p
         className={`${
-          correctAns ? 'overlay-complete' : 'overlay-incomplete'
+          correctAnswer ? 'overlay-complete' : 'overlay-incomplete'
         } pointer-events-none h-full w-full font-space-mono text-[18px] leading-[180%] tracking-[5px] text-inherit md:text-[30px]`}
         style={{
           lineBreak: 'anywhere',
