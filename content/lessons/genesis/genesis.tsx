e80a6a7b66eba9132a290da6859b70e859ca3632
Ben Allen <benalleng@gmail.com>
2023-02-02 08:12:45 -0500

    expand max width

--- content/lessons/genesis/genesis.tsx
+++ content/lessons/genesis/genesis.tsx
@@ -73,7 +73,7 @@ export default function Genesis1() {
       <hr className="border-1 invisible h-1 w-full border-white/25 md:visible" />
 
       <LessonPrompt
-        className="max-w-[840px] px-4 py-8 md:items-center"
+        className="max-w-[1280px] px-4 py-8 md:items-center"
         label="Paste the ScriptSig HEX Representation"
         answer={answer}
         onChange={setUserInput}
