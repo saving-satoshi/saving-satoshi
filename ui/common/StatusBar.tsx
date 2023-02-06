74ec098b2f33dc678c2a67ae5478d430f453090f
Ben Allen <benalleng@gmail.com>
2023-02-06 09:32:21 -0500

    adjust classname order

--- ui/common/StatusBar.tsx
+++ ui/common/StatusBar.tsx
@@ -59,7 +59,7 @@ export default function StatusBar({
 
   return (
     <div
-      className={clsx('max-md:bottom-0 max-md:fixed border-t border-white/25 max-md:px-4 max-md:py-8', {
+      className={clsx('border-t border-white/25 max-md:px-4 max-md:py-8 max-md:bottom-0 max-md:fixed', {
         'w-screen': full,
         'w-full': !full,
         'bg-success/25 statusbar-bottom': getStatus() === Status.Success,
