663d8484aba07d8a6fef323cbf7b508720cff0b3
Ben Allen <benalleng@gmail.com>
2023-02-06 12:40:51 -0500

    center lesson intro content

--- content/lessons/transacting/transacting.tsx
+++ content/lessons/transacting/transacting.tsx
@@ -4,7 +4,7 @@ import { Button, Text, Title } from 'ui'
 
 export default function Transacting() {
   return (
-    <div className="grid w-full grid-cols-1 justify-center justify-items-center">
+    <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
       <div className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0">
         <div className="content-center justify-items-center px-1 font-nunito">
           <div className="genesis">
