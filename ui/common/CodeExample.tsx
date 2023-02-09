556c0aae9e5d2e880b637e887798d4ddb9a38193
Ben Allen <benalleng@gmail.com>
2023-02-09 15:52:20 -0500

    remove copy as string

--- ui/common/CodeExample.tsx
+++ ui/common/CodeExample.tsx
@@ -1,22 +1,13 @@
 import { CopyButton } from 'ui'
 
-export default function CodeExample({ code, language, copy }: {code: string; language: string; copy?: boolean | string}) {
-  const copyElement = () => {
-    if(copy === true) {
-      return <CopyButton compact content={code} />
-    } else if(typeof(copy) === 'string') {
-      return <CopyButton compact content={copy} />
-    } else {
-      return null
-    }
-  }
+export default function CodeExample({ code, language, copy }: {code: string; language: string; copy?: boolean}) {
   return (
     <pre className="mt-2 border-2 border-dashed border-white p-2">
       <span
         className={`language-${language} flex break-all whitespace-pre-line items-center justify-between pl-2 pr-0`}
       >
         {code}
-        {copyElement()}
+        {copy ? <CopyButton compact content={code} /> : null}
       </span>
     </pre>
   )
