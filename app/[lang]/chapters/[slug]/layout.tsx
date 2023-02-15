135b028e387c116cdb9f044c5f1661fe0543700f
shaavan <shaavan.github@gmail.com>
2023-02-15 20:20:07 +0530

    Add the Menu.tsx file responsible for display dropdown menu's background
    - Also introduce new custom themes value in tailwind.config.js and use it to turn Menu's
      background opaque
    - Removed the blur class in global.css and replaced it with using the
      newly defined tailwind.config.js values. This is done so that the code
      will need less changes when used in next chapters which have different
      color theme.

--- app/[lang]/chapters/[slug]/layout.tsx
+++ app/[lang]/chapters/[slug]/layout.tsx
@@ -6,7 +6,7 @@ export default async function Layout({ children, params }) {
 
   return (
     <div className="ch1-background flex flex-col">
-      <div className="fix-grow-issue flex min-h-screen flex-col blur">
+      <div className="fix-grow-issue flex min-h-screen flex-col bg-back/75 drop-shadow-3xl backdrop-blur-4xl">
         {isValidChapter && <Navbar params={params} />}
         {children}
       </div>
