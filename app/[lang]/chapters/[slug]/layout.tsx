e6238ce42b2a1fd16aaadef88448dd59cff0a4e6
shaavan <shaavan.github@gmail.com>
2023-02-15 20:34:14 +0530

    Modifies the layout.tsx to display the NavbarMobile when the window size becomes smaller than desktop size

--- app/[lang]/chapters/[slug]/layout.tsx
+++ app/[lang]/chapters/[slug]/layout.tsx
@@ -1,13 +1,18 @@
-import Navbar from 'components/Navbar'
+import Navbar, { NavbarMobile } from 'components/Navbar'
 import { chapters } from 'content'
 
-export default async function Layout({ children, params }) {
+export default function Layout({ children, params }) {
   const isValidChapter = chapters[params.slug]
 
   return (
     <div className="ch1-background flex flex-col">
       <div className="fix-grow-issue flex min-h-screen flex-col bg-back/75 drop-shadow-3xl backdrop-blur-4xl">
-        {isValidChapter && <Navbar params={params} />}
+        <div className="md:hidden">
+          <NavbarMobile params={params} />
+        </div>
+        <div className="hidden md:block">
+          <Navbar params={params} />
+        </div>
         {children}
       </div>
     </div>
