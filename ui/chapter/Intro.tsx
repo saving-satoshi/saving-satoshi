72a17b7299f8e6ad27e7f767481a0bec8e827c8f
Ben Allen <benalleng@gmail.com>
2023-02-15 23:21:22 -0500

    re-center paragprahs

--- ui/chapter/Intro.tsx
+++ ui/chapter/Intro.tsx
@@ -10,11 +10,15 @@ export default function Intro({
   next: string
 }) {
   const textRender = texts.map((paragraph) => {
-    return <Text className={paragraph.class}>{paragraph.text}</Text>
+    return (
+      <Text key={paragraph.text} className={paragraph.class}>
+        {paragraph.text}
+      </Text>
+    )
   })
 
   return (
-    <div className="grid w-full grid-cols-1 justify-center justify-items-center">
+    <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
       <div className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0">
         <div className="content-center justify-items-center px-1 font-nunito">
           <div className="genesis">
@@ -22,7 +26,7 @@ export default function Intro({
             {textRender}
           </div>
 
-          <Button href={next} classes="mt-8">
+          <Button href={next} classes="mt-8 max-md:w-full">
             Continue
           </Button>
         </div>
