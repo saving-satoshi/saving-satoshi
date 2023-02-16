91b42399f88b83cf89bb4cc78844efdc0becc635
Ben Allen <benalleng@gmail.com>
2023-02-16 14:33:27 -0500

    create translations template for transacting

--- content/lessons/transacting/transacting-3/translations.ts
+++ content/lessons/transacting/transacting-3/translations.ts
@@ -1,11 +1,40 @@
+/**
+ *  TODO TRANSLATIONS
+ *  @nl
+ */
+
 export const translations = {
   transacting_three: {
     en: {
       title: 'Transacting',
+      heading: 'Another secret message',
+      paragraph_one:
+        'Then, look for the hex representation of the ScriptPubKey field.',
+      paragraph_two:
+        'Once you’ve found it, decode the hidden message, just like we did in the previous exercise.',
+      paragraph_three: 'And remember, ’Bitcoin is for everyone’.',
+      terminalChallengeSuccess:
+        'That’s correct! Nice work. We don’t know why tacos were so important to the anonymous sender of this transaction, but here we are. And it’s an admirable goal at least.',
+      terminalChallengeLines:
+        'Enter your commands here and press Enter...\n\n Note that $scriptPubKeyHex is not defined for you this time. You’ll need to replace this variable in the code with the value you found in the previous challenge',
+      terminalChallengeErrMessage:
+        'Almost. Take another look to find the correct value',
     },
 
     nl: {
       title: 'Transacting',
+      heading: 'Another secret message',
+      paragraph_one:
+        'Then, look for the hex representation of the ScriptPubKey field.',
+      paragraph_two:
+        'Once you’ve found it, decode the hidden message, just like we did in the previous exercise.',
+      paragraph_three: 'And remember, ’Bitcoin is for everyone’.',
+      terminalChallengeSuccess:
+        'That’s correct! Nice work. We don’t know why tacos were so important to the anonymous sender of this transaction, but here we are. And it’s an admirable goal at least.',
+      terminalChallengeLines:
+        'Enter your commands here and press Enter...\n\n Note that $scriptPubKeyHex is not defined for you this time. You’ll need to replace this variable in the code with the value you found in the previous challenge',
+      terminalChallengeErrMessage:
+        'Almost. Take another look to find the correct value',
     },
   },
 }
