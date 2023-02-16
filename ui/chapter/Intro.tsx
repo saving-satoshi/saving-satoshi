b206c505953533d7fcd6c458388d9b48e86a3fd5
Ben Allen <benalleng@gmail.com>
2023-02-16 15:16:00 -0500

    accept title as prop

--- ui/chapter/Intro.tsx
+++ ui/chapter/Intro.tsx
@@ -2,10 +2,12 @@ import { Button, Text, Title } from 'ui'
 
 export default function Intro({
   title,
+  heading,
   children,
   next,
 }: {
   title: string
+  heading: string
   children: any
   next: string
 }) {
@@ -13,8 +15,8 @@ export default function Intro({
     <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
       <div className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0">
         <div className="content-center justify-items-center px-1 font-nunito">
-          <div className="genesis">
-            <Title>{title}</Title>
+          <div className={title}>
+            <Title>{heading}</Title>
             {children}
           </div>
 
