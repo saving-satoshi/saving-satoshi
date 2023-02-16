25d19ce27928bc8e2ea8d73621bcb2cec5774422
Ben Allen <benalleng@gmail.com>
2023-02-16 14:31:58 -0500

    accept children as prop

--- ui/chapter/Intro.tsx
+++ ui/chapter/Intro.tsx
@@ -2,28 +2,20 @@ import { Button, Text, Title } from 'ui'
 
 export default function Intro({
   title,
-  texts,
+  children,
   next,
 }: {
   title: string
-  texts: { class: string; text: string }[]
+  children: any
   next: string
 }) {
-  const textRender = texts.map((paragraph) => {
-    return (
-      <Text key={paragraph.text} className={paragraph.class}>
-        {paragraph.text}
-      </Text>
-    )
-  })
-
   return (
     <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
       <div className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0">
         <div className="content-center justify-items-center px-1 font-nunito">
           <div className="genesis">
             <Title>{title}</Title>
-            {textRender}
+            {children}
           </div>
 
           <Button href={next} classes="mt-8 max-md:w-full">
