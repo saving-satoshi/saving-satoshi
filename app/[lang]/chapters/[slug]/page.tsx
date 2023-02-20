fbdd4f85ad0c21b34f664e1d07a62d4e3293665f
shaavan <shaavan.github@gmail.com>
2023-02-20 19:46:00 +0530

    fixes Coming Soon page to fit the viewport

--- app/[lang]/chapters/[slug]/page.tsx
+++ app/[lang]/chapters/[slug]/page.tsx
@@ -3,14 +3,13 @@ import { introductions } from 'content'
 
 function ComingSoon() {
   return (
-    <div className="mb-12 flex h-screen items-center justify-center">
-      <h1 className="text-center text-6xl text-white sm:text-7xl lg:text-8xl">
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="items-center text-center text-6xl text-white sm:text-7xl lg:text-8xl">
         Coming Soon...
       </h1>
     </div>
   )
 }
-
 export default async function Page({ params }) {
   const chapter = chapters[params.slug]
   const Intro = introductions[chapter?.metadata?.intro]
