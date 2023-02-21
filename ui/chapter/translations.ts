45b62b249c1b220b586ecbe41bfc18b06194007a
Ben Allen <benalleng@gmail.com>
2023-02-21 16:41:15 -0500

    add translations for chapter ui

---
+++ ui/chapter/translations.ts
@@ -0,0 +1,18 @@
+export const translations = {
+  intro: {
+    en: {
+      next: 'Continue',
+    },
+  },
+  end: {
+    en: {
+      save: 'Save my progress',
+      next: 'Continue without saving',
+    },
+  },
+  challenge_list: {
+    en: {
+      coming_soon: 'Coming soon. Hang tight.',
+    },
+  },
+}
