8bda0b649d7d7d32a3e962c65d5028f6d14d7ae4
Ben Allen <benalleng@gmail.com>
2023-02-21 16:43:10 -0500

    remove incorrect norwegian translations

--- content/lessons/transacting/transacting-1/translations.ts
+++ content/lessons/transacting/transacting-1/translations.ts
@@ -15,16 +15,5 @@ export const translations = {
       paragraph_three:
         'For the following transaction, identify the output of type OP_RETURN.',
     },
-
-    nl: {
-      title: 'Transacting',
-      heading: 'What’s in a transaction?',
-      paragraph_one:
-        'Bitcoin is censorship resistant money. Anybody can send money by broadcasting a transaction to the network. After broadcast, transactions are packaged up into blocks by miners. Miners compete against other miners for the privilege of building on the chain.',
-      paragraph_two:
-        'Transactions have two parts: inputs and outputs. In the previous exercise we decoded a secret message found inside a transaction input. This time we’ll decode a message that belongs to the output part.',
-      paragraph_three:
-        'For the following transaction, identify the output of type OP_RETURN.',
-    },
   },
 }
--- content/lessons/transacting/transacting-2/translations.ts
+++ content/lessons/transacting/transacting-2/translations.ts
@@ -13,14 +13,5 @@ export const translations = {
       link: 'View transaction',
       input_challenge_label: 'Enter the OP_RETURN type',
     },
-
-    nl: {
-      title: 'Transacting',
-      heading: 'OP_RETURN',
-      paragraph_one:
-        'For the following transaction, identify the output of type OP_RETURN.',
-      link: 'View transaction',
-      input_challenge_label: 'Enter the OP_RETURN type',
-    },
   },
 }
