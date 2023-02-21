396717d437eca4d475ccc68dd600e5c0d21e5bb9
Ben Allen <benalleng@gmail.com>
2023-02-21 16:41:50 -0500

    rename index.tsx

---
+++ ui/chapter/index.tsx
@@ -0,0 +1,11 @@
+export { default as Chapter } from './Chapter'
+export { default as ChapterChallengeList } from './ChallengeList'
+export { default as ChapterChallengeListItem } from './ChallengeListItem'
+export { default as ChapterEnd } from './End'
+export { default as ChapterTabs } from './Tabs'
+export { default as ChapterTab } from './Tab'
+export { default as Intro } from './Intro'
+
+import { translations as ChapterUITranslations } from './translations'
+
+export const translations = [ChapterUITranslations]
