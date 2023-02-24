1d9cd69da237afaf829f1f4abddc299ea1414a33
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-02-24 19:44:49 +0100

    fix: improve success message for genesis terminal challenge - resolve feedback

--- content/lessons/genesis/genesis-2/translations.ts
+++ content/lessons/genesis/genesis-2/translations.ts
@@ -5,7 +5,7 @@ export const translations = {
       heading: 'Let’s make sense of this',
       waiting_for_input: 'Waiting for you to write and run the script...',
       success:
-        'Interesting! The message references a news article (you can read it <Link href="/assets/images/the-times-03-01-2009.jpg" className="underline">here</Link>). This gives us an important clue about the motivation of Satoshi for creating Bitcoin.',
+        'Interesting! The message references a news article (you can read it <Link href="https://bitcoinmagazine.com/culture/ten-years-later-reflection-bitcoins-genesis-and-satoshis-timing" className="underline">here</Link>). This gives us an important clue about the motivation of Satoshi for creating Bitcoin.',
     },
 
     nl: {
@@ -13,7 +13,7 @@ export const translations = {
       heading: 'Laten we dit duidelijk maken',
       waiting_for_input: 'Wachten tot de script wordt uitgevoerd...',
       success:
-        'Interessant! Het bericht verwijst naar een nieuwsartikel (je kunt het <Link href="/assets/images/the-times-03-01-2009.jpg" className="underline">hier</Link> lezen). Dit geeft ons een belangrijke aanwijzing over de motivatie van Satoshi voor het maken van Bitcoin.',
+        'Interessant! Het bericht verwijst naar een nieuwsartikel (je kunt het <Link href="https://bitcoinmagazine.com/culture/ten-years-later-reflection-bitcoins-genesis-and-satoshis-timing" className="underline">hier</Link> lezen). Dit geeft ons een belangrijke aanwijzing over de motivatie van Satoshi voor het maken van Bitcoin.',
     },
   },
 }
