1d0cd5835796884ce0e2e4c7918399b9ad236c05
Ben Allen <benalleng@gmail.com>
2023-02-23 21:51:22 -0500

    add ui transaltion page

---
+++ ui/translations.ts
@@ -0,0 +1,19 @@
+export const translations = {
+  end: {
+    en: {
+      save: 'Save my progress',
+      next: 'Continue without saving',
+    },
+  },
+  challenge_list: {
+    en: {
+      coming_soon: 'Coming soon. Hang tight.',
+    },
+  },
+  chapter: {
+    en: {
+      chapter_locked_one: 'Complete Chapter',
+      chapter_locked_two: 'to unlock.',
+    },
+  },
+}
