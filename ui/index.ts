e123661a7a44e4fa4bf4eb9eef53d9ee6534536a
Ben Allen <benalleng@gmail.com>
2023-03-01 13:39:57 -0500

    remove old translations

--- ui/index.ts
+++ ui/index.ts
@@ -3,7 +3,3 @@ export * from './chapter'
 export * from './introduction'
 export * from './lesson'
 export * from './common'
-
-import { translations as UITranslations } from './translations'
-
-export const translations = [UITranslations]
