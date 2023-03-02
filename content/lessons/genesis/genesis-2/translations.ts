50d2b2dfcf4f7f1dcae6ad950955ffc8f975573c
ecurrencyhodler <ecurrencyhodler@gmail.com>
2023-03-01 17:59:56 -0800

    Added classname underline
    Accidently removed the underline and this fixes it.

--- content/lessons/genesis/genesis-2/translations.ts
+++ content/lessons/genesis/genesis-2/translations.ts
@@ -4,7 +4,7 @@ export const translations = {
       title: 'Genesis',
       heading: 'Let’s make sense of this',
       waiting_for_input: 'Waiting for you to write and run the script...',
-      success: `Interesting! The message references a news article on the same day Satoshi mined the genesis block. This gives us an important clue about Satoshi's motivation for creating Bitcoin. You can read more about it <Link href="https://en.bitcoin.it/wiki/Genesis_block">here</Link>.`,
+      success: `Interesting! The message references a news article on the same day Satoshi mined the genesis block. This gives us an important clue about Satoshi's motivation for creating Bitcoin. You can read more about it <Link href="https://en.bitcoin.it/wiki/Genesis_block" className="underline">here</Link>.`,
     },
 
     nl: {
