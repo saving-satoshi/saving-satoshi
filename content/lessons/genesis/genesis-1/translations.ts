4ac01adab64d91e6a2868c6266c1a6f139e1cd92
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-04 19:06:29 +0100

    fix: 179 resolve pr feedback

--- content/lessons/genesis/genesis-1/translations.ts
+++ content/lessons/genesis/genesis-1/translations.ts
@@ -6,7 +6,7 @@ export const translations = {
       paragraph_one: `Let's find the very first block in the Bitcoin blockchain. Go to a <Tooltip content="genesis_one.tooltip_block_explorer" className="underline">block explorer</Tooltip> and search for ’0’. This will take you to the block at height 0. As you can see, this block only contains one transaction.`,
       paragraph_two: `Open up the transaction so you can take a closer look at the details. On the left is an input called 'Coinbase'. Inside that is a field called 'ScriptSig'. Find the <Tooltip content="genesis_one.tooltip_hex" className="underline">HEX</Tooltip> representation and paste it below.`,
       tooltip_block_explorer:
-        'A <a href="https://blockstream.info/" target="_blank" rel="noreferrer">block explorer</a> is a tool that allows users to view the contents of a blockchain, including transactions, addresses, and other important data.',
+        'A <a href="https://bitcoinops.org/en/topics/block-explorers/" target="_blank" rel="noreferrer">block explorer</a> is a useful tool to quickly look up information about bitcoin transactions.',
       tooltip_hex:
         'Short for hexadecimal, a numeral system that represents numbers using a base of 16.',
       view_block_0: 'View Block 0',
@@ -19,7 +19,7 @@ export const translations = {
       paragraph_one: `Laten we het allereerste block in de Bitcoin blockchain vinden. Gebruik een <Tooltip content="genesis_one.tooltip_block_explorer" className="underline">block explorer</Tooltip> en zoek naar ’0’. Dit zal je naar het block op hoogte 0 nemen. Zoals je kunt zien bevat dit blok maar één transactie.`,
       paragraph_two: `Klap de transactie uit zodat u de details beter kunt bekijken. Aan de linkerzijde is er een input genaamd 'Coinbase'. Daarbinnen bevindt zich een veld met de naam 'ScriptSig'. Kopieer de <Tooltip content="genesis_one.tooltip_hex" className="underline">HEX</Tooltip> versie van de ScriptSig en plak deze hieronder.`,
       tooltip_block_explorer:
-        'Een <a href="https://blockstream.info/" target="_blank" rel="noreferrer">block explorer</a> is een tool waarmee gebruikers de inhoud van een blockchain kunnen bekijken, inclusief transacties, adressen en andere belangrijke gegevens.',
+        'Een <a href="https://bitcoinops.org/en/topics/block-explorers/" target="_blank" rel="noreferrer">block explorer</a> is een handige tool om snel informatie over bitcoin transacties op te zoeken.',
       tooltip_hex:
         'Afkorting van hexadecimaal, een cijfersysteem dat getallen vertegenwoordigt met een basis van 16.',
       view_block_0: 'Vind Block 0',
