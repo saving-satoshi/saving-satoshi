99b86feb262b7f1eb2f0a58959d3f8e7ed935d9c
GBKS <chri@sto.ph>
2023-03-06 08:42:03 +0100

    message

--- app/[lang]/chapters/[slug]/[lesson]/layout.tsx
+++ app/[lang]/chapters/[slug]/[lesson]/layout.tsx
@@ -1,4 +1,4 @@
 import { chapters } from 'content'
 export default function Layout({ children }) {
-  return <div className="flex h-full grow flex-col">{children}</div>
+  return <div className="flex grow flex-col">{children}</div>
 }
