642d5120bc0d89ec01f5ba50bec95aa25816a5cb
Shashwat Vangani <shaavan.github@gmail.com>
2023-03-06 22:04:34 +0530

    Changed the transaction in Chapter-1, Challenge-2 (#223)

--- content/lessons/transacting/transacting-3/translations.ts
+++ content/lessons/transacting/transacting-3/translations.ts
@@ -14,7 +14,7 @@ export const translations = {
         'Once you’ve found it, decode the hidden message, just like we did in the previous exercise.',
       paragraph_three: 'And remember, ’Bitcoin is for everyone’.',
       terminal_challenge_success:
-        'That’s correct! Nice work. We don’t know why tacos were so important to the anonymous sender of this transaction, but here we are. And it’s an admirable goal at least.',
+        'That’s correct! Nice work.\n\n As you can see, the clue is an address. Go to it.\n\n There you’ll find an abandoned warehouse with all the tools you need to fight BitRey and bring equilibrium back to bitcoin.\n\n Your next challenge awaits you.',
       terminal_challenge_lines:
         'Enter your commands here and press Enter...\n\n Note that $scriptPubKeyHex is not defined for you this time. You’ll need to replace this variable in the code with the value you found in the previous challenge',
       terminal_challenge_error:
