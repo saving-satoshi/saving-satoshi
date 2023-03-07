036cd9e2bbfd7a761eb35dbbd3c924929001b15f
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-07 21:03:36 +0100

    fix: translation system - remove en translations from nl locale

--- i18n/locales/nl/translations.ts
+++ i18n/locales/nl/translations.ts
@@ -26,20 +26,10 @@ export const translations = {
 
   chapter_three: {
     title: 'Je beloning claimen',
-    paragraph_one:
-      'Phew, that was close. You are still shaking from your battle againstAmestris, yet relieved that a victory was possible.',
-    paragraph_two:
-      'As you settle at your desk, you take a deep breath and drift off toreplay the day’s events. Your hands keep themselves busy playing withthe mysterious envelope, as you wonder who sent it. Who still usesenvelopes anyways, it’s 2139 after all. Even more curious is that thereis a stamp on it, from 2008...could this be another hint?',
-    paragraph_three:
-      'Suddenly, you realize that you never claimed the block rewards from yourcompetition with Amestris. Your bitcoin wallet is running low, so now’sa good time to top it up again.',
   },
 
   chapter_four: {
     title: 'Wil de echte Satoshi opstaan?',
-    paragraph_one:
-      'A mysterious billionaire is claiming to be Satoshi Nakamoto. Why would Satoshi come forward after all this time? What are the chances Satoshi is even alive?',
-    paragraph_two:
-      'While the public debate is largely focused on the spectacle, you have an inkling that it might be possible to disarm the claim using your knowledge of cryptography.',
   },
 
   chapter_five: {
@@ -73,12 +63,6 @@ export const translations = {
     paragraph_two: `Elk blok in de bitcoin-blockchain bevat een reeks transacties. Een leuke manier om deze blokken en de dingen die ze bevatten te bekijken, is door een blokverkenner te gebruiken.`,
   },
 
-  done: {
-    title: 'You did it!!!',
-    description:
-      'Amazing. You completed the first chapter and learned a lot about hashes and transactions. Are you ready for more?',
-  },
-
   genesis_one: {
     title: 'Genesis',
     heading: 'Vind block 0',
@@ -100,56 +84,6 @@ export const translations = {
       'Interessant! Het bericht verwijst naar een nieuwsartikel dat op dezelfde dag dat Satoshi het genesis block heeft gemined, gepubliceerd is. Dit geeft ons een belangrijke aanwijzing over de motivatie van Satoshi voor het maken van Bitcoin. Je kunt er <Link href="https://bitcoinmagazine.com/culture/ten-years-later-reflection-bitcoins-genesis-and-satoshis-timing" className="underline">hier</Link> meer over lezen.',
   },
 
-  transacting_one: {
-    title: 'Transacting',
-    heading: 'What’s in a transaction?',
-    paragraph_one:
-      'Bitcoin is censorship resistant money. Anybody can send money by broadcasting a transaction to the network. After broadcast, transactions are packaged up into blocks by miners. Miners compete against other miners for the privilege of building on the chain.',
-    paragraph_two:
-      'Transactions have two parts: inputs and outputs. In the previous exercise we decoded a secret message found inside a transaction input. This time we’ll decode a message that belongs to the output part.',
-    paragraph_three:
-      'For the following transaction, identify the output of type OP_RETURN.',
-  },
-
-  transacting_two: {
-    title: 'Transacting',
-    heading: 'OP_RETURN',
-    paragraph_one:
-      'For the following transaction, identify the output of type OP_RETURN.',
-    link: 'View transaction',
-    input_challenge_label: 'Enter the OP_RETURN type',
-  },
-
-  transacting_three: {
-    title: 'Transacting',
-    heading: 'Another secret message',
-    paragraph_one:
-      'Then, look for the hex representation of the ScriptPubKey field.',
-    paragraph_two:
-      'Once you’ve found it, decode the hidden message, just like we did in the previous exercise.',
-    paragraph_three: 'And remember, ’Bitcoin is for everyone’.',
-    terminal_challenge_success:
-      'That’s correct! Nice work.\n\n As you can see, the clue is an address. Go to it.\n\n There you’ll find an abandoned warehouse with all the tools you need to fight BitRey and bring equilibrium back to bitcoin.\n\n Your next challenge awaits you.',
-    terminal_challenge_lines:
-      'Enter your commands here and press Enter...\n\n Note that $scriptPubKeyHex is not defined for you this time. You’ll need to replace this variable in the code with the value you found in the previous challenge',
-    terminal_challenge_error:
-      'Almost. Take another look to find the correct value',
-  },
-
-  end: {
-    save: 'Save my progress',
-    next: 'Continue without saving',
-  },
-
-  challenge_list: {
-    coming_soon: 'Coming soon. Hang tight.',
-  },
-
-  chapter: {
-    chapter_locked_one: 'Complete Chapter',
-    chapter_locked_two: 'to unlock.',
-  },
-
   hero: {
     title: 'Saving Satoshi',
     description: 'Programmeer je weg door de mysteries van bitcoin.',
@@ -157,49 +91,11 @@ export const translations = {
     tell_more: 'Vertel me meer',
   },
 
-  footer: {
-    paragraph_one: 'An open-source production by the bitcoin community.',
-    link: 'Check the code',
-  },
-
   navbar: {
     intro: 'Intro',
     chapter: 'Hoofdstuk',
     chapter_complete: 'Hoofdstuk compleet',
     challenge: 'Uitdaging',
-  },
-
-  modal_login: {
-    heading: 'Load your progress',
-    paragraph_one:
-      'Enter your personal code below to restore your chapter and challenge progress, as well as your avatar.',
-    clear: 'No code yet?',
-    prompt: 'Enter your code',
-    confirm: 'Load my progress',
-    paragraph_two:
-      'Sorry, that’s not the right code for the progress stored in this browser.',
-    paragraph_three: 'No code yet or want to start again?',
-    quit: 'Clear save progress.',
-    success: 'Progress loaded',
-    success_message: 'You’re all set to continue with Chapter',
-  },
-
-  modal_logout: {
-    heading: 'You are logged in',
-    paragraph_one:
-      'Your progress is stored in this browser. You will be automatically logged out after 30 days. Use the code below to restore it after taking a break.',
-    signout: 'Sign out',
-  },
-
-  modal_signup: {
-    heading: 'Want to save your progress?',
-    paragraph_one:
-      'Copy and store a simple code to save and load your progress on any device and browser. If you already have a code, load your progress here.',
-    subheading_one: 'Choose an avatar',
-    subheading_two: 'Back up your personal code',
-    paragraph_two:
-      'All set? Code copied and backed-up? Make sure your do, as it can’t be recovered if you lose it.',
-    confirm: 'I’ve copied and backed up my code',
   },
 
   demo_disclaimer: {
