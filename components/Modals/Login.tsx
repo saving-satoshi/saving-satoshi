cfa27c739ce8be7c81868c1e6244fbf8118fadbd
sabakhilji <52318459+sabakhilji@users.noreply.github.com>
2023-03-07 17:07:11 -0500

    Change background color for modal( issue#212)
    Changing Modal Background opacity from 75% to 50%.

--- components/Modals/Login.tsx
+++ components/Modals/Login.tsx
@@ -80,11 +80,10 @@ export default function LoginModal({ onClose, onLogin, open }) {
   return (
     <Modal
       isOpen={open}
+      overlayClassName="fixed inset-0 bg-[#01010180]"
       className="fixed inset-0 top-1/2 left-1/2 h-full w-screen -translate-x-1/2 -translate-y-1/2 transform bg-back p-5  pt-10 font-nunito text-white shadow-lg outline-none sm:absolute sm:h-fit sm:w-[550px] sm:rounded-lg sm:pt-5"
       contentLabel="Login Modal"
-      styles={{overlay:{
-      backgroundColor:'rgba(1,1,1,0.75)'}}
-    >
+     >
       <div className="float-right flex justify-end">
         <button onClick={handleCloseClick} aria-label="Close">
           <CloseIcon className="h-6 w-6" />
