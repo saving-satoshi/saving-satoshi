cb89557be3b38e5fffc727d2ff722c8f39f70fc4
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-07 19:56:07 +0100

    Tooltip improvements & copy change (#233)
    * fix: add a tag support to tooltip, modify tooltip copy for genesis-1
    
    * fix: 179 resolve pr feedback
    
    * fix: missing translations in tooltip

--- i18n/locales/en/translations.ts
+++ i18n/locales/en/translations.ts
@@ -110,9 +110,10 @@ export const translations = {
     en: {
       title: 'Genesis',
       heading: 'Find block 0',
-      paragraph_one: `Let's find the very first block in the Bitcoin blockchain. Go to a <Tooltip content="genesis_one.tooltip_block_explorer" href="https://blockstream.info" className="underline">block explorer</Tooltip> and search for ’0’. This will take you to the block at height 0. As you can see, this block only contains one transaction.`,
+      paragraph_one: `Let's find the very first block in the Bitcoin blockchain. Go to a <Tooltip content="genesis_one.tooltip_block_explorer" className="underline">block explorer</Tooltip> and search for ’0’. This will take you to the block at height 0. As you can see, this block only contains one transaction.`,
       paragraph_two: `Open up the transaction so you can take a closer look at the details. On the left is an input called 'Coinbase'. Inside that is a field called 'ScriptSig'. Find the <Tooltip content="genesis_one.tooltip_hex" className="underline">HEX</Tooltip> representation and paste it below.`,
-      tooltip_block_explorer: 'We recommend https://blockstream.info/',
+      tooltip_block_explorer:
+        'A <a href="https://bitcoinops.org/en/topics/block-explorers/" target="_blank" rel="noreferrer">block explorer</a> is a useful tool to quickly look up information about bitcoin transactions.',
       tooltip_hex:
         'Short for hexadecimal, a numeral system that represents numbers using a base of 16.',
       view_block_0: 'View Block 0',
