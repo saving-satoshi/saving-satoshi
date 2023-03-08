222cfb4f701b42ed2a11af1e310b4e923f4701b0
Ben Allen <benalleng@gmail.com>
2023-03-08 16:38:10 -0500

    Remove: lang param from home-button

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -13,7 +13,7 @@ export default function Topbar({ items }: { items: NavItem[] }) {
     <div className="absolute left-0 top-0 w-full">
       <div className="flex items-center justify-between px-6 py-4 text-white sm:m-auto">
         <Link
-          href={`/${lang}`}
+          href={`/`}
           className="transition duration-150 ease-in-out hover:opacity-75"
         >
           <h1 className="text-xl sm:text-3xl">{siteConfig.name}</h1>
