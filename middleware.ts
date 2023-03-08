5587aecf78fd15540a1af054f1e75440a1401323
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2023-03-09 02:43:02 +0530

    Removed /en ending from Homepage (#247)
    * Fix: removed /en from homepage
    
    * Fix: Redirect to /[lang]

--- middleware.ts
+++ middleware.ts
@@ -22,17 +22,23 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+  // Redirect to '/' for homepage
+  if (pathname !== '/' || getLocale(request) !== 'en') {
+    const pathnameIsMissingLocale = i18n.locales.every(
+      (locale) =>
+        !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    )
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+    // Redirect if there is no locale
+    if (pathnameIsMissingLocale) {
+      const locale = getLocale(request)
 
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url))
+      // e.g. incoming request is /products
+      // The new URL is now /en-US/products
+      return NextResponse.redirect(
+        new URL(`/${locale}${pathname}`, request.url)
+      )
+    }
   }
 }
 
