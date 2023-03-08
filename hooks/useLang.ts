9da5cc07f0ed0fc6b43cd1b7b74e1e1f61fe53f3
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-08 22:23:44 +0100

    fix: 249 missing translations on homepage for english locale

--- hooks/useLang.ts
+++ hooks/useLang.ts
@@ -1,8 +1,17 @@
 import { usePathname } from 'next/navigation'
 
+import { i18n } from 'i18n/config'
+
 export const useLang = () => {
   const pathName = usePathname()
+
   const parts = pathName.split('/').filter((p) => p)
 
-  return parts.shift()
+  const lang = parts.shift()
+
+  if (!lang) {
+    return i18n.defaultLocale
+  }
+
+  return lang
 }
