206d54a090dc8d2fda18dcf1ea16f0fa1abe0a77
sabakhilji <52318459+sabakhilji@users.noreply.github.com>
2023-03-10 11:48:41 -0500

    Add sucess page(# 261)
    Add success page after task 1

--- content/lessons/index.tsx
+++ content/lessons/index.tsx
@@ -4,9 +4,11 @@ import * as Transacting1 from './transacting/transacting-1'
 import * as Transacting2 from './transacting/transacting-2'
 import * as Transacting3 from './transacting/transacting-3'
 import * as Done from './done'
+import * as Success1 from ./success1
 
 export const lessons = {
   'genesis-1': Genesis1,
+  'success1': Success1,
   'genesis-2': Genesis2,
   'transacting-1': Transacting1,
   'transacting-2': Transacting2,
