bb8a04de6b413de7a2b23c80f5280c0cbb383104
shaavan <shaavan.github@gmail.com>
2023-03-22 17:08:03 +0530

    Add per-page theming option

--- components/Navbar/NavbarMobile/Menu.tsx
+++ components/Navbar/NavbarMobile/Menu.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import TabGroupMobile from 'components/Navbar/NavbarMobile/TabGroupMobile'
 import clsx from 'clsx'
+import { lessons } from 'content'
 
 export default function Menu(props) {
+  const { slug, lesson: lessonId } = props.params
+
+  const defaultTheme = 'bg-back'
+  const { theme = defaultTheme } = lessons[slug][lessonId].metadata
+
   return (
     <div
       id="lesson-navigation-menu"
-      className={clsx('absolute z-10 w-full bg-back duration-[400ms]', {
-        'h-full': props.isOpen,
-        'h-0 delay-200': !props.isOpen,
-      })}
+      className={clsx(
+        'absolute z-10 w-full bg-back duration-[400ms]',
+        `${theme}`,
+        {
+          'h-full': props.isOpen,
+          'h-0 delay-200': !props.isOpen,
+        }
+      )}
       aria-modal="true"
       aria-hidden={!props.isOpen}
       aria-label="Lesson Navigation Menu"
