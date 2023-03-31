611bacd35a165c3aaf2c41e407626d8e2f94403b
vinayaksh42 <vinayaksh42@gmail.com>
2023-03-31 19:28:01 +0530

    Minor changes to logic

--- hooks/useLessonUnlocked.ts
+++ hooks/useLessonUnlocked.ts
@@ -15,30 +15,30 @@ export const useLessonUnlocked = () => {
 
   if (userInfo.user === undefined) {
     return `/${lang}`
-  } else {
-    const userLesson = userInfo.user.progress.lesson
-    const userChapter = userInfo.user.progress.chapter
-    const lessonArray = [
-      ...chapters[chapterId].metadata.intros,
-      ...chapters[chapterId].metadata.lessons,
-      ...chapters[chapterId].metadata.outros,
-    ]
-    const redirectURL = `/${lang}/${pageId}/${userChapter}/${userLesson}`
-
-    if (
-      userChapter.split('-')[1] >= chapterId.split('-')[1] &&
-      getIndex(lessonArray, userLesson) >= getIndex(lessonArray, lessonId)
-    ) {
-      return true
-    }
-
-    if (
-      userChapter.split('-')[1] >= chapterId.split('-')[1] &&
-      removeAfterHyphen(userLesson) === removeAfterHyphen(lessonId)
-    ) {
-      return true
-    }
-
-    return redirectURL
   }
+
+  const userLesson = userInfo.user.progress.lesson
+  const userChapter = userInfo.user.progress.chapter
+  const lessonArray = [
+    ...chapters[chapterId].metadata.intros,
+    ...chapters[chapterId].metadata.lessons,
+    ...chapters[chapterId].metadata.outros,
+  ]
+  const redirectURL = `/${lang}/${pageId}/${userChapter}/${userLesson}`
+
+  if (
+    userChapter.split('-')[1] >= chapterId.split('-')[1] &&
+    getIndex(lessonArray, userLesson) >= getIndex(lessonArray, lessonId)
+  ) {
+    return true
+  }
+
+  if (
+    userChapter.split('-')[1] >= chapterId.split('-')[1] &&
+    removeAfterHyphen(userLesson) === removeAfterHyphen(lessonId)
+  ) {
+    return true
+  }
+
+  return redirectURL
 }
