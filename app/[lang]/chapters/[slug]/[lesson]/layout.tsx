db98b5c0e40c2f3305f2e2ab9eeddde648f52bc3
vinayaksh42 <vinayaksh42@gmail.com>
2023-04-07 20:34:19 +0530

    Dev param for locked lesson

--- app/[lang]/chapters/[slug]/[lesson]/layout.tsx
+++ app/[lang]/chapters/[slug]/[lesson]/layout.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useLessonUnlocked, useLocalStorage } from 'hooks'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import Navbar from 'components/Navbar'
 import { lessons } from 'content'
 
 export default function Layout({ children, params }) {
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const dev = searchParams.get('dev')
   const userInfo = useLocalStorage('SavingSatoshiUser')
   const { slug, lesson: lessonId } = params
   const defaultTheme = 'bg-back'
@@ -14,7 +16,7 @@ export default function Layout({ children, params }) {
 
   const result = useLessonUnlocked(userInfo)
 
-  if (typeof result === 'string') {
+  if (typeof result === 'string' && !dev) {
     router.replace(result)
   }
 
@@ -22,8 +24,8 @@ export default function Layout({ children, params }) {
     <div className={`${theme} flex flex-col`}>
       <div className="fix-grow-issue flex min-h-screen flex-col overflow-hidden drop-shadow-3xl backdrop-blur-4xl">
         <Navbar params={params} />
-        {result === true && children}
-        {(result === false || result === 'string') && (
+        {(result === true || dev) && children}
+        {(result === false || result === 'string') && !dev && (
           <div className="flex h-full w-full grow flex-col items-center justify-center">
             <span className="mb-10 text-4xl text-white">
               Loading Challenge...
