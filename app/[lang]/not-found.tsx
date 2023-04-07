9d4e90a5fc23d23146ebfba60e5ef07b1d148902
sabakhilji <52318459+sabakhilji@users.noreply.github.com>
2023-04-07 12:28:34 -0400

    update z-index

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -22,7 +22,7 @@ export default function NotFound() {
       />
       <Topbar />
 
-      <div className="absolute top-1/2 left-1/2 z-10 flex w-screen -translate-x-1/2 -translate-y-1/2 flex-col justify-center">
+      <div className="absolute top-1/2 left-1/2 z-0 flex w-screen -translate-x-1/2 -translate-y-1/2 flex-col justify-center">
         <h1 className=" text-center font-cbrush text-6xl text-white">
           {t('notfound.first')}
         </h1>
