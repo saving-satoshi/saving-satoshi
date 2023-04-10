c599e0f865fdc22a3d4e56e094ec58ae717d666b
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2023-04-10 14:05:13 +0530

    Block navigation to locked lessons (#329)
    * Add: Block navigation to locked lessons
    
    * Build fix
    
    * Build Fail due to name error
    
    * Fix for build fail
    
    * Fix: view old lesson
    
    * Tab link disabled for locked lesson
    
    * Redirect user directly to last lesson
    
    * Logic for multiple chapters
    
    * Changes as per feedback
    
    * Minor changes to logic
    
    * Styling changes
    
    * Fix: lesson content visible for a second
    
    * Testing for redirect to homepage
    
    * Revert "Testing for redirect to homepage"
    
    This reverts commit 731a1ed0caed18a905feef70ebd495e386ee2674.
    
    * Testing for homepage redirect
    
    * Logic for handling lower chapters
    
    * save progress for chapter-2
    
    * redirect to homepage for new user
    
    * Fix for redirect to homepage issue
    
    * Fix for local storage undefined
    
    * Production test
    
    * code cleanup
    
    * Changes as per feedback and logic update
    
    * Dev param for locked lesson
    
    * Remove: useLocalStorage hook

---
+++ hooks/useLessonUnlocked.ts
@@ -0,0 +1,48 @@
+'use client'
+
+import { usePathData } from 'hooks'
+import { chapters } from 'content'
+import { User } from 'types'
+
+export const useLessonUnlocked = (userInfo) => {
+  const user: User = userInfo ? JSON.parse(userInfo) : null
+  const { lang, pageId, chapterId, lessonId } = usePathData()
+
+  // false if the userInfo is not available
+  if (user === null) {
+    return false
+  }
+
+  const userLesson = user?.progress?.lesson
+  const userChapter = user?.progress?.chapter
+  const lessonArray = [
+    ...chapters[chapterId].metadata.intros,
+    ...chapters[chapterId].metadata.lessons,
+    ...chapters[chapterId].metadata.outros,
+  ]
+  const redirectURL = `/${lang}/${pageId}/${userChapter}/${userLesson}`
+
+  // true if the user is trying to access a previous completed chapter
+  if (userChapter.split('-')[1] > chapterId.split('-')[1]) {
+    return true
+  }
+
+  // true if the user is trying to access an ongoing unlocked chapter and is on the ongoing lesson or previous lesson
+  if (
+    userChapter.split('-')[1] == chapterId.split('-')[1] &&
+    lessonArray.indexOf(userLesson) >= lessonArray.indexOf(lessonId)
+  ) {
+    return true
+  }
+
+  // true if the user is trying to access an ongoing unlocked chapter and is on a later part of the lesson. ex: genesis-4
+  if (
+    userChapter.split('-')[1] == chapterId.split('-')[1] &&
+    userLesson.split('-')[0] === lessonId.split('-')[0]
+  ) {
+    return true
+  }
+
+  // redirectURL to the ongoing lesson if none of the above conditions are true
+  return redirectURL
+}
