bd6a2ebbef96588fe6eff2b8f013a104a4afa712
benalleng <benalleng@gmail.com>
2023-04-18 11:51:43 -0400

    remove key props

--- ui/chapter/Intro.tsx
+++ ui/chapter/Intro.tsx
@@ -8,18 +8,9 @@ export default function Intro({
   heading?: string
 }) {
   return (
-    <div
-      key="div-1"
-      className="my-auto grid w-full grid-cols-1 justify-center justify-items-center"
-    >
-      <div
-        key="div-2"
-        className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0"
-      >
-        <div
-          key="div-3"
-          className="content-center justify-items-center px-1 font-nunito"
-        >
+    <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
+      <div className="flex w-full items-center px-6 text-white lg:w-1/2 lg:px-0">
+        <div className="content-center justify-items-center px-1 font-nunito">
           {heading && <Title>{heading}</Title>}
           {children}
         </div>
