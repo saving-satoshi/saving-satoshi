1458d2a7a36e9d3ef0b70de58d00f825495b489a
benalleng <benalleng@gmail.com>
2023-04-19 01:02:23 -0400

    Add: formatHash placeholder

--- lib/formatHash.tsx
+++ lib/formatHash.tsx
@@ -1,4 +1,8 @@
-export default function formatHash(hash: string) {
+export default function formatHash(
+  hash: string,
+  length?: number,
+  underscores?: boolean
+) {
   const result = []
   const chunkSize = 4
   const numberOfRows = 2
@@ -9,8 +13,8 @@ export default function formatHash(hash: string) {
   for (let i = 0; i < numberOfRows; i++) {
     const row = []
 
-    for (let j = 0; j < hash.length / (chunkSize * numberOfRows); j++) {
-      const startIndex = i * (hash.length / numberOfRows) + j * chunkSize
+    for (let j = 0; j < length / (chunkSize * numberOfRows); j++) {
+      const startIndex = i * (length / numberOfRows) + j * chunkSize
       const chunk = hash
         .slice(startIndex, startIndex + chunkSize)
         .split('')
@@ -26,11 +30,35 @@ export default function formatHash(hash: string) {
           return char
         })
 
-      row.push(
-        <span key={startIndex} className="text-white text-opacity-50">
-          {chunk}
-        </span>
-      )
+      const placeholder = '_'.repeat(length)
+      const placeholderChunk =
+        underscores &&
+        placeholder
+          .slice(startIndex, startIndex + chunkSize)
+          .split('')
+          .map((char, idx) => {
+            if (zeroCount < leadingZeros && char === '0') {
+              zeroCount++
+              return (
+                <span key={idx} className="text-white">
+                  {char}
+                </span>
+              )
+            }
+            return char
+          })
+
+      !!hash && underscores
+        ? row.push(
+            <span key={startIndex} className="text-white text-opacity-50">
+              {chunk}
+            </span>
+          )
+        : row.push(
+            <span key={startIndex} className="text-white text-opacity-50">
+              {placeholderChunk}
+            </span>
+          )
     }
 
     result.push(
--- ui/common/Hasher.tsx
+++ ui/common/Hasher.tsx
@@ -43,24 +43,9 @@ export default function Hasher({
         <h2 className="text-left text-[18px] font-bold md:text-center">
           {label}
         </h2>
-        <p
-          className={clsx(
-            'h-full w-full resize-none overflow-hidden break-words bg-transparent text-left font-space-mono text-[18px] leading-[180%] tracking-[1px] outline-none md:w-full md:max-w-5xl md:text-center md:text-[30px] md:tracking-[5%]',
-            {
-              'overlay-complete':
-                (typeof answer === 'string' &&
-                  hash.startsWith(answer) === true) ||
-                (typeof answer === 'number' && input.length >= answer),
-              'overlay-incomplete':
-                (typeof answer === 'string' &&
-                  hash.startsWith(answer) === false) ||
-                (typeof answer === 'number' && input.length < answer),
-              underscore: !hash || !answer,
-            }
-          )}
-        >
-          {formatHash(hash)}
-        </p>
+        <span className="h-full w-full resize-none overflow-hidden bg-transparent text-left font-space-mono text-[18px] leading-[180%] tracking-[1px] outline-none md:w-full md:max-w-5xl md:text-center md:text-[30px] md:tracking-[5%]">
+          {formatHash(hash, 64, true)}
+        </span>
       </div>
     </>
   )
