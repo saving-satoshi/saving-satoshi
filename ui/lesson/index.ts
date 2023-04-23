6b11a7d2a99a9a9f89b99d086412056a27e258d4
benalleng <benalleng@gmail.com>
2023-04-23 15:22:26 -0400

    file restructure

--- ui/lesson/index.ts
+++ ui/lesson/index.ts
@@ -1,12 +1,12 @@
 import Lesson, { useLessonContext } from './Lesson'
 import LessonInfo from './Info'
-import LessonPrompt from './InputChallenge/Prompt'
-import LessonTerminal from './TerminalChallenge/Terminal'
+import LessonPrompt from './Prompt'
+import LessonTerminal from './Terminal'
 import LessonTabs from './Tabs'
-import TerminalChallenge from './TerminalChallenge/TerminalChallenge'
-import InputChallenge from './InputChallenge/InputChallenge'
+import TerminalChallenge from './TerminalChallenge'
+import InputChallenge from './InputChallenge'
 import ScriptingChallenge from './ScriptingChallenge'
-import HashChallenge from './HashChallenge/HashChallenge'
+import HashChallenge from './HashChallenge'
 
 export {
   Lesson,
