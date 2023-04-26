566bce6ee2975adbbca267f8c3250bf1ae3da981
benalleng <benalleng@gmail.com>
2023-04-26 09:34:05 -0400

    retry prefetch

--- hooks/useSaveAndReturn.ts
+++ hooks/useSaveAndReturn.ts
@@ -24,7 +24,7 @@ export default function useSaveAndReturn() {
     if (account && progress && !isLessonUnlocked(progress, nextLessonKey)) {
       await saveProgress(nextLessonKey)
     }
-    router.prefetch('/chapters')
+    router.prefetch(lang + '/chapters#chapter-' + nextLessonKey.charAt(2))
     router.push(lang + '/chapters#chapter-' + nextLessonKey.charAt(2))
   }
 
