01f7395489a19acd5862359713ce81ac341917f1
benalleng <benalleng@gmail.com>
2023-04-25 21:05:28 -0400

    new saveandreturn hook

---
+++ hooks/useSaveAndReturn.ts
@@ -0,0 +1,31 @@
+'use client'
+
+import { getNextLessonKey, isLessonUnlocked } from 'lib/progress'
+import { useRouter } from 'next/navigation'
+import { useProgressContext } from 'providers/ProgressProvider'
+import { usePathData, useLang } from 'hooks'
+import { lessons } from 'content'
+import { useAuthContext } from 'providers/AuthProvider'
+
+export default function useSaveAndReturn() {
+  const lang = useLang()
+  const router = useRouter()
+  const { account } = useAuthContext()
+  const { progress, saveProgress } = useProgressContext()
+  const { chapterId, lessonId } = usePathData()
+
+  const chapterLessons = lessons[chapterId]
+  const lesson = chapterLessons[lessonId].metadata
+  const currentLessonKey = lesson.key
+
+  const saveAndReturn = async () => {
+    const nextLessonKey = getNextLessonKey(currentLessonKey)
+
+    if (account && progress && !isLessonUnlocked(progress, nextLessonKey)) {
+      await saveProgress(nextLessonKey)
+    }
+    router.push(lang + '/chapters#chapter-' + nextLessonKey.charAt(2))
+  }
+
+  return saveAndReturn
+}
