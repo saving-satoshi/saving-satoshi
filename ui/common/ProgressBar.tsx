5f44e3b9bc5f11d6ae6463e208e13047afb280ab
vinayaksh42 <vinayaksh42@gmail.com>
2023-05-06 01:18:09 +0530

    Progress bar color

--- ui/common/ProgressBar.tsx
+++ ui/common/ProgressBar.tsx
@@ -9,7 +9,7 @@ export default function ProgressBar({ progress }: { progress: number }) {
         className={clsx(
           'absolute top-0 left-0 h-full rounded-[5px] bg-[#FBEBC6]',
           {
-            'bg-[#5DBC59]': progress === 100,
+            '!bg-[#5DBC59]': progress === 100,
           }
         )}
         style={{ width: `${progress}%` }}
