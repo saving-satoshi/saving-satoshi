3275aceb7aab8392e9a312f083ed92751bdbeff0
vinayaksh42 <vinayaksh42@gmail.com>
2023-05-11 23:34:02 +0530

    transistion gpu for progress bar

--- ui/common/ProgressBar.tsx
+++ ui/common/ProgressBar.tsx
@@ -7,7 +7,7 @@ export default function ProgressBar({ progress }: { progress: number }) {
     <div className="relative h-[30px] w-full rounded-[5px]  bg-black/20">
       <div
         className={clsx(
-          'absolute top-0 left-0 h-full rounded-[5px] bg-[#FBEBC6]',
+          'absolute top-0 left-0 h-full transform-gpu rounded-[5px] bg-[#FBEBC6] transition-transform',
           {
             '!bg-[#5DBC59]': progress === 100,
           }
