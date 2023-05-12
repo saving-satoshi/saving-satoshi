73055655329876abdaf5cb6abafbef270432e706
benalleng <benalleng@gmail.com>
2023-05-12 10:27:18 -0400

    Fix: Chapter 2 intro page padding

--- content/lessons/chapter-2/hashing-1.tsx
+++ content/lessons/chapter-2/hashing-1.tsx
@@ -20,6 +20,14 @@ export default function Hashing1({ lang }) {
         {t('chapter_two.hashing_one.paragraph_one')}
       </p>
 
+      <p className="mt-8 break-all text-lg md:text-xl">
+        {t('chapter_two.hashing_one.list_one')}
+      </p>
+
+      <p className="break-all text-lg md:text-xl">
+        {t('chapter_two.hashing_one.list_two')}
+      </p>
+
       <p className="mt-8 text-lg md:text-xl">
         {t('chapter_two.hashing_one.paragraph_two')}
       </p>
@@ -30,10 +38,6 @@ export default function Hashing1({ lang }) {
 
       <p className="mt-8 text-lg md:text-xl">
         {t('chapter_two.hashing_one.paragraph_four')}
-      </p>
-
-      <p className="mt-8 text-lg md:text-xl">
-        {t('chapter_two.hashing_one.paragraph_five')}
       </p>
 
       <Button onClick={saveAndProceed} classes="mt-10 max-md:w-full">
--- ui/chapter/Intro.tsx
+++ ui/chapter/Intro.tsx
@@ -1,14 +1,26 @@
+import clsx from 'clsx'
+
 import { Title } from 'ui'
 
 export default function Intro({
   children,
   heading,
+  mobileCentered,
 }: {
   children: any
   heading?: string
+  mobileCentered?: boolean
 }) {
   return (
-    <div className="my-auto grid w-full grid-cols-1 justify-center justify-items-center">
+    <div
+      className={clsx(
+        'grid w-full grid-cols-1 justify-center justify-items-center md:my-auto',
+        {
+          'my-auto': mobileCentered,
+          'my-8': !mobileCentered,
+        }
+      )}
+    >
       <div className="flex w-full justify-center px-6 text-white lg:w-1/2 lg:px-0">
         <div className="content-center justify-items-center px-1 font-nunito">
           {heading && <Title>{heading}</Title>}
