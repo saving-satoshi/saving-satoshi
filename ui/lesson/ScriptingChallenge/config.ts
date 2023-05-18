aaef3ff66e2015987f19e489edd64beabbfa9cf9
benalleng <benalleng@gmail.com>
2023-05-18 14:54:07 -0400

    Fix: config prop bug

--- ui/lesson/ScriptingChallenge/config.ts
+++ ui/lesson/ScriptingChallenge/config.ts
@@ -3,6 +3,13 @@ import { editor } from 'monaco-editor'
 export const monacoOptions: editor.IStandaloneEditorConstructionOptions = {
   readOnly: false,
   minimap: { enabled: false },
+  // overviewRulerLanes: 0,
+  // overviewRulerBorder: false,
+  // scrollbar: {
+  //   vertical: 'auto',
+  //   horizontal: 'hidden',
+  //   handleMouseWheel: true,
+  // },
   scrollBeyondLastLine: false,
   wordWrap: 'on',
   theme: 'vs-dark',
