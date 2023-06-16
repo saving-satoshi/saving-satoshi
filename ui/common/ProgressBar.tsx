8af428f5601bf2e65ccf51fc67fabddbf361b030
benalleng <benalleng@gmail.com>
2023-06-16 14:26:48 -0400

    feat: make new variants for progressBar

--- ui/common/ProgressBar.tsx
+++ ui/common/ProgressBar.tsx
@@ -2,32 +2,70 @@
 
 import clsx from 'clsx'
 
+export enum ProgressBarVariant {
+  Blocks = 0,
+  Percentage = 1,
+  Total = 2,
+  TotalBar = 3,
+}
+
 export default function ProgressBar({
   progress,
   title,
+  variant,
+  fontSize,
   total,
+  percentage,
+  focus,
 }: {
   progress: number
   title: string
+  variant: ProgressBarVariant
+  fontSize: string
   total?: number
+  percentage?: number
+  focus?: boolean
 }) {
   return (
     <>
       <div
-        className={clsx('relative mb-2.5 font-nunito text-lg font-semibold', {
-          'fade-in text-white': progress !== 0,
-          'text-black/50': progress === 0,
-        })}
+        className={clsx(
+          `${fontSize} mb-2.5 flex flex-row items-center gap-[10px] self-stretch font-nunito text-lg font-semibold`,
+          {
+            'fade-in text-white': progress !== 0,
+            'text-black/50': progress === 0,
+            'py-[10px] px-[15px]':
+              variant === ProgressBarVariant.Blocks ||
+              variant === ProgressBarVariant.Percentage,
+          }
+        )}
       >
-        <span className={clsx({ 'fade-in text-[#EDA081]': progress !== 0 })}>
+        <span
+          className={clsx('flex-grow', {
+            'fade-in text-[#EDA081]': progress !== 0,
+          })}
+        >
           {title}
         </span>{' '}
-        <span className="absolute right-0">
-          {progress} {total && `of ${total}`}
+        {variant === ProgressBarVariant.Percentage && percentage && (
+          <span
+            className={clsx('fade-in items-start', {
+              'rounded-[5px] bg-[#28B123] py-[5px] px-[7px] text-white': focus,
+              'text-[#3DCFEF]': !focus,
+            })}
+          >
+            {percentage}%
+          </span>
+        )}
+        <span>
+          {progress}{' '}
+          {(ProgressBarVariant.Total || ProgressBarVariant.TotalBar) &&
+            total &&
+            `of ${total}`}
         </span>
       </div>
-      {total && (
-        <div className="relative h-[30px] w-full rounded-[5px]  bg-black/20">
+      {variant === ProgressBarVariant.TotalBar && total && (
+        <div className="relative h-[30px] w-full rounded-[5px] bg-black/20">
           <div className="h-full w-full overflow-hidden rounded-[5px]">
             <div
               className={clsx(
