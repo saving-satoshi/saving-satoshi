e653040287535f5baf985f260214327e2e4fa9d0
benalleng <benalleng@gmail.com>
2023-06-27 15:08:34 -0400

    chore: remove logging

--- ui/common/ProfilesContainer.tsx
+++ ui/common/ProfilesContainer.tsx
@@ -40,8 +40,6 @@ export default function ProfilesContainer({
     }
   }, [])
 
-  console.log(hasOverflow)
-
   return (
     <>
       <div className="flex flex-col items-center gap-[30px] self-stretch">
