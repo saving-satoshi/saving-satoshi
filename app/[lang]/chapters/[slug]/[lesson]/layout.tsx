1698c9769ef10fafa3c93964cec4918f252a9db9
Shashwat Vangani <shaavan.github@gmail.com>
2023-07-03 18:50:27 +0530

    Fix an error, and remove test changes from Chip component (#544)
    * Fix error in Chip component PR
    
    * Remove test changes from chip component PR

--- app/[lang]/chapters/[slug]/[lesson]/layout.tsx
+++ app/[lang]/chapters/[slug]/[lesson]/layout.tsx
@@ -2,7 +2,6 @@
 
 import Navbar from 'components/Navbar'
 import { lessons, chapters } from 'content'
-import ProfileChip from 'components/ProfileChip'
 
 export default function Layout({ children, params }) {
   const { slug, lesson: lessonId } = params
@@ -15,11 +14,6 @@ export default function Layout({ children, params }) {
     <div className={`${theme} flex flex-col`}>
       <div className="fix-grow-issue flex min-h-screen flex-col overflow-hidden">
         <Navbar params={params} />
-        <ProfileChip
-          ranking={1}
-          balance={1.49}
-          image={'/assets/images/chapter-1-holocat.jpg'}
-        />
         {children}
       </div>
     </div>
