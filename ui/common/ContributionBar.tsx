8e7c6b7f01c382452d319b01bfd57255fbb3c774
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-07-25 16:17:37 +0200

    refactor: contribution bar

--- ui/common/ContributionBar.tsx
+++ ui/common/ContributionBar.tsx
@@ -3,160 +3,134 @@
 import { useEffect, useState, useRef } from 'react'
 import Image from 'next/image'
 
-const TRANSITION = `transform 300ms linear`
-
 export default function ContributionBar({ total, protagonists, antagonists }) {
-  const elementRef = useRef(null)
-  const [protagonistOffsets, setProtagonistOffsets] = useState(
+  const elementRef = useRef<any>()
+  const [protagonistState, setProtagonistState] = useState(
     protagonists.map(() => ({
-      offset: 0,
-      translation: 0,
+      offsetBar: 0,
+      offsetAvatar: 0,
     }))
   )
-  const [antagonistOffsets, setAntagonistOffsets] = useState(
+  const [antagonistState, setAntagonistState] = useState(
     antagonists.map(() => ({
-      offset: 0,
-      translation: 0,
+      offsetBar: 0,
+      offsetAvatar: 0,
     }))
   )
 
-  const updateAvatarPositions = () => {
-    if (!elementRef.current) {
+  const updateProtagonists = () => {
+    const elRef = elementRef.current
+    if (!elRef) {
       return
     }
 
-    const elRef = elementRef.current as HTMLElement
-    const elementWidth = elRef.getBoundingClientRect().width
+    const rect = (elRef as HTMLElement).getBoundingClientRect()
 
-    const calculateOffset = (value: number, index: number, flip?: boolean) => {
-      const scale = value / total
-      const totalWidth = Math.ceil(elementWidth * scale)
-      const result = Math.ceil(totalWidth / 2 - 13)
-      const positionOffset = totalWidth * index
-      return flip ? result * -1 : result + positionOffset
-    }
-
-    const calculateTranslation = (
-      value: number,
-      index: number,
-      flip?: boolean
-    ) => {
-      const scale = value / total
-      const totalWidth = Math.ceil(elementWidth * scale)
-      const positionOffset = totalWidth * index
-      return flip ? -positionOffset : positionOffset
-    }
-
-    setProtagonistOffsets(
-      protagonists.map((player, i) => ({
-        offset: calculateOffset(player.value, i),
-        translation: calculateTranslation(player.value, i),
-      }))
+    setProtagonistState(
+      protagonists.map((player, i) => {
+        const cumulativeValue = protagonists.slice(0, i).reduce((acc, p) => {
+          const s = p.value / total
+          return acc + rect.width * s
+        }, 0)
+        const scale = player.value / total
+        const playerOffset = rect.width * scale
+        const offsetBar = -rect.width + playerOffset + cumulativeValue
+        const offsetAvatar = cumulativeValue + playerOffset / 2 - 13
+        return { offsetBar, offsetAvatar }
+      })
     )
+  }
 
-    setAntagonistOffsets(
-      antagonists.map((player, i) => ({
-        offset: calculateOffset(player.value, i, true),
-        translation: calculateTranslation(player.value, i, true),
-      }))
+  const updateAntagonists = () => {
+    const elRef = elementRef.current
+    if (!elRef) {
+      return
+    }
+
+    const rect = (elRef as HTMLElement).getBoundingClientRect()
+
+    setAntagonistState(
+      antagonists.map((player, i) => {
+        const cumulativeValue = antagonists.slice(0, i).reduce((acc, p) => {
+          const s = p.value / total
+          return acc + rect.width * s
+        }, 0)
+        const scale = player.value / total
+        const playerOffset = rect.width * scale
+        const offsetBar = rect.width - playerOffset - cumulativeValue
+        const offsetAvatar = -cumulativeValue - playerOffset / 2 + 13
+        return { offsetBar, offsetAvatar }
+      })
     )
   }
 
   useEffect(() => {
-    updateAvatarPositions()
-  }, [protagonists, antagonists, elementRef])
+    updateProtagonists()
+  }, [elementRef, protagonists])
 
   useEffect(() => {
-    window.addEventListener('resize', updateAvatarPositions)
-    return () => window.removeEventListener('resize', updateAvatarPositions)
-  }, [])
+    updateAntagonists()
+  }, [elementRef, antagonists])
 
   return (
     <div
-      className="relative h-[30px] w-full overflow-hidden rounded-[5px] bg-black/20"
+      className="relative h-[30px] overflow-hidden rounded bg-black/20"
       ref={elementRef}
     >
-      <div className="pointer-events-none absolute z-50 flex h-full w-1/2 border-r-2 border-dotted border-white/50"></div>
-      <div className="relative flex h-full w-full">
-        {protagonists.map((player, i) => {
-          const scale = player.value / total
+      <div className="h-full w-full overflow-hidden">
+        {protagonists.map((_, i) => {
+          const idx = protagonists.length - 1 - i
+          const player = protagonists[idx]
+          const offsetBar = protagonistState[idx].offsetBar
+          const offsetAvatar = protagonistState[idx].offsetAvatar
 
           return (
             <div key={i}>
               <div
-                className="absolute h-full w-full"
+                className="absolute left-0 top-0 z-10 w-[26px] overflow-hidden rounded-full bg-white p-[2px]"
                 style={{
-                  transform: `translate3d(${protagonistOffsets[i].translation}px, 0, 0)`,
-                  transition: TRANSITION,
+                  transform: `translate3d(${offsetAvatar}px, 2px, 0)`,
+                  transition: 'transform 300ms linear',
                 }}
               >
-                <div
-                  key={i}
-                  className="flex h-full w-full origin-left items-center justify-center"
-                  style={{
-                    transform: `scaleX(${scale})`,
-                    transition: TRANSITION,
-                    background: player.color,
-                  }}
-                />
+                <Image src={player.avatar} alt="" width={26} height={26} />
               </div>
               <div
-                className="bg-red-500 absolute left-0 top-0 aspect-square h-[26px] overflow-hidden rounded-full border-2 border-white"
+                className="absolute left-0 top-0 h-full w-full rounded"
                 style={{
-                  transform: `translate3d(${protagonistOffsets[i].offset}px, 2px, 0px)`,
-                  transition: TRANSITION,
+                  transform: `translate3d(${offsetBar}px, 0, 0)`,
+                  transition: 'transform 300ms linear',
+                  backgroundColor: player.color,
                 }}
-              >
-                <Image
-                  width={26}
-                  height={26}
-                  alt={player.username}
-                  src={player.avatar}
-                  className="h-full w-full object-cover"
-                />
-              </div>
+              />
             </div>
           )
         })}
-
-        {antagonists.map((player, i) => {
-          const scale = player.value / total
+        {antagonists.map((_, i) => {
+          const idx = antagonists.length - 1 - i
+          const player = antagonists[idx]
+          const offsetBar = antagonistState[idx].offsetBar
+          const offsetAvatar = antagonistState[idx].offsetAvatar
 
           return (
             <div key={i}>
               <div
-                key={i}
-                className="absolute h-full w-full"
+                className="absolute right-0 top-0 z-10 w-[26px] overflow-hidden rounded-full bg-white p-[2px]"
                 style={{
-                  transform: `translate3d(${antagonistOffsets[i].translation}px, 0, 0)`,
-                  transition: TRANSITION,
+                  transform: `translate3d(${offsetAvatar}px, 2px, 0)`,
+                  transition: 'transform 300ms linear',
                 }}
               >
-                <div
-                  key={i}
-                  className="flex h-full w-full origin-right items-center justify-center"
-                  style={{
-                    transform: `scaleX(${scale})`,
-                    transition: TRANSITION,
-                    background: player.color,
-                  }}
-                />
-                <div
-                  className="bg-red-500 absolute right-0 top-0 aspect-square h-[26px] overflow-hidden rounded-full border-2 border-white"
-                  style={{
-                    transform: `translate3d(${antagonistOffsets[i].offset}px, 2px, 0px)`,
-                    transition: TRANSITION,
-                  }}
-                >
-                  <Image
-                    width={26}
-                    height={26}
-                    alt={player.username}
-                    src={player.avatar}
-                    className="h-full w-full object-cover"
-                  />
-                </div>
+                <Image src={player.avatar} alt="" width={26} height={26} />
               </div>
+              <div
+                className="absolute right-0 top-0 h-full w-full rounded"
+                style={{
+                  transform: `translate3d(${offsetBar}px, 0, 0)`,
+                  transition: 'transform 300ms linear',
+                  backgroundColor: player.color,
+                }}
+              />
             </div>
           )
         })}
