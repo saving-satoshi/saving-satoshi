e40f1ac6bc33bbe2103dda61654e9f9b067ff97c
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-08-23 23:40:35 +0200

    chore: use larger twitter card
    Co-authored-by: Ben Allen <108441023+benalleng@users.noreply.github.com>

--- config/metadata/twitter.ts
+++ config/metadata/twitter.ts
@@ -1,6 +1,6 @@
 const twitter = {
   twitter: {
-    card: 'summary',
+    card: 'summary_large_image',
     title:
       'Explore the mysteries of Satoshi and learn about bitcoin along the way.',
     images: ['/assets/previews/main-image-preview.jpg'],
