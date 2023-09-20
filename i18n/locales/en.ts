f8fe1f427909a3bee3dbf1fc073015d1e69c5d77
Ben Allen <108441023+benalleng@users.noreply.github.com>
2023-09-20 00:27:02 -0400

    feat: add tenXhint (#659)

--- i18n/locales/en.ts
+++ i18n/locales/en.ts
@@ -372,6 +372,7 @@ const translations = {
       progress_bar_three: 'Transactions confirmed',
       progress_bar_four: 'Bitcoin earned',
       button_hash: '10x hash power, please',
+      ten_x_hint: 'Tap the 10x button to speed up the hashing!',
     },
     outro_one: {
       title: 'Outro',
@@ -387,19 +388,19 @@ const translations = {
   chapter_three: {
     title: 'The 51% Attack',
     paragraph_one:
-        'You enter the address of a block explorer blocks are back to ten minutes intervals. Somehow, holocat sleeps through the noise of the machines.',
-      paragraph_two:
-        '—However, something is off. The blocks are empty, and transactions aren’t processing. Did you make a mistake? Could this be a coincidence? Another message pops up on the computer screen, waking holocat. It’s not a coincidence.',
+      'You enter the address of a block explorer blocks are back to ten minutes intervals. Somehow, holocat sleeps through the noise of the machines.',
+    paragraph_two:
+      '—However, something is off. The blocks are empty, and transactions aren’t processing. Did you make a mistake? Could this be a coincidence? Another message pops up on the computer screen, waking holocat. It’s not a coincidence.',
     intro_one: {
       title: 'Hashing it out',
       paragraph_one:
         '—SATOSHI NAKAMOTO: “Hey, you! Yeah, you! Remember me? Bitcoin is under a 51% attack! Vanderpoole used a virus to bend existing mining pools to his goals. He’s using them to mine empty blocks to hold the Bitcoin ecosystem hostage and force people to support increasing bitcoin’s supply.” – Satoshi Nakamoto',
-    paragraph_two:
+      paragraph_two:
         '—The old computer coughs a little, then produces a wad of data in something called a “spreadsheet”: contact info for some of the largest bitcoin pool operators and a patch to the virus that is letting BitRey control them. Send the file to them so they can regain control of their machines and join your fight against BitRey.',
       paragraph_three:
         'HOLOCAT: “We’ve got more work to do. Well, you do. I’m gonna run through walls and scare mice.”',
     },
-    
+
     solo_one: {
       title: 'You vs. Bitrey',
       step_zero_heading: 'Let’s give it a try',
