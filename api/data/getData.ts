b541ea121d177afa39c8914d1a4d13b0378145f7
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-09-25 11:30:57 +0200

    fix: data endpoints

--- api/data/getData.ts
+++ api/data/getData.ts
@@ -3,7 +3,7 @@ import { get } from 'utils'
 export default async function getData(lessonId) {
   try {
     const res = await get({
-      url: `/v1/data/load/${lessonId}`,
+      url: `/v1/data/${lessonId}`,
       includeToken: true,
     })
 
