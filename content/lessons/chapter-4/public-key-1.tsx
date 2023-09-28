2d0b318366cd9662efb66443d7d84534a63ed9ca
benalleng <benalleng@gmail.com>
2023-09-27 22:16:09 -0400

    sizing

--- content/lessons/chapter-4/public-key-1.tsx
+++ content/lessons/chapter-4/public-key-1.tsx
@@ -29,22 +29,24 @@ export default function PublicKey1({ lang }) {
     <TextImage
       lang={lang}
       imageSrc={
-        <div className="mx-auto my-14 h-[300px] w-[300px] border border-white/50 lg:my-auto xl:h-[600px] xl:w-[600px]">
-          <Lottie
-            options={options}
-            height={isSmallScreen ? 300 : 600}
-            width={isSmallScreen ? 200 : 400}
-          />
-          <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-          <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-          <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-          <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-          <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-          <hr className="relative left-[-100px] top-[-406px] w-[300px] rotate-90 opacity-25 xl:left-[-200px] xl:top-[-806px] xl:w-[600px]" />
-          <hr className="relative left-[-50px] top-[-407px] w-[300px] rotate-90 opacity-25 xl:left-[-100px] xl:top-[-807px] xl:w-[600px]" />
-          <hr className="relative left-0 top-[-408px] w-[300px] rotate-90 opacity-25 xl:top-[-808px] xl:w-[600px]" />
-          <hr className="relative left-[50px] top-[-409px] w-[300px] rotate-90 opacity-25 xl:left-[100px] xl:top-[-809px] xl:w-[600px]" />
-          <hr className="relative left-[100px] top-[-410px] w-[300px] rotate-90 opacity-25 xl:left-[200px] xl:top-[-810px] xl:w-[600px]" />
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="h-[300px] w-[300px] border border-white/50 xl:h-[600px] xl:w-[600px]">
+            <Lottie
+              options={options}
+              height={isSmallScreen ? 300 : 600}
+              width={isSmallScreen ? 200 : 400}
+            />
+            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative left-[-100px] top-[-406px] w-[300px] rotate-90 opacity-25 xl:left-[-200px] xl:top-[-806px] xl:w-[600px]" />
+            <hr className="relative left-[-50px] top-[-407px] w-[300px] rotate-90 opacity-25 xl:left-[-100px] xl:top-[-807px] xl:w-[600px]" />
+            <hr className="relative left-0 top-[-408px] w-[300px] rotate-90 opacity-25 xl:top-[-808px] xl:w-[600px]" />
+            <hr className="relative left-[50px] top-[-409px] w-[300px] rotate-90 opacity-25 xl:left-[100px] xl:top-[-809px] xl:w-[600px]" />
+            <hr className="relative left-[100px] top-[-410px] w-[300px] rotate-90 opacity-25 xl:left-[200px] xl:top-[-810px] xl:w-[600px]" />
+          </div>
         </div>
       }
       imageAlt={metadata.title}
