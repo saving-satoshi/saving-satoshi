479a6b479f7bb0c368dd0ed225360a5c125120e1
benalleng <benalleng@gmail.com>
2023-10-02 13:22:37 -0400

    feat: more animation

--- content/lessons/chapter-4/public-key-2.tsx
+++ content/lessons/chapter-4/public-key-2.tsx
@@ -4,6 +4,11 @@ import { useMediaQuery, useTranslations } from 'hooks'
 import { TextImage } from 'ui'
 import Lottie from 'react-lottie'
 import Animation1 from 'public/assets/animations/Animation1.json'
+import focusellipse from 'public/assets/images/focus-ellipse.png'
+import Circle from 'public/assets/images/circle.svg'
+import { useEffect, useState } from 'react'
+import { sleep } from 'utils'
+import Image from 'next/image'
 
 export const metadata = {
   title: 'chapter_four.public_key_two.title',
@@ -13,40 +18,106 @@ export const metadata = {
 
 export default function PublicKey1({ lang }) {
   const t = useTranslations(lang)
+  const [playLottie, setPlayLottie] = useState<boolean>(false)
+  const [lottieScreen, setLottieScreen] = useState<number[]>([600, 400])
+  const [hiddenPoint, setHiddenPoint] = useState('hidden')
+  const [hiddenFocusOne, setHiddenFocusOne] = useState('hidden')
+  const [hiddenFocusTwo, setHiddenFocusTwo] = useState('hidden')
+  const [hiddenPointText, setHiddenPointText] = useState('hidden')
 
   const isSmallScreen = useMediaQuery({ width: 1279 })
 
   const options = {
     loop: false,
-    autoplay: true,
+    autoplay: playLottie,
     animationData: Animation1,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
   }
 
+  const playAnimation = async () => {
+    sleep(2000)
+    setPlayLottie(true)
+    if (isSmallScreen) {
+      setLottieScreen([300, 200])
+    } else {
+      setLottieScreen([600, 400])
+    }
+    await sleep(3000)
+    setHiddenPoint('fade-in')
+    await sleep(1000)
+    setHiddenFocusOne('')
+    await sleep(1000)
+    setHiddenFocusTwo('')
+    await sleep(1000)
+    setHiddenPointText('')
+  }
+
+  useEffect(() => {
+    playAnimation()
+  }, [isSmallScreen])
+
   return (
     <TextImage
       lang={lang}
       imageSrc={
-        <div className="flex h-full w-full items-center justify-center">
-          <div className="h-[300px] w-[300px] border border-white/50 xl:h-[600px] xl:w-[600px]">
-            <Lottie
-              options={options}
-              height={isSmallScreen ? 300 : 600}
-              width={isSmallScreen ? 200 : 400}
-            />
-            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-            <hr className="relative top-[-300px] mt-[50px] w-[300px] opacity-25 xl:top-[-600px] xl:mt-[100px] xl:w-[600px]" />
-            <hr className="relative left-[-100px] top-[-406px] w-[300px] rotate-90 opacity-25 xl:left-[-200px] xl:top-[-806px] xl:w-[600px]" />
-            <hr className="relative left-[-50px] top-[-407px] w-[300px] rotate-90 opacity-25 xl:left-[-100px] xl:top-[-807px] xl:w-[600px]" />
-            <hr className="relative left-0 top-[-408px] w-[300px] rotate-90 opacity-25 xl:top-[-808px] xl:w-[600px]" />
-            <hr className="relative left-[50px] top-[-409px] w-[300px] rotate-90 opacity-25 xl:left-[100px] xl:top-[-809px] xl:w-[600px]" />
-            <hr className="relative left-[100px] top-[-410px] w-[300px] rotate-90 opacity-25 xl:left-[200px] xl:top-[-810px] xl:w-[600px]" />
+        <div className="flex h-full w-full flex-col items-center justify-center gap-7">
+          <div className="mt-8 h-[300px] w-[300px] border border-white/50 xl:h-[600px] xl:w-[600px]">
+            <hr className="relative mt-[50px] w-[300px] opacity-25 xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative mt-[50px] w-[300px] opacity-25 xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative mt-[50px] w-[300px] opacity-25 xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative mt-[50px] w-[300px] opacity-25 xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative mt-[50px] w-[300px] opacity-25 xl:mt-[100px] xl:w-[600px]" />
+            <hr className="relative left-[-100px] top-[-106px] w-[300px] rotate-90 opacity-25 xl:left-[-200px] xl:top-[-206px] xl:w-[600px]" />
+            <hr className="relative left-[-50px] top-[-107px] w-[300px] rotate-90 opacity-25 xl:left-[-100px] xl:top-[-207px] xl:w-[600px]" />
+            <hr className="relative left-0 top-[-108px] w-[300px] rotate-90 opacity-25 xl:top-[-208px] xl:w-[600px]" />
+            <hr className="relative left-[50px] top-[-109px] w-[300px] rotate-90 opacity-25 xl:left-[100px] xl:top-[-209px] xl:w-[600px]" />
+            <hr className="relative left-[100px] top-[-110px] w-[300px] rotate-90 opacity-25 xl:left-[200px] xl:top-[-210px] xl:w-[600px]" />
+            <div className="relative left-[25px] top-[-261px] xl:left-[50] xl:top-[-511px]">
+              <Lottie
+                options={options}
+                height={lottieScreen[0]}
+                width={lottieScreen[1]}
+              />
+              <div
+                className={`${hiddenPoint} relative left-[-97px] top-[-121px] flex items-center justify-center xl:left-[-196px] xl:top-[-246px]`}
+              >
+                <div className="absolute">
+                  <Circle />
+                </div>
+                <Image
+                  src={focusellipse}
+                  alt={'X'}
+                  height={30}
+                  width={30}
+                  className="animate-focus-ping absolute object-center"
+                />
+                <Image
+                  src={focusellipse}
+                  alt={'X'}
+                  height={30}
+                  width={30}
+                  className={`${hiddenFocusOne} animate-focus-ping absolute object-center`}
+                />
+                <Image
+                  src={focusellipse}
+                  alt={'X'}
+                  height={30}
+                  width={30}
+                  className={`${hiddenFocusTwo} animate-focus-ping absolute object-center`}
+                />
+              </div>
+            </div>
+            <span
+              className={`${hiddenPointText} fade-in relative left-[25px] top-[-300px] font-space-mono text-xl text-white xl:left-[50px] xl:top-[-585px] xl:text-3xl`}
+            >
+              Generator point G
+            </span>
           </div>
+          <span className="font-space-mono text-xl text-white xl:text-3xl">
+            y^2 = x^2 + 7
+          </span>
         </div>
       }
       imageAlt={metadata.title}
