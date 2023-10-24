b3f4c85ea611d647856db7a544e426c88102ff7f
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-10-25 00:03:29 +0100

    fix: used part-1 challenged uncompressed publicKey and saved part-2 data

--- content/lessons/chapter-4/public-key-4.tsx
+++ content/lessons/chapter-4/public-key-4.tsx
@@ -4,112 +4,163 @@ import { ScriptingChallenge, LessonInfo, CodeExample } from 'ui'
 import { EditorConfig } from 'types'
 import { useTranslations } from 'hooks'
 import { Text } from 'ui'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getLessonKey } from 'lib/progress'
+import { getData } from 'api/data'
 
 export const metadata = {
   title: 'chapter_four.public_key_four.title',
   key: 'CH4PKY4',
 }
-const javascript = {
-  program: `console.log("KILL")`,
-  defaultFunction: {
-    name: 'compressPublicKey',
-    args: ['publicKey'],
-  },
-  defaultCode: `
-  function compress_publickey(publickey) {
-    // Determine if the y coordinate is even or odd and prepend the
-    // corresponding header byte to the x coordinate.
-    // Return 33-byte Buffer
-    const header_byte = {
-      'y_is_even': Buffer.from([2]),
-      'y_is_odd':  Buffer.from([3])
-    };
-    const x_bytes = publickey.x.encode('UTF-8');
-    if ((publickey.y.val & 1n) === 0n)
-      return Buffer.concat([header_byte['y_is_even'], x_bytes]);
-    else
-      return Buffer.concat([header_byte['y_is_odd'], x_bytes]);
+
+const javascriptMatch = (match: string) => {
+  if (match === '\\"') {
+    return ''
+  } else if (match === ',' || match === '}') {
+    return 'n,'
   }
-
-  console.log(compress_publickey({"x":"0x2241aafa8bd7a36a669158f4c71378dd4e4f9aa3239f354c29528afc16965bb9","y":"0xf00415d8c4a478b2b84d3a4b9c3509c583b0e8fff6c03f9705d55612643280a5"}))
-`,
-  validate: async (answer) => {
-    console.log(answer)
-    return [false, '']
-  },
-  constraints: [
-    {
-      range: [1, 1, 18, 1],
-      allowMultiline: true,
-    },
-  ],
-}
-
-const python = {
-  program: `print("KILL")`,
-  defaultFunction: {
-    name: 'compress_publickey',
-    args: ['public'],
-  },
-  defaultCode: `# Import ECDSA library.
-  
-  def compress_publickey(publickey):
-      # Determine if the y coordinate is even or odd and prepend
-      # the corresponding header byte to the x coordinate.
-      # Return 33-byte array
-      header_byte = {
-          "y_is_even": bytes([2]),
-          "y_is_odd":  bytes([3])
-      }
-`,
-  validate: async (answer) => {
-    console.log(answer)
-    return [false, '']
-  },
-  constraints: [
-    {
-      range: [1, 1, 11, 1],
-      allowMultiline: true,
-    },
-  ],
-}
-
-const config: EditorConfig = {
-  defaultLanguage: 'javascript',
-  languages: {
-    javascript,
-    python,
-  },
+  return match // return the match unchanged if no replacements are made
 }
 
 export default function PublicKey4({ lang }) {
-  const t = useTranslations(lang)
+  const [prevData, setPrevData] = useState({ lessonId: '', data: '' })
+  const newJavascriptData = prevData.data.replace(
+    /(\\"|[,\}])/g,
+    javascriptMatch
+  )
 
+  const newPythonData = prevData.data
+    .replace(/\\"[x]\\"/g, "'x'")
+    .replace(/\\"[y]\\"/g, "'y'")
+    .replace(/\\"/g, '')
+
+  const javascript = {
+    program: `console.log("KILL")`,
+    defaultFunction: {
+      name: 'compressPublicKey',
+      args: ['publicKey'],
+    },
+    defaultCode: `${
+      prevData?.data &&
+      'const publicKey = ' + JSON.parse(newJavascriptData) + '}'
+    }
+
+function compressPublicKey(publickey) {
+  // Determine if the y coordinate is even or odd and prepend the
+  // corresponding header byte to the x coordinate.
+  // Return 33-byte Buffer
+  const header_byte = {
+    'y_is_even': Buffer.from([2]),
+    'y_is_odd':  Buffer.from([3])
+  };
+
+}
+// To submit your answer, log it to the terminal using console.log().
+
+`,
+    validate: async (answer) => {
+      const pattern = /^0x[0-9a-f]{64}$/i
+      if (answer.length !== 66) {
+        if (answer.match(pattern)) {
+          return [true, undefined]
+        }
+        return [false, "Length isn't valid"]
+      }
+      return [true, undefined]
+    },
+    constraints: [
+      {
+        range: [11, 1, 15, 1],
+        allowMultiline: true,
+      },
+    ],
+  }
+
+  const python = {
+    program: `print("KILL")`,
+    defaultFunction: {
+      name: 'compress_publickey',
+      args: ['public'],
+    },
+    defaultCode: `${
+      prevData?.data && 'publicKeyFromPart1 = ' + JSON.parse(newPythonData)
+    }
+  
+def compress_publickey(publickey):
+    # Determine if the y coordinate is even or odd and prepend
+    # the corresponding header byte to the x coordinate.
+    # Return 33-byte array
+    header_byte = {
+          "y_is_even": bytes([2]),
+          "y_is_odd":  bytes([3])
+    }
+# To submit your answer, print it to the terminal using print().
+
+`,
+    validate: async (answer) => {
+      const pattern = /^0x[0-9a-f]{64}$/i
+      if (answer.length !== 66) {
+        if (answer.match(pattern)) {
+          return [true, undefined]
+        }
+        return [false, "Length isn't valid"]
+      }
+      return [true, undefined]
+    },
+    constraints: [
+      {
+        range: [12, 1, 13, 1],
+        allowMultiline: true,
+      },
+    ],
+  }
+
+  const config: EditorConfig = {
+    defaultLanguage: 'javascript',
+    languages: {
+      javascript,
+      python,
+    },
+  }
+  const t = useTranslations(lang)
+  const [isLoading, setIsLoading] = useState(true)
   const [language, setLanguage] = useState(config.defaultLanguage)
 
+  const getPrevLessonData = async () => {
+    setPrevData(await getData('CH4PKY3'))
+  }
+
+  useEffect(() => {
+    getPrevLessonData().finally(() => setIsLoading(false))
+  }, [])
   const handleSelectLanguage = (language: string) => {
     setLanguage(language)
   }
 
   return (
-    <ScriptingChallenge
-      lang={lang}
-      config={config}
-      lessonKey={getLessonKey('chapter-2', 'scripting-2')}
-      successMessage={t('chapter_four.public_key_four.success')}
-      onSelectLanguage={handleSelectLanguage}
-    >
-      <LessonInfo>
-        <Text className="font-nunito text-xl text-white">
-          {t('chapter_four.public_key_four.paragraph_one')}
-        </Text>
-        <CodeExample className="mt-4" code={`y^2 = x^3 + 7`} language="shell" />
-        <Text className="mt-4 font-nunito text-xl text-white">
-          {t(`chapter_four.public_key_four.paragraph_two`)}
-        </Text>
-      </LessonInfo>
-    </ScriptingChallenge>
+    !isLoading && (
+      <ScriptingChallenge
+        lang={lang}
+        config={config}
+        lessonKey={getLessonKey('chapter-2', 'scripting-2')}
+        saveData
+        successMessage={t('chapter_four.public_key_four.success')}
+        onSelectLanguage={handleSelectLanguage}
+      >
+        <LessonInfo>
+          <Text className="font-nunito text-xl text-white">
+            {t('chapter_four.public_key_four.paragraph_one')}
+          </Text>
+          <CodeExample
+            className="mt-4"
+            code={`y^2 = x^3 + 7`}
+            language="shell"
+          />
+          <Text className="mt-4 font-nunito text-xl text-white">
+            {t(`chapter_four.public_key_four.paragraph_two`)}
+          </Text>
+        </LessonInfo>
+      </ScriptingChallenge>
+    )
   )
 }
