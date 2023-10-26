a4bede6ceb0d0622e847073c5415deddc50b74a4
daviroo <david.borthwick@gmail.com>
2023-10-26 10:55:19 +0100

    chore: Added umami tracking

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Topbar from 'components/Topbar'
 import Hero from 'components/Hero'
 import Footer from 'components/Footer'
 import defaultMetadata from 'config/metadata'
+import Script from 'next/script'
 
 export const metadata = {
   ...defaultMetadata,
@@ -10,6 +11,12 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col">
+      <Script
+        async
+        defer
+        data-website-id="7654dd5b-5486-4070-99b0-753bed3ac21e"
+        src="https://visits.bitcoindevs.xyz/script.js"
+      />
       <div className="bg-gradient-to-b from-[#00000060] via-[#00000020] to-transparent pb-28">
         <Topbar />
       </div>
