dc4c878d575e5b197edb15ff6cf104457cd0b6f4
daviroo <david.borthwick@gmail.com>
2023-10-26 16:24:32 +0100

    chore: Only loading tracking code in production

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from 'components/Hero'
 import Footer from 'components/Footer'
 import defaultMetadata from 'config/metadata'
 import Script from 'next/script'
+import Umami from 'components/Util/Tracking'
 
 export const metadata = {
   ...defaultMetadata,
@@ -11,20 +12,11 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col">
-      {window.location.origin === 'https://savingsatoshi.com' && (
-        <Script
-          async
-          defer
-          data-website-id="7654dd5b-5486-4070-99b0-753bed3ac21e"
-          src="https://visits.bitcoindevs.xyz/script.js"
-        />
-      )}
+      <Umami />{' '}
       <div className="bg-gradient-to-b from-[#00000060] via-[#00000020] to-transparent pb-28">
         <Topbar />
       </div>
-
       <Hero />
-
       <Footer className="bg-back" />
     </div>
   )
