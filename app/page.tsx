b9479cda167062c2dd33097fb03fa8e16cbfacb3
daviroo <david.borthwick@gmail.com>
2023-10-26 15:20:33 +0100

    chore: Only loading tracking in production

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,14 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col">
-      <Script
-        async
-        defer
-        data-website-id="7654dd5b-5486-4070-99b0-753bed3ac21e"
-        src="https://visits.bitcoindevs.xyz/script.js"
-      />
+      {window.location.origin === 'https://savingsatoshi.com' && (
+        <Script
+          async
+          defer
+          data-website-id="7654dd5b-5486-4070-99b0-753bed3ac21e"
+          src="https://visits.bitcoindevs.xyz/script.js"
+        />
+      )}
       <div className="bg-gradient-to-b from-[#00000060] via-[#00000020] to-transparent pb-28">
         <Topbar />
       </div>
