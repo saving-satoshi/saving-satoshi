247b6301937ca16c53d14b6e4f78126d3fe247c1
benalleng <benalleng@gmail.com>
2023-10-27 11:02:32 -0400

    refactor: make sure code is string in scripting challenge

--- ui/lesson/ScriptingChallenge/index.tsx
+++ ui/lesson/ScriptingChallenge/index.tsx
@@ -47,7 +47,7 @@ export default function ScriptingChallenge({
   const { saveProgress, saveProgressLocal } = useProgressContext()
   const { account } = useAuthContext()
   const [code, setCode] = useState(
-    config.languages[config.defaultLanguage].defaultCode?.[0]
+    config.languages[config.defaultLanguage].defaultCode?.toString()
   )
   const [constraints, setConstraints] = useState(
     config.languages[config.defaultLanguage].constraints
@@ -68,7 +68,7 @@ export default function ScriptingChallenge({
     if (!challengeSuccess) {
       setLanguage(value)
       onSelectLanguage(value)
-      setCode(config.languages[value].defaultCode?.[0])
+      setCode(config.languages[value].defaultCode?.toString())
       setHiddenRange(config.languages[value].hiddenRange)
       setConstraints(config.languages[value].constraints)
     }
