dc36e7d581eca57cbc14366f9f2dbf939d101d72
benalleng <benalleng@gmail.com>
2023-10-30 20:07:12 -0400

    feat: chapter-5-scaffold

---
+++ content/lessons/chapter-5/verify-signature-5.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useMediaQuery, useTranslations } from 'hooks'
+import { useEffect, useState } from 'react'
+import { Introduction, Text } from 'ui'
+
+export const metadata = {
+  title: 'chapter_four.intro_one.title',
+  image: '/assets/images/chapter-4-intro-1.jpg',
+  key: 'CH5VFS5',
+}
+
+export default function VerifySignature5({ lang }) {
+  const t = useTranslations(lang)
+  const [objectPosition, setObjectPosition] = useState<string | undefined>()
+
+  const isMediumScreen = useMediaQuery({ width: 1024 })
+
+  useEffect(() => {
+    if (isMediumScreen) {
+      setObjectPosition('object-left')
+    } else {
+      setObjectPosition('object-bottom')
+    }
+  }, [isMediumScreen])
+
+  return (
+    <Introduction lang={lang} imagePosition={objectPosition}>
+      <Text className="text-lg md:text-xl">
+        {t('chapter_four.intro_one.paragraph_one')}
+      </Text>
+      <Text className="mt-4 text-lg md:text-xl">
+        {t('chapter_four.intro_one.paragraph_two')}
+      </Text>
+      <Text className="mt-4 text-lg md:text-xl">
+        {t('chapter_four.intro_one.paragraph_three')}
+      </Text>
+    </Introduction>
+  )
+}
