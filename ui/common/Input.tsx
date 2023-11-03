79d653e295b03a975403a8b7ab2c4465324158c0
benalleng <benalleng@gmail.com>
2023-11-03 13:43:28 -0400

    feat: bg style

--- ui/common/Input.tsx
+++ ui/common/Input.tsx
@@ -134,7 +134,7 @@ export default function Input({
           spellCheck="false"
           className="absolute left-0 top-0 h-full w-full resize-none overflow-hidden break-all bg-transparent font-space-mono text-[18px] leading-[180%] tracking-[1px] text-transparent outline-none md:text-[24px] md:tracking-[5px] lg:text-[30px]"
           style={{
-            caretColor: '#6e7d92',
+            caretColor: '#ffffff59',
           }}
         />
         <p
--- ui/lesson/Prompt.tsx
+++ ui/lesson/Prompt.tsx
@@ -24,27 +24,31 @@ export default function LessonPrompt({
   const inputPattern = pattern ? pattern : /./g
 
   return (
-    <div
-      className={clsx(
-        'grow justify-center font-space-mono text-white md:w-9/12 lg:w-9/12',
-        className,
-        {
-          'hidden md:flex': !isActive,
-          flex: isActive,
-        }
-      )}
-    >
-      <div className="flex flex-col justify-center">
-        <h2 className="text-left text-xl font-bold md:text-center">{label}</h2>
+    <div className="flex w-full grow justify-center bg-[#00000026]">
+      <div
+        className={clsx(
+          'grow justify-center font-space-mono text-white md:w-9/12 lg:w-9/12',
+          className,
+          {
+            'hidden md:flex': !isActive,
+            flex: isActive,
+          }
+        )}
+      >
+        <div className="flex flex-col justify-center">
+          <h2 className="text-left text-xl font-bold md:text-center">
+            {label}
+          </h2>
 
-        <div className="w-full pt-8">
-          <Input
-            onChange={onChange}
-            answer={answer}
-            pattern={inputPattern}
-            hints={hints}
-            precedingText={precedingText}
-          />
+          <div className="w-full pt-8">
+            <Input
+              onChange={onChange}
+              answer={answer}
+              pattern={inputPattern}
+              hints={hints}
+              precedingText={precedingText}
+            />
+          </div>
         </div>
       </div>
     </div>
