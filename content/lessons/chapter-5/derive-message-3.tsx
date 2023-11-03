31af4b019039d48083162732664ea7641f92e383
benalleng <benalleng@gmail.com>
2023-11-03 10:35:56 -0400

    chore: update all titles

--- content/lessons/chapter-5/derive-message-3.tsx
+++ content/lessons/chapter-5/derive-message-3.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 import { Introduction, Text } from 'ui'
 
 export const metadata = {
-  title: 'chapter_four.intro_one.title',
+  title: 'chapter_five.derive_message_one.title',
   image: '/assets/images/chapter-4-intro-1.jpg',
   key: 'CH5DRM3',
 }
