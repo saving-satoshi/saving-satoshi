894f63274420e1410d86da9e9951147f9d44a00d
daviroo <david.borthwick@gmail.com>
2023-11-03 14:25:22 +0000

    feat: Added chapter 5 intro pages

--- content/lessons/chapter-5/intro-3.tsx
+++ content/lessons/chapter-5/intro-3.tsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from 'react'
 import { Introduction, Text } from 'ui'
 
 export const metadata = {
-  title: 'chapter_four.intro_one.title',
-  image: '/assets/images/chapter-4-intro-1.jpg',
+  title: 'chapter_five.intro_one.title',
+  image: '/assets/images/chapter-5-intro-3.jpg',
   key: 'CH5INT3',
 }
 
@@ -27,13 +27,13 @@ export default function Intro3({ lang }) {
   return (
     <Introduction lang={lang} imagePosition={objectPosition}>
       <Text className="text-lg md:text-xl">
-        {t('chapter_four.intro_one.paragraph_one')}
+        {t('chapter_five.intro_one.paragraph_one')}
       </Text>
       <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_four.intro_one.paragraph_two')}
+        {t('chapter_five.intro_one.paragraph_two')}
       </Text>
       <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_four.intro_one.paragraph_three')}
+        {t('chapter_five.intro_one.paragraph_three')}
       </Text>
     </Introduction>
   )
