fe79e9d69fd51eda0db76d1dd3a32389868b9491
benalleng <benalleng@gmail.com>
2023-11-16 09:11:40 -0500

    feat: add chapter-5 resources

---
+++ content/resources/chapter-5/derive-message.tsx
@@ -0,0 +1,315 @@
+'use client'
+
+import { useTranslations } from 'hooks'
+import { useState } from 'react'
+import { Loader } from 'shared'
+
+import MonacoEditor from '@monaco-editor/react'
+
+import { EditorConfig } from 'types'
+import { Text, ResourcePage, ToggleSwitch, CodeExample } from 'ui'
+import LanguageTabs from 'ui/lesson/ScriptingChallenge/LanguageTabs'
+import { readOnlyOptions } from 'ui/lesson/ScriptingChallenge/config'
+
+const javascript = {
+  program: `console.log("KILL")`,
+  defaultFunction: {
+    name: 'verify',
+    args: [],
+  },
+  defaultCode: `function create_tx_message() {
+  let msg = '';
+  // version:
+  msg += '01000000';
+  // number of inputs:
+  msg += '01';
+  // hash of tx being spent by input #0:
+  msg += 'c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704';
+  // index of output of tx being spent by input #0:
+  msg += '00000000';
+  // scriptPubKey of output being spent by input #0:
+  // https://blockstream.info/tx/0437cd7f8525ceed2324359c2d0ba26006d92d856a9c20fa0241106ee5a597c9?output:0&expand
+  msg += '43410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6';
+  msg += '909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656';
+  msg += 'b412a3ac';
+  // input #0 sequence:
+  msg += 'ffffffff';
+  // number of outputs:
+  msg += '02';
+  // output #0 value (10 BTC or 1,000,000,000 satoshis):
+  msg += '00ca9a3b00000000';
+  // output #0 scriptPubKey (Hal Finney's public key plus OP_CHECKSIG):
+  msg += '434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302f';
+  msg += 'a28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e';
+  msg += '6cd84cac';
+  // outut #1 value (40 BTC or 4,000,000,000 satoshis):
+  msg += '00286bee00000000';
+  // output #1 scriptPubKey (Satoshi's oen public key again, for change):
+  msg += '43410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6';
+  msg += '909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656';
+  msg += 'b412a3ac';
+  // locktime:
+  msg += '00000000';
+  // SIGHASH type
+  msg += '01000000';
+  return msg
+}`,
+  validate: async () => {
+    return [true, undefined]
+  },
+  constraints: [],
+}
+
+const python = {
+  program: `print("KILL")`,
+  defaultFunction: {
+    name: 'verify',
+    args: [],
+  },
+  defaultCode: `def createTxMessage():
+    msg = ""
+    # version:
+    msg += "01000000"
+    # number of inputs:
+    msg += "01"
+    # hash of tx being spent by input #0:
+    msg += "c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704"
+    # index of output of tx being spent by input #0:
+    msg += "00000000"
+    # scriptPubKey of output being spent by input #0:
+    # https://blockstream.info/tx/0437cd7f8525ceed2324359c2d0ba26006d92d856a9c20fa0241106ee5a597c9?output:0&expand
+    msg += "43410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6"
+    msg += "909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656"
+    msg += "b412a3ac"
+    # input #0 sequence:
+    msg += "ffffffff"
+    # number of outputs:
+    msg += "02"
+    # output #0 value (10 BTC or 1,000,000,000 satoshis):
+    msg += "00ca9a3b00000000"
+    # output #0 scriptPubKey (Hal Finney's public key plus OP_CHECKSIG):
+    msg += "434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302f"
+    msg += "a28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e"
+    msg += "6cd84cac"
+    # outut #1 value (40 BTC or 4,000,000,000 satoshis):
+    msg += "00286bee00000000"
+    # output #1 scriptPubKey (Satoshi's oen public key again, for change):
+    msg += "43410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6"
+    msg += "909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656"
+    msg += "b412a3ac"
+    # locktime:
+    msg += "00000000"
+    # SIGHASH type
+    msg += "01000000"
+    return msg
+`,
+  validate: async () => {
+    return [true, undefined]
+  },
+  constraints: [],
+}
+
+const config: EditorConfig = {
+  defaultLanguage: 'javascript',
+  languages: {
+    javascript: javascript,
+    python: python,
+  },
+}
+
+export default function DeriveMessageResources({ lang }) {
+  const t = useTranslations(lang)
+  const initialStateCode = config.languages[config.defaultLanguage].defaultCode
+  const [code, setCode] = useState<string>(initialStateCode as string)
+  const [language, setLanguage] = useState(config.defaultLanguage)
+  const [challengeOneIsToggled, setChallengeOneIsToggled] = useState(false)
+  const [challengeTwoIsToggled, setChallengeTwoIsToggled] = useState(false)
+  const [challengeThreeIsToggled, setChallengeThreeIsToggled] = useState(false)
+  const [challengeFourIsToggled, setChallengeFourIsToggled] = useState(false)
+
+  const challengeOneToggleSwitch = () => {
+    setChallengeOneIsToggled(!challengeOneIsToggled)
+  }
+
+  const challengeTwoToggleSwitch = () => {
+    setChallengeTwoIsToggled(!challengeTwoIsToggled)
+  }
+
+  const challengeThreeToggleSwitch = () => {
+    setChallengeThreeIsToggled(!challengeThreeIsToggled)
+  }
+
+  const challengeFourToggleSwitch = () => {
+    setChallengeFourIsToggled(!challengeFourIsToggled)
+  }
+
+  const handleSetLanguage = (value) => {
+    setLanguage(value)
+    setCode(config.languages[value].defaultCode as string)
+  }
+
+  const handleBeforeMount = (monaco) => {
+    monaco.editor.defineTheme('satoshi', {
+      base: readOnlyOptions.theme,
+      inherit: true,
+      rules: [],
+      colors: {
+        'editor.background': '#00000026',
+        'editor.lineHighlightBorder': '#00000000', // 4th channel is for transparency
+      },
+    })
+  }
+
+  const handleMount = (_editor, monaco) => {
+    monaco.editor.setTheme('satoshi')
+  }
+
+  return (
+    <ResourcePage
+      lang={lang}
+      readingResources={
+        <>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t('chapter_five.resources.verify_signature.eliptic_curve_heading')}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.eliptic_curve_paragraph_one'
+            )}
+          </Text>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t(
+              'chapter_five.resources.verify_signature.public_private_key_heading'
+            )}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.public_private_key_paragraph_one'
+            )}
+          </Text>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t(
+              'chapter_five.resources.verify_signature.signature_verification_heading'
+            )}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.signature_verification_paragraph_one'
+            )}
+          </Text>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t(
+              'chapter_five.resources.verify_signature.finite_field_arithmetic_heading'
+            )}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.finite_field_arithmetic_paragraph_one'
+            )}
+          </Text>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t('chapter_five.resources.verify_signature.ge_and_fe_heading')}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.ge_and_fe_paragraph_one'
+            )}
+          </Text>
+          <Text className="mt-[25px] text-xl font-bold">
+            {t(
+              'chapter_five.resources.verify_signature.modular_inverse_heading'
+            )}
+          </Text>
+          <Text>
+            {t(
+              'chapter_five.resources.verify_signature.modular_inverse_paragraph_one'
+            )}
+          </Text>
+        </>
+      }
+      codeResources={
+        <>
+          <Text>{t('help_page.solution_one')}</Text>
+          <div className="flex flex-row items-center gap-2">
+            <ToggleSwitch
+              checked={challengeOneIsToggled}
+              onChange={challengeOneToggleSwitch}
+            />
+            <Text>{t('help_page.spoilers_confirm')}</Text>
+          </div>
+          {challengeOneIsToggled && (
+            <div className="text-white">
+              <CodeExample
+                copy
+                language="bash"
+                code="304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d0901"
+              />
+            </div>
+          )}
+          <Text>{t('help_page.solution_two')}</Text>
+          <div className="flex flex-row items-center gap-2">
+            <ToggleSwitch
+              checked={challengeTwoIsToggled}
+              onChange={challengeTwoToggleSwitch}
+            />
+            <Text>{t('help_page.spoilers_confirm')}</Text>
+          </div>
+          {challengeTwoIsToggled && (
+            <div className="text-white">
+              <CodeExample
+                copy
+                language="bash"
+                code="0411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3"
+              />
+            </div>
+          )}
+          <Text>{t('help_page.solution_three')}</Text>
+          <div className="flex flex-row items-center gap-2">
+            <ToggleSwitch
+              checked={challengeThreeIsToggled}
+              onChange={challengeThreeToggleSwitch}
+            />
+            <Text>{t('help_page.spoilers_confirm')}</Text>
+          </div>
+          {challengeThreeIsToggled && (
+            <div className="text-white">
+              <CodeExample
+                copy
+                language="bash"
+                code="0100000001c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704000000004847304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d0901ffffffff0200ca9a3b00000000434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302fa28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e6cd84cac00286bee0000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac00000000"
+              />
+            </div>
+          )}
+          <Text>{t('help_page.solution_four')}</Text>
+          <div className="flex flex-row items-center gap-2">
+            <ToggleSwitch
+              checked={challengeFourIsToggled}
+              onChange={challengeFourToggleSwitch}
+            />
+            <Text>{t('help_page.spoilers_confirm')}</Text>
+          </div>
+          {challengeFourIsToggled && (
+            <div className="border border-white/25">
+              <LanguageTabs
+                languages={config.languages}
+                value={language}
+                onChange={handleSetLanguage}
+                noHide
+              />
+              <MonacoEditor
+                loading={<Loader className="h-10 w-10 text-white" />}
+                height={`calc(var(--dynamic-height) - 235px)`}
+                value={code}
+                beforeMount={handleBeforeMount}
+                onMount={handleMount}
+                language={language}
+                theme={'satoshi'}
+                options={readOnlyOptions}
+              />
+            </div>
+          )}
+        </>
+      }
+    />
+  )
+}
