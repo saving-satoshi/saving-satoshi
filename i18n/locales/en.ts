a29b49b96d382e8a870413e72f15cd8781a39a43
Ben Allen <108441023+benalleng@users.noreply.github.com>
2023-11-16 09:19:33 -0500

    DoubleInputChallenges (#746)
    * feat: add DoubleInputChallenges
    
    * feat: modify text size
    
    * feat: fix border on mobile
    
    * feat: success is permanent
    
    * feat: imports and useState fix
    
    * fix: progress typo
    
    * fix x and y vs r and s

--- i18n/locales/en.ts
+++ i18n/locales/en.ts
@@ -903,6 +903,31 @@ const translations = {
       paragraph_one:
         'We also have a signature we know Satoshi created with his own private keys, and we have his public key. Let’s learn how to verify the signature and then we can try to verify Vanderpoole’s signature.',
     },
+    verify_signature_two: {},
+    verify_signature_three: {
+      heading: 'Decode the Signature',
+      label_one: 'Paste the R value',
+      label_two: 'Paste the S value',
+      paragraph_one:
+        "Satoshi's signature is encoded in a system called DER which is a subset of ASN.1.",
+      paragraph_two:
+        'There are two 32-byte numbers we need to extract. They are referred to as R and S respectively and are each prefixed by the bytes 0220 in the DER sequence. Instead of fully decoding the DER blob, just look for the prefixes and paste the R and S values.',
+      paragraph_three:
+        "# Satoshi's signature, from the input scriptSig of the tx to Hal Finney",
+      paragraph_four:
+        'sig_der = """304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d09"""',
+    },
+    verify_signature_four: {
+      heading: 'Decode the Public Key',
+      label_one: 'Paste the x coordinate',
+      label_two: 'Paste the y coordinate',
+      paragraph_one:
+        'We learned in chapter 4 that public keys are really points in the ECDSA curve, meaning they have an x and y value. The first byte 04 means "uncompressed" (as opposed to 02 and 03 like we learned in chapter 4). Remove that first byte and the remaining data are 32-byte x and y coordinates. Copy and paste again.',
+      paragraph_two:
+        "# Satoshi's public key, from the block 9 coinbase output scriptPubKey",
+      paragraph_three:
+        'pubkey = """0411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3"""',
+    },
     verify_signature_five: {
       title: 'Verify the signature',
       heading: 'Verify the signature!',
---
+++ ui/lesson/DoubleInputChallenge.tsx
@@ -0,0 +1,139 @@
+'use client'
+
+import clsx from 'clsx'
+import { useState, useEffect, ReactElement } from 'react'
+import { ColorGroup, LessonDirection } from 'types'
+import { Lesson, LessonTabs, LessonPrompt, StatusBar } from 'ui'
+
+const tabData = [
+  {
+    id: 'info',
+    text: 'Info',
+  },
+  {
+    id: 'code',
+    text: 'Code',
+  },
+]
+
+/**
+ * @answer {string} correct answer to the challenge problem
+ * @next {string} link to next part of chapter
+ * @label {string} label on top of input field
+ * @pattern {RegEx} pattern for input field
+ * @hints {boolean} determine whether the input field displays hints for the user
+ */
+export default function DoubleInputChallenge({
+  children,
+  answerOne,
+  answerTwo,
+  labelOne,
+  labelTwo,
+  pattern,
+  hints,
+  precedingText,
+  successMessage,
+  direction = LessonDirection.Vertical,
+  inputClassNames = '',
+}: {
+  children: any
+  answerOne: string
+  answerTwo: string
+  labelOne: string
+  labelTwo: string
+  pattern?: RegExp
+  hints?: boolean
+  precedingText?: string
+  successMessage?: string
+  successElement?: ReactElement
+  successColorGroups?: ColorGroup[]
+  direction?: LessonDirection
+  inputClassNames?: string
+}) {
+  const [userInputOne, setUserInputOne] = useState('')
+  const [userInputTwo, setUserInputTwo] = useState('')
+  const [success, setSuccess] = useState<boolean | null>(null)
+  const [userHint, setUserHint] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    if (!userInputOne && !userInputTwo && !success) {
+      setSuccess(null)
+    } else if (
+      !success &&
+      answerOne.startsWith(userInputOne) &&
+      userInputOne !== answerOne
+    ) {
+      setSuccess(false)
+      setUserHint(true)
+    } else if (
+      !success &&
+      !answerOne.startsWith(userInputOne) &&
+      userInputOne !== answerOne
+    ) {
+      setSuccess(false)
+      setUserHint(false)
+    } else if (
+      !success &&
+      answerTwo.startsWith(userInputTwo) &&
+      userInputTwo !== answerTwo
+    ) {
+      setSuccess(false)
+      setUserHint(true)
+    } else if (
+      !success &&
+      !answerTwo.startsWith(userInputTwo) &&
+      userInputTwo !== answerTwo
+    ) {
+      setSuccess(false)
+      setUserHint(false)
+    } else if (userInputOne === answerOne && userInputTwo === answerTwo) {
+      setSuccess(true)
+    }
+  }, [answerOne, answerTwo, userInputOne, userInputTwo])
+
+  return (
+    <Lesson direction={direction}>
+      <LessonTabs items={tabData} classes="px-4 py-2 w-full" stretch={true} />
+
+      {children}
+
+      {direction === LessonDirection.Vertical ? (
+        <hr className="border-1 invisible h-1 w-full border-white/50 md:visible" />
+      ) : (
+        <div className="block w-1 border-r border-white/50"></div>
+      )}
+      <div
+        className={clsx('flex grow flex-col', {
+          'md:max-w-[50%]': direction === LessonDirection.Horizontal,
+        })}
+      >
+        <LessonPrompt
+          className="flex max-w-[1280px] items-center justify-center px-4 py-8 md:px-20"
+          label={labelOne}
+          answer={answerOne}
+          onChange={setUserInputOne}
+          pattern={pattern}
+          hints={hints}
+          precedingText={precedingText}
+          inputClassNames={inputClassNames}
+        />
+        <LessonPrompt
+          className="flex max-w-[1280px] items-center justify-center border-t border-white/50 px-4 py-8 md:px-20"
+          label={labelTwo}
+          answer={answerTwo}
+          onChange={setUserInputTwo}
+          pattern={pattern}
+          hints={hints}
+          precedingText={precedingText}
+          inputClassNames={inputClassNames}
+        />
+
+        <StatusBar
+          success={success}
+          hints={userHint}
+          successMessage={successMessage}
+        />
+      </div>
+    </Lesson>
+  )
+}
