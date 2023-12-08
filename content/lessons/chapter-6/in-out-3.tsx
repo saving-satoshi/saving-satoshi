757c4839314b1c2f6d548ac3ddde4984450f2124
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-12-08 21:15:33 +0100

    Chapter Six Challenge one intro pages (#787)
    * feat: challenge one intro pages
    
    * fix: rename file and content of in-outs pages
    
    * fix: swapped filename and translations
    
    * fix: renaming of files

--- content/lessons/chapter-6/in-out-3.tsx
+++ content/lessons/chapter-6/in-out-3.tsx
@@ -4,8 +4,8 @@ import { useTranslations } from 'hooks'
 import { Introduction, Text } from 'ui'
 
 export const metadata = {
-  title: 'chapter_six.in_out_one.title',
-  image: '/assets/images/chapter-4-intro-1.jpg',
+  title: 'chapter_six.in_out_three.title',
+  image: '/assets/images/chapter-6-in-out-3.jpg',
   key: 'CH6INO3',
 }
 
@@ -15,19 +15,13 @@ export default function InOut3({ lang }) {
   return (
     <Introduction lang={lang} imagePosition="object-[50%_27%]">
       <Text className="text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_one')}
+        {t('chapter_six.in_out_three.paragraph_one')}
       </Text>
       <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_two')}
+        {t('chapter_six.in_out_three.paragraph_two')}
       </Text>
       <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_three')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_four')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_five')}
+        {t('chapter_six.in_out_three.paragraph_three')}
       </Text>
     </Introduction>
   )
