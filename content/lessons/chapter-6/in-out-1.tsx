c12c0e39c8d018f4bd3fcd187fd1764c972e670c
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-12-18 11:00:01 +0100

    feat: read-only editor for bitcoin script

--- content/lessons/chapter-6/in-out-1.tsx
+++ content/lessons/chapter-6/in-out-1.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useTranslations } from 'hooks'
-import { Introduction, Text } from 'ui'
+import { Introduction, Lesson, LessonInfo, Text } from 'ui'
+import PlainEditor from 'ui/lesson/ScriptingChallenge/PlainEditor'
 
 export const metadata = {
   title: 'chapter_six.in_out_one.title',
@@ -13,22 +14,39 @@ export default function InOut1({ lang }) {
   const t = useTranslations(lang)
 
   return (
-    <Introduction lang={lang} imagePosition="object-[50%_27%]">
-      <Text className="text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_one')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_two')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_three')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_four')}
-      </Text>
-      <Text className="mt-4 text-lg md:text-xl">
-        {t('chapter_five.intro_one.paragraph_five')}
-      </Text>
-    </Introduction>
+    <PlainEditor
+      code={`$ bitcoin-cli listunspent 
+
+[  
+  { 
+    "txid": "74149a689ce95562309cf4c404ef6ca91e76b6a19ef25e9625e9c13d93fac4e1",    
+    "vout": 0,    
+    "address": "bc1qm2dr49zrgf9wc74h5c58wlm3xrnujfuf5g80hs",    
+    "label": "",    
+    "scriptPubKey": "0014da9a3a9443424aec7ab7a628777f7130e7c92789",    
+    "amount": 6.50000000,    
+    "confirmations": 341,    
+    "spendable": true,    
+    "solvable": true,    
+    "desc": "wpkh([a73804d3/0'/0'/0']02ab3d3cb82c1eb89168824b20f667224d868250dedec69177012e5a26c5221ae8)#5mf00k95",    
+    "parent_descs": [    
+    ],    
+    "safe": true  
+  }
+]
+    `}
+    >
+      <LessonInfo>
+        <Text className="text-lg md:text-xl">
+          {t('chapter_six.in_out_one.paragraph_one')}
+        </Text>
+        <Text className="mt-4 text-lg md:text-xl">
+          {t('chapter_six.in_out_one.paragraph_two')}
+        </Text>
+        <Text className="mt-4 text-lg md:text-xl">
+          {t('chapter_six.in_out_one.paragraph_three')}
+        </Text>
+      </LessonInfo>
+    </PlainEditor>
   )
 }
---
+++ ui/lesson/ScriptingChallenge/PlainEditor.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
+import { EditorConfig, LessonDirection, LessonView } from 'types'
+import { Lesson, LessonTabs, useLessonContext } from 'ui'
+import { useMediaQuery, useDynamicHeight } from 'hooks'
+import MonacoEditor from '@monaco-editor/react'
+import { Loader } from 'shared'
+
+const tabData = [
+  {
+    id: 'info',
+    text: 'Info',
+  },
+  {
+    id: 'code',
+    text: 'Code',
+  },
+  {
+    id: 'execute',
+    text: 'Execute',
+  },
+]
+
+export default function PlainEditor({
+  children,
+  code,
+}: {
+  children?: React.ReactNode
+  code: string
+}) {
+  const [hydrated, setHydrated] = useState(false)
+  const [errors, setErrors] = useState<string[]>([])
+
+  useDynamicHeight()
+  const isSmallScreen = useMediaQuery({ width: 767 })
+
+  const handleEditorValidate = (markers) => {
+    setErrors(
+      markers.filter(
+        (marker) => marker.code !== '80001' && marker.code !== '6133'
+      )
+    )
+  }
+  const { activeView } = useLessonContext()
+  const isActive = activeView === LessonView.Code
+  const headerHeight = isSmallScreen ? 63 : 100
+  const languageTabsHeight = 40
+  const statusBarHeight = 56
+  const terminalHeight = 218
+  const terminalTabsHeight = 40
+  const mobileMenuHeight = 48
+
+  const totalHeight = isSmallScreen
+    ? headerHeight + mobileMenuHeight + languageTabsHeight + statusBarHeight
+    : headerHeight +
+      languageTabsHeight +
+      statusBarHeight +
+      terminalHeight +
+      terminalTabsHeight
+
+  useEffect(() => {
+    setHydrated(true)
+  }, [])
+
+  if (!hydrated) {
+    return null
+  }
+  const handleBeforeMount = (monaco) => {
+    monaco.editor.defineTheme('satoshi', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [],
+      colors: {
+        'editor.background': '#00000000',
+        'editor.lineHighlightBorder': '#00000000', // 4th channel is for transparency
+      },
+    })
+  }
+
+  return (
+    hydrated && (
+      <Lesson
+        direction={
+          isSmallScreen ? LessonDirection.Vertical : LessonDirection.Horizontal
+        }
+      >
+        <LessonTabs items={tabData} classes="px-4 py-2 w-full" stretch={true} />
+        {children}
+
+        <div className=" grow border-white/25 md:max-w-[50vw] md:basis-1/3 md:border-l">
+          <div
+            className={clsx(
+              'relative h-full bg-[#00000026] font-mono text-sm text-white',
+              {
+                'hidden md:flex': !isActive,
+                flex: isActive,
+              }
+            )}
+          >
+            <MonacoEditor
+              loading={<Loader className="h-10 w-10 text-white" />}
+              width={isSmallScreen ? '100vw' : 'calc(100vw / 2)'}
+              height={'100%'}
+              language={'plaintext'}
+              theme={'satoshi'}
+              value={code}
+              options={{ readOnly: true, minimap: { enabled: false } }}
+              beforeMount={handleBeforeMount}
+            />
+          </div>
+        </div>
+      </Lesson>
+    )
+  )
+}
