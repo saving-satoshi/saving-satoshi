6887039f48cb3d1e4fcde9b1fb084473d061adaf
umarluqman <umarluqman.78@gmail.com>
2024-01-03 16:27:30 +0800

    fix: add descriptive a11y avatar label

--- components/Modals/SignUp.tsx
+++ components/Modals/SignUp.tsx
@@ -13,6 +13,14 @@ import { register } from 'api/auth'
 import Modal from './Modal'
 import { Text, ToggleSwitch } from 'ui'
 
+const avatars = [
+  'white space-suit',
+  'red space-suit',
+  'orange space-suit',
+  'bunny engineer',
+  'badger astronaut',
+]
+
 export default function SignupModal({ onClose, state }) {
   const lang = useLang()
   const t = useTranslations(lang)
@@ -23,7 +31,7 @@ export default function SignupModal({ onClose, state }) {
   const nextLessonKey = getNextLessonKey(progress)
 
   const [loading, setLoading] = useState<boolean>(false)
-  const [avatar, setAvatar] = useState(1)
+  const [avatar, setAvatar] = useState(avatars[0])
   const [copyAcknowledged, setCopyAcknowledged] = useState<boolean>(false)
   const [privateKey, setPrivateKey] = useState<string | undefined>(undefined)
   const [copied, setCopied] = useState(false)
@@ -85,22 +93,22 @@ export default function SignupModal({ onClose, state }) {
         </h2>
 
         <HorizontalScrollView>
-          {[1, 2, 3, 4, 5].map((i) => (
+          {avatars.map((name, index) => (
             <button
-              key={i}
+              key={name}
               className={clsx({
-                'mr-3.5 sm:mr-3': i !== 5,
+                'mr-3.5 sm:mr-3': index !== avatars.length - 1,
               })}
-              aria-label={`Select avatar ${i}`}
-              aria-pressed={avatar === i}
-              onClick={() => setAvatar(i)}
+              aria-label={`Select avatar with ${name}`}
+              aria-pressed={avatar === name}
+              onClick={() => setAvatar(name)}
             >
               <Avatar
-                avatar={`/assets/avatars/${i}.png`}
+                avatar={`/assets/avatars/${name}.png`}
                 size={80}
                 className={clsx('inline-block h-20 w-20 border-2', {
-                  'border-white': avatar === i,
-                  'border-transparent': avatar !== i,
+                  'border-white': avatar === name,
+                  'border-transparent': avatar !== name,
                 })}
               />
             </button>
