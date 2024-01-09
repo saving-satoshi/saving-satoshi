37160c5e3c15987449d8f02eef9abe0497d5865f
Ben Allen <108441023+benalleng@users.noreply.github.com>
2024-01-09 11:56:45 -0500

    feat: change chapter 3 avatars (#809)

--- content/lessons/chapter-3/coop-3.tsx
+++ content/lessons/chapter-3/coop-3.tsx
@@ -64,7 +64,7 @@ export default function Coop3({ lang }) {
     },
     {
       username: 'Mining Maniacs',
-      avatar: '/assets/avatars/2.png',
+      avatar: '/assets/avatars/mining-maniacs.png',
       hashpower: 4054,
       hashes: protagonistHash[1],
       color: '#FE5329',
@@ -72,7 +72,7 @@ export default function Coop3({ lang }) {
     },
     {
       username: 'Coin Crunchers',
-      avatar: '/assets/avatars/4.png',
+      avatar: '/assets/avatars/coin-crunchers.png',
       hashpower: 3857,
       hashes: protagonistHash[2],
       color: '#62BFB7',
@@ -80,7 +80,7 @@ export default function Coop3({ lang }) {
     },
     {
       username: 'Hash Hoppers',
-      avatar: '/assets/avatars/3.png',
+      avatar: '/assets/avatars/hash-hoppers.png',
       hashpower: 7911,
       hashes: protagonistHash[3],
       color: '#85BF09',
