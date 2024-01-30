d6ae73a01535edd414fb0e208e1bca7753004f2c
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-01-30 23:17:50 +0100

    feat: scafold chapter 7 for implementation

---
+++ content/lessons/chapter-7/index.tsx
@@ -0,0 +1,15 @@
+import * as Intro1 from './intro-1'
+import * as Intro2 from './intro-2'
+import * as Intro3 from './intro-3'
+import * as Outro1 from './outro-1'
+import * as MempoolTransaction1 from './mempool-transaction-1'
+
+const Chapter7 = {
+  'intro-1': Intro1,
+  'intro-2': Intro2,
+  'intro-3': Intro3,
+  'mempool-transaction-1': MempoolTransaction1,
+  'outro-1': Outro1,
+}
+
+export default Chapter7
