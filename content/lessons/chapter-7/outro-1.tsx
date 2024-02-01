95bb2534580a2721bcb071c72396eead9720d889
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-02-01 20:06:00 +0100

    Chapter 7 Outro 1  (#836)
    * chore: added outro image and put in the text
    
    * fix: changed theme color

--- content/lessons/chapter-7/outro-1.tsx
+++ content/lessons/chapter-7/outro-1.tsx
@@ -4,11 +4,11 @@ import { useTranslations } from 'hooks'
 import { ChapterEnd } from 'ui'
 
 export const metadata = {
-  title: 'chapter_four.outro_one.title',
-  image: '/assets/images/chapter-4-finish.jpg',
+  title: 'chapter_seven.outro_one.title',
+  image: '/assets/images/chapter-7-outro.jpg',
   theme: 'bg-transparent/20',
-  secondaryTheme: 'bg-[#472B26]',
-  gradientTheme: 'from-[#472B26] via-[rgba(71, 43, 38, 0)]',
+  secondaryTheme: 'bg-[#19202D]',
+  gradientTheme: 'from-[#19202D] via-[#19202DBF]',
   key: 'CH7OUT1',
 }
 
@@ -17,7 +17,6 @@ export default function Outro1({ lang }) {
 
   return (
     <ChapterEnd
-      className="top-[70px]"
       image={metadata.image}
       direction="left"
       lang={lang}
@@ -25,13 +24,10 @@ export default function Outro1({ lang }) {
       gradientTheme={metadata.gradientTheme}
     >
       <h1 className="text-5xl font-bold text-white">
-        {t('chapter_four.outro_one.heading')}
+        {t('chapter_seven.outro_one.heading')}
       </h1>
       <p className="mt-4 font-nunito text-2xl text-white">
-        {t('chapter_four.outro_one.paragraph_one')}
-      </p>
-      <p className="mt-4 font-nunito text-2xl text-white">
-        {t('chapter_four.outro_one.paragraph_two')}
+        {t('chapter_seven.outro_one.paragraph_one')}
       </p>
     </ChapterEnd>
   )
