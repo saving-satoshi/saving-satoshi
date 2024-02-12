b8132dfb97967570628ab03bb452ac2d3fa02ba6
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-02-12 22:40:24 +0100

    fix: lock icon flash

--- components/Navbar/NavbarDesktop/Tab.tsx
+++ components/Navbar/NavbarDesktop/Tab.tsx
@@ -62,13 +62,16 @@ export default function Tab({
         .replace('intro', chapters[slug].metadata.challenges[0].split('-')[0])
     : undefined
   const isActive = challenge.lessonId.split('-')[0] === challengeId
+  // const currentIndex =
   const isLast = index === count - 1
   const lessonHref =
     challenge.lessonId === chapters[slug].metadata.challenges[0]
       ? chapters[slug].metadata.intros[0]
       : challenge.lessonId
   const href = `${routes.chaptersUrl}/${slug}/${lessonHref}`
-
+  const currentIndex = chapters[slug].metadata.challenges.indexOf(
+    challengeId + '-1'
+  )
   return (
     <Tooltip
       id={`navbar-tab-tooltip-${index + 1}`}
@@ -101,18 +104,27 @@ export default function Tab({
         )}
       >
         {index + 1}
-        {!isUnlocked && (
-          <Icon
-            icon="lock"
-            className="absolute right-[10px] top-[10px] h-3 w-3 opacity-50"
-          />
-        )}
-        {isCompleted && (
-          <Icon
-            icon="check"
-            className="absolute right-[5px] top-[5px] h-[20px] w-[20px]"
-          />
-        )}
+        {!(currentIndex >= index) &&
+          !isCompleted &&
+          pnLessonId.split('-')[0] !== 'outro' && (
+            <Icon
+              icon="lock"
+              className="absolute right-[10px] top-[10px] h-3 w-3 opacity-50"
+            />
+          )}
+        {currentIndex > index ||
+          (isCompleted && (
+            <Icon
+              icon="check"
+              className="absolute right-[5px] top-[5px] h-[20px] w-[20px]"
+            />
+          )) ||
+          (pnLessonId.split('-')[0] === 'outro' && (
+            <Icon
+              icon="check"
+              className="absolute right-[5px] top-[5px] h-[20px] w-[20px]"
+            />
+          ))}
       </Link>
     </Tooltip>
   )
