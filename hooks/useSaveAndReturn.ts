43aa849a3e5e9ab132f971f06a993f65ef2afeb4
benalleng <benalleng@gmail.com>
2024-02-15 11:56:33 -0500

    refactor: revert to currentLessonKey from progress

--- hooks/useSaveAndReturn.ts
+++ hooks/useSaveAndReturn.ts
@@ -3,7 +3,8 @@
 import { getNextLessonKey, isLessonUnlocked, getChapterKey } from 'lib/progress'
 import { useRouter } from 'next/navigation'
 import { useProgressContext } from 'providers/ProgressProvider'
-import { useLocalizedRoutes } from 'hooks'
+import { usePathData, useLocalizedRoutes } from 'hooks'
+import { lessons } from 'content'
 import { useAuthContext } from 'providers/AuthProvider'
 
 export default function useSaveAndReturn() {
@@ -12,8 +13,13 @@ export default function useSaveAndReturn() {
   const { account } = useAuthContext()
   const { progress, saveProgress, saveProgressLocal } = useProgressContext()
 
+  const { chapterId, lessonId } = usePathData()
+  const chapterLessons = lessons?.[chapterId]
+  const lesson = chapterLessons?.[lessonId]?.metadata ?? null
+  const currentLessonKey = lesson?.key ?? 'CH1INT1'
+
   const saveAndReturn = async () => {
-    const nextLessonKey = getNextLessonKey(progress, account)
+    const nextLessonKey = getNextLessonKey(currentLessonKey, account)
     const chapterKey = getChapterKey(nextLessonKey)
 
     if (progress && !isLessonUnlocked(progress, nextLessonKey)) {
