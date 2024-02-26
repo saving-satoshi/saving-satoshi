54fb6df47799308ab7a2413b8da49ccb297ee4b8
Ben Allen <108441023+benalleng@users.noreply.github.com>
2024-02-26 09:38:36 -0500

    feat: add chapter 7 help pages (#862)

---
+++ content/resources/chapter-7/index.ts
@@ -0,0 +1,7 @@
+import * as MempoolTransactionResources from './mempool-transaction'
+
+const Chapter6 = {
+  'mempool-transaction': MempoolTransactionResources,
+}
+
+export default Chapter6
--- content/resources/chapter-7/mempool-transaction.tsx
+++ content/resources/chapter-7/mempool-transaction.tsx
@@ -17,20 +17,7 @@ const javascript = {
     name: 'findHash',
     args: ['nonce'],
   },
-  defaultCode: [
-    `function findHash(nonce) {
-  let hash = '';
-
-  // while the hash does not start with 5 zeroes we want the prgram to repeat
-  while (hash.substring(0, 5) !== '00000') {
-    // Hash the nonce using the crypto library and then increment the nonce
-    hash = crypto.createHash('sha256').update(nonce.toString()).digest('hex');
-
-    nonce++;
-  }
-  return hash
-}`,
-  ],
+  defaultCode: [`//No Javascript Solution, check back soon`],
   validate: async (answer) => {
     return [true, undefined]
   },
@@ -44,16 +31,73 @@ const python = {
     args: ['nonce'],
   },
   defaultCode: [
-    `def find_hash(nonce):
-    # Lets initialize the hash here as an empty string
-    hash = ''
+    `txs = {tx.txid: tx for tx in mempool}
 
-    # while the hash does not start with 5 zeroes we want the prgram to repeat
-    while hash[0:5] != '00000':
-        # Hash the nonce using the crypto library and then increment the nonce
-        hash = sha256(str(nonce).encode()).digest().hex()
-        nonce += 1
-    return hash`,
+    def get_ancestors(tx, depth=0):
+        """Returns: list of (ancestor, depth) tuples.
+
+        Ancestors may appear more than once in an ancestor tree, including with different depths.
+
+        Sorting by descending depth and deduping implies no descendant appears before its ancestor."""
+        ancestors = [(tx, depth)]
+        for parent in tx.parents:
+            ancestors += get_ancestors(txs[parent], depth + 1)
+        if depth > 0:
+            return ancestors
+        ret = []
+        for ancestor, _ in sorted(ancestors, key=lambda ad: ad[1], reverse=True):
+            if ancestor not in ret:
+                ret.append(ancestor)
+        return ret
+
+    for tx in mempool:
+        # Calculate ancestors for each tx
+        tx.ancestors = get_ancestors(tx, 0)
+        tx.ancestor_fee = sum([ancestor_txid.fee for ancestor_txid in tx.ancestors])
+        tx.ancestor_weight = sum([ancestor_txid.weight for ancestor_txid in tx.ancestors])
+        tx.ancestor_feerate = tx.ancestor_fee / tx.ancestor_weight
+        for anc in [a.txid for a in tx.ancestors if a.txid != tx.txid]:
+            txs[anc].descendants.append(tx.txid)
+
+    # Reorder transactions by ancestor_feerate
+    txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
+
+    # Now construct the block
+    block = []
+    block_weight = 0
+
+    def remove_from_pool(tx):
+        # print("including tx {}".format(tx.txid))
+        for desc in tx.descendants:
+            txs[desc].ancestor_fee -= tx.fee
+            txs[desc].ancestor_weight -= tx.weight
+            txs[desc].ancestor_feerate = txs[desc].ancestor_fee / txs[desc].ancestor_weight
+            txs[desc].ancestors.remove(tx)
+            # print("modifying descendant {}".format(desc))
+
+    while txs:
+        # Pop first transaction from ordered dict
+        tx = next(iter(txs.values()))
+        if block_weight + tx.ancestor_weight > MAX_BLOCK_WEIGHT:
+            # Package won't fit in block
+            txs.pop(tx.txid)
+            continue
+        # Add all unincluded package txs to block
+        # print("including package {}: {}".format(tx.txid, ','.join([a.txid for a in tx.ancestors])))
+        ancestors = [a for a in tx.ancestors]
+        for ancestor in ancestors:
+            if ancestor.txid not in txs:
+                # ancestor already included
+                print(ancestor.txid)
+                assert False
+            remove_from_pool(ancestor)
+            block.append(txs.pop(ancestor.txid).txid)
+            block_weight += ancestor.weight
+        # Resort list
+        txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
+        continue  # Break into outer loop. Restart iteration over all unincluded txs
+
+    return block`,
   ],
   validate: async (answer) => {
     return [true, undefined]
@@ -69,7 +113,7 @@ const config: EditorConfig = {
   },
 }
 
-export default function ScriptingResources({ lang }) {
+export default function MempoolTransactionResources({ lang }) {
   const t = useTranslations(lang)
 
   const [code, setCode] = useState(
@@ -106,27 +150,7 @@ export default function ScriptingResources({ lang }) {
   return (
     <ResourcePage
       lang={lang}
-      readingResources={
-        <>
-          <Text className="mt-[25px] text-xl font-bold">
-            {t('chapter_two.resources.scripting.hash_libraries_heading')}
-          </Text>
-          <Text>
-            {t('chapter_two.resources.scripting.hash_libraries_paragraph')}
-          </Text>
-          <Text className="mt-[25px] text-xl font-bold">
-            {t('chapter_two.resources.scripting.nonce_heading')}
-          </Text>
-          <Text>{t('chapter_two.resources.scripting.nonce_paragraph')}</Text>
-        </>
-      }
-      tipsResources={
-        <ul className="list-inside list-disc font-nunito text-white">
-          <li>{t('chapter_two.resources.scripting.tip_one')}</li>
-          <li>{t('chapter_two.resources.scripting.tip_two')}</li>
-          <li>{t('chapter_two.resources.scripting.tip_three')}</li>
-        </ul>
-      }
+      readingResources={<></>}
       codeResources={
         <>
           <Text>{t('help_page.solution_one')}</Text>
@@ -148,7 +172,7 @@ export default function ScriptingResources({ lang }) {
               <div className="relative grow bg-[#00000026] font-mono text-sm text-white">
                 <MonacoEditor
                   loading={<Loader className="h-10 w-10 text-white" />}
-                  height={`235px`}
+                  height={`1260px`}
                   value={code}
                   beforeMount={handleBeforeMount}
                   onMount={handleMount}
