af2c94870d44d52c268375f010a930b7e3d2c7f1
benalleng <benalleng@gmail.com>
2024-02-27 12:08:08 -0500

    add resources for JS and fix editor completion height

--- content/resources/chapter-7/mempool-transaction.tsx
+++ content/resources/chapter-7/mempool-transaction.tsx
@@ -17,7 +17,40 @@ const javascript = {
     name: 'findHash',
     args: ['nonce'],
   },
-  defaultCode: [`//No Javascript Solution, check back soon`],
+  defaultCode: [
+    `  // This block constructor opportunistically includes transactions with unconfirmed parents.
+  // If a transaction has unconfirmed parents, but those parents are all already included in the block, then it is valid for inclusion.
+  const block = [];
+  let block_weight = 0;
+  for (const tx of mempool) {
+      tx.feerate = parseFloat(tx.fee) / parseFloat(tx.weight);
+  }
+  // Construct dictionary for fast lookup
+  const txs = new Map([...mempool.map(tx => [tx.txid, tx])].sort((a, b) => b[1].feerate - a[1].feerate));
+  while (true) {
+    let added = false;
+    for (const tx of txs.values()) {
+      // Opportunistically include txs with unconfirmed parents if their parents
+      if (tx.parents.some(parent_tx => txs.has(parent_tx))) {
+        continue;
+      }
+      if (block_weight + tx.weight > MAX_BLOCK_WEIGHT) {
+        // Transaction won't fit in block
+        continue;
+      }
+      block.push(txs.get(tx.txid).txid);
+      block_weight += tx.weight;
+      txs.delete(tx.txid);
+      added = true;
+      break;
+    }
+    if (!added) {
+      // Couldn't add any more transactions.
+      break;
+    }
+  }
+  return block;`,
+  ],
   validate: async (answer) => {
     return [true, undefined]
   },
@@ -31,72 +64,31 @@ const python = {
     args: ['nonce'],
   },
   defaultCode: [
-    `txs = {tx.txid: tx for tx in mempool}
+    `    """This block constructor opportunistically includes transactions with
+    unconfirmed parents.
 
-    def get_ancestors(tx, depth=0):
-        """Returns: list of (ancestor, depth) tuples.
-
-        Ancestors may appear more than once in an ancestor tree, including with different depths.
-
-        Sorting by descending depth and deduping implies no descendant appears before its ancestor."""
-        ancestors = [(tx, depth)]
-        for parent in tx.parents:
-            ancestors += get_ancestors(txs[parent], depth + 1)
-        if depth > 0:
-            return ancestors
-        ret = []
-        for ancestor, _ in sorted(ancestors, key=lambda ad: ad[1], reverse=True):
-            if ancestor not in ret:
-                ret.append(ancestor)
-        return ret
-
-    for tx in mempool:
-        # Calculate ancestors for each tx
-        tx.ancestors = get_ancestors(tx, 0)
-        tx.ancestor_fee = sum([ancestor_txid.fee for ancestor_txid in tx.ancestors])
-        tx.ancestor_weight = sum([ancestor_txid.weight for ancestor_txid in tx.ancestors])
-        tx.ancestor_feerate = tx.ancestor_fee / tx.ancestor_weight
-        for anc in [a.txid for a in tx.ancestors if a.txid != tx.txid]:
-            txs[anc].descendants.append(tx.txid)
-
-    # Reorder transactions by ancestor_feerate
-    txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
-
-    # Now construct the block
+    If a transaction has unconfirmed parents, but those parents are all already
+    included in the block, then it is valid for inclusion."""
     block = []
     block_weight = 0
-
-    def remove_from_pool(tx):
-        # print("including tx {}".format(tx.txid))
-        for desc in tx.descendants:
-            txs[desc].ancestor_fee -= tx.fee
-            txs[desc].ancestor_weight -= tx.weight
-            txs[desc].ancestor_feerate = txs[desc].ancestor_fee / txs[desc].ancestor_weight
-            txs[desc].ancestors.remove(tx)
-            # print("modifying descendant {}".format(desc))
-
-    while txs:
-        # Pop first transaction from ordered dict
-        tx = next(iter(txs.values()))
-        if block_weight + tx.ancestor_weight > MAX_BLOCK_WEIGHT:
-            # Package won't fit in block
-            txs.pop(tx.txid)
-            continue
-        # Add all unincluded package txs to block
-        # print("including package {}: {}".format(tx.txid, ','.join([a.txid for a in tx.ancestors])))
-        ancestors = [a for a in tx.ancestors]
-        for ancestor in ancestors:
-            if ancestor.txid not in txs:
-                # ancestor already included
-                print(ancestor.txid)
-                assert False
-            remove_from_pool(ancestor)
-            block.append(txs.pop(ancestor.txid).txid)
-            block_weight += ancestor.weight
-        # Resort list
-        txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
-        continue  # Break into outer loop. Restart iteration over all unincluded txs
-
+    for tx in transactions:
+        tx.feerate = float(tx.fee) / float(tx.weight)
+    # Construct dictionary for fast lookup
+    txs = OrderedDict(sorted([(tx.txid, tx) for tx in transactions], key=lambda x: x[1].feerate, reverse=True))
+    while True:
+        for tx in txs.values():
+            # Opportunistically include txs with unconfirmed parents if their parents
+            if any([parent_tx in txs for parent_tx in tx.parents]):
+                continue
+            if block_weight + tx.weight > MAX_BLOCK_WEIGHT:
+                # Transaction won't fit in block
+                continue
+            block.append(txs.pop(tx.txid).txid)
+            block_weight += tx.weight
+            break
+        else:
+            # Couldn't add any more transactions.
+            break
     return block`,
   ],
   validate: async (answer) => {
@@ -172,7 +164,7 @@ export default function MempoolTransactionResources({ lang }) {
               <div className="relative grow bg-[#00000026] font-mono text-sm text-white">
                 <MonacoEditor
                   loading={<Loader className="h-10 w-10 text-white" />}
-                  height={`1260px`}
+                  height={`635px`}
                   value={code}
                   beforeMount={handleBeforeMount}
                   onMount={handleMount}
