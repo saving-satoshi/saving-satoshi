1818d6f71c9c2aabe9ec4dc30bca998b67fefd5e
Ben Allen <108441023+benalleng@users.noreply.github.com>
2024-03-12 11:22:07 -0400

    refactor: remove introduction dir (#883)

--- ui/introduction/Title.tsx
+++
@@ -1,3 +0,0 @@
-export default function Title({ children }) {
-  return <span>{children}</span>
-}
--- ui/introduction/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Introduction } from './Introduction'
-export { default as IntroductionTitle } from './Title'
-export { default as IntroductionSubtitle } from './Subtitle'
-export { default as IntroductionDescription } from './Description'
