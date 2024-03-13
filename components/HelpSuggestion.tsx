248dde1748b36fdb746e422b42867c9db5bf1ebd
daviroo <david.borthwick@gmail.com>
2024-03-13 08:00:38 +0000

    chore: Added text to translation

--- components/HelpSuggestion.tsx
+++ components/HelpSuggestion.tsx
@@ -1,15 +1,17 @@
+import { useLang, useTranslations } from 'hooks'
 import React from 'react'
 import { Text } from 'ui'
 
 const HelpSuggestion = () => {
+  const lang = useLang()
+  const t = useTranslations(lang)
   return (
     <div className="mt-4 flex items-center rounded-xl bg-black bg-opacity-20 p-4 bg-blend-darken">
       <div className="mr-4 flex h-12 w-12 flex-shrink-0 items-center justify-center border border-white">
         <Text className="text-lg md:text-xl">?</Text>
       </div>
       <Text className="md:text-md flex-grow text-sm">
-        If you&apos;re ever stuck, press the &ldquo;?&ldquo; button in the
-        top-right corner. It provides helpful resources and tips.
+        {t('help_page.help_suggestion')}
       </Text>
     </div>
   )
