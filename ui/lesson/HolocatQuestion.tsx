039c707370b45a6cf29aceebbebabc67ab16eeb9
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-03-17 20:29:20 +0100

    fix: rebased validate-signature-2

--- ui/lesson/HolocatQuestion.tsx
+++ ui/lesson/HolocatQuestion.tsx
@@ -34,16 +34,16 @@ const HolocatQuestion = ({
     )
   } else {
     return (
-      <div className={`${width ? 'max-w-[100px]' : width}`}>
+      <div className={`${width ? 'max-w-[100px]' : width} mx-1.5 inline-block`}>
         <Tooltip
           offset={10}
           theme={theme || ''}
           position="top"
           id={id}
-          className="flex cursor-pointer  flex-wrap items-center gap-1 text-lg italic"
+          className="flex cursor-pointer flex-wrap items-center  gap-1 text-lg italic no-underline hover:underline"
           content={<p>{question}</p>}
         >
-          <p>{children}</p>{' '}
+          <p>{children}</p>
           <Avatar avatar="/assets/avatars/holocat.jpg" size={32} />
         </Tooltip>
       </div>
