2219d44e9c7296b6a7c0877439d755af9f22034a
Ben Allen <108441023+benalleng@users.noreply.github.com>
2024-04-08 10:12:29 -0400

    Refactor validate signature 3 and 4 for more interaction (#893)
    * feat: refactor validate signature 3 and 4
    
    * fix: hidden range
    
    * feat: add resources for validate signature 3 and 4
    
    * fix: copy
    
    * fix: copy attempt 2
    
    * Apply suggestions from code review
    
    Co-authored-by: daviroo <david.borthwick@gmail.com>
    
    ---------
    
    Co-authored-by: daviroo <david.borthwick@gmail.com>

--- content/lessons/chapter-5/validate-signature-4.tsx
+++ content/lessons/chapter-5/validate-signature-4.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { useMediaQuery, useTranslations } from 'hooks'
+import { useTranslations } from 'hooks'
 import { getLessonKey } from 'lib/progress'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { EditorConfig } from 'types'
-import { readOnlyOptions } from 'ui/lesson/ScriptingChallenge/config'
 import { CodeExample, LessonInfo, ScriptingChallenge, Text, Title } from 'ui'
 import { useDataContext } from 'contexts/DataContext'
 import { getLanguageString } from 'lib/SavedCode'
@@ -15,13 +14,19 @@ export const metadata = {
 }
 
 const javascript = {
-  program: `console.log(verify(sig_r_fe, sig_s_fe, key_ge, msg_fe));
+  program: `console.log(verify_keys(keys).toString());
 console.log("KILL")
 `,
   defaultFunction: {
     name: 'verify',
     args: [],
   },
+  rangesToCollapse: [
+    {
+      start: 70,
+      end: 72,
+    },
+  ],
   defaultCode: `const secp256k1 = require('@savingsatoshi/secp256k1js')
 // View the library source code
 // https://github.com/saving-satoshi/challenges/blob/master/chapter4/javascript/lib/secp256k1.js
@@ -36,8 +41,16 @@ const msg_fe = 0x73a16290e005b119b9ce0ceea52949f0bd4f925e808b5a54c631702d3fea124
 const sig_r_fe = 0x8bd06d50f4a4b2bba64ccfb68f011e8babcec06b1cc07741fe686159abef8d69n
 const sig_s_fe = 0x3f0754da6e85699666c61e12707c45a037a5142f6a1b43e7014979a8c16d87c9n
 
-// Public key values from step 7:
-const key_ge = new GE(new FE("0x9d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee0663063"),new FE("0x6b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6"))
+// Public key values
+// Remember they need to be of the format new GE(new FE(x.hex()), new FE(y.hex())) to be read by verify()
+const keys = [
+  "04bbb554daf8811b95c8af5272fa8b4e2d6335bf19fff24d3187b8781497299aa4d27c900c367e4e506d671a4ea3aa50843f182a090d701f3bc8e6578d2455d81e",
+  "04cc679cd88b28444049aa9db8f88864ace38f79ba6310d0d3f027c9462a9f420befaaf888ce372cbf6f0ece99e5ada86436c960c1c0840a588ea7dbd78187445d",
+  "049d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee06630636b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6",
+  "0461bfb73040740c12f57146b3a7f2ccfd75b6cd2a0d5df7a789cfaeb77bda4dcd222df570946cb6de62d6b1a939f55da85859f575e84ba86c67c4aa97d85ba516",
+  "042a87d97397b2c43dff63670e38e78db159daa0e1070ec42181d0ed44a7d1aa508d42bd9759659c4a3194dea56da71325fb25acda6ee931cd8b93172b5d0f3c8f",
+  "04d1cdabaea3be5d8161b93b7e20b0375cefee0a36259d654185555deff881406a421384e927328e2dcb5ed87103365ef3007bd31e12591e5d1c56c83516db26ec"
+]
 
 function verify(sig_r, sig_s, key, msg) {
   // Verify an ECDSA signature given a public key and a message.
@@ -82,14 +95,32 @@ function verify(sig_r, sig_s, key, msg) {
   const R = (secp256k1.G.mul(u1)).add(key.mul(u2));
   return R.x.equals(new FE(sig_r));
 }
+
+function verify_keys(keys) {
+  // YOUR CODE HERE
+}
 `,
   validate: async (answer) => {
-    // NO VALIDATION REQUIRED - THIS ISN'T EDITABLE BY THE USER
-    return [true, 'The signature is invalid']
+    if (
+      answer ===
+      '049d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee06630636b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6'
+    ) {
+      return [
+        true,
+        'The signature is valid for this public key Vanderpoole used this public key, this was not satoshi!',
+      ]
+    }
+    if (answer === 'true') {
+      return [
+        false,
+        'Make sure you are returning the public key Vanderpoole used and not a boolean value',
+      ]
+    }
+    return [false, 'That is not quite right, try again.']
   },
   constraints: [
     {
-      range: [1, 1, 1, 1],
+      range: [1, 1, 73, 1],
       allowMultiline: true,
     },
   ],
@@ -97,13 +128,19 @@ function verify(sig_r, sig_s, key, msg) {
 
 const python = {
   program: `
-print(verify(sig_r, sig_s, key_ge, msg));
+print(verify_keys(keys));
 print("KILL")
 `,
   defaultFunction: {
     name: 'verify',
     args: [],
   },
+  rangesToCollapse: [
+    {
+      start: 49,
+      end: 51,
+    },
+  ],
   defaultCode: `import secp256k1py.secp256k1 as SECP256K1
 # View the library source code
 # https://github.com/saving-satoshi/challenges/blob/master/chapter4/python/lib/secp256k1.py
@@ -117,41 +154,65 @@ msg = 0x73a16290e005b119b9ce0ceea52949f0bd4f925e808b5a54c631702d3fea1242
 sig_r = 0x8bd06d50f4a4b2bba64ccfb68f011e8babcec06b1cc07741fe686159abef8d69
 sig_s = 0x3f0754da6e85699666c61e12707c45a037a5142f6a1b43e7014979a8c16d87c9
 
-# Public key values from step 7:
-key_ge = GE(0x9d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee0663063, 0x6b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6)
+# Public key values
+# Remember they need to be of the format new GE(new FE(x.hex()), new FE(y.hex())) to be read by verify()
+keys = [
+    "04bbb554daf8811b95c8af5272fa8b4e2d6335bf19fff24d3187b8781497299aa4d27c900c367e4e506d671a4ea3aa50843f182a090d701f3bc8e6578d2455d81e",
+    "04cc679cd88b28444049aa9db8f88864ace38f79ba6310d0d3f027c9462a9f420befaaf888ce372cbf6f0ece99e5ada86436c960c1c0840a588ea7dbd78187445d",
+    "049d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee06630636b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6",
+    "0461bfb73040740c12f57146b3a7f2ccfd75b6cd2a0d5df7a789cfaeb77bda4dcd222df570946cb6de62d6b1a939f55da85859f575e84ba86c67c4aa97d85ba516",
+    "042a87d97397b2c43dff63670e38e78db159daa0e1070ec42181d0ed44a7d1aa508d42bd9759659c4a3194dea56da71325fb25acda6ee931cd8b93172b5d0f3c8f",
+    "04d1cdabaea3be5d8161b93b7e20b0375cefee0a36259d654185555deff881406a421384e927328e2dcb5ed87103365ef3007bd31e12591e5d1c56c83516db26ec"
+]
 
-# The verify function from step 8:
 def verify(r, s, key, msg):
-  if r == 0 or r >= GE.ORDER:
-    print("FALSE - invalid r value")
-    return false
+    if r == 0 or r >= GE.ORDER:
+        print("FALSE - invalid r value")
+        return false
 
-  if s == 0 or s >= GE.ORDER:
-    print("FALSE - invalid s value")
-    return false
-  # Calculate the inverse of sig_s modulo ORDER
-  sig_s_inverted = pow(sig_s, -1, GE.ORDER)
+    if s == 0 or s >= GE.ORDER:
+        print("FALSE - invalid s value")
+        return false
+    # Calculate the inverse of sig_s modulo ORDER
+    sig_s_inverted = pow(sig_s, -1, GE.ORDER)
 
-  # Calculate u1 and u2
-  u1 = (msg * sig_s_inverted) % GE.ORDER
-  u2 = (sig_r * sig_s_inverted) % GE.ORDER
+    # Calculate u1 and u2
+    u1 = (msg * sig_s_inverted) % GE.ORDER
+    u2 = (sig_r * sig_s_inverted) % GE.ORDER
 
-  # Calculate R = u1 * G + u2 * public key
-  # We need to use the appropriate methods for point multiplication and addition
-  R1 = GE.mul((u1, G))
-  R2 = GE.mul((u2, key))
-  R = R1 + R2
+    # Calculate R = u1 * G + u2 * public key
+    # We need to use the appropriate methods for point multiplication and addition
+    R1 = GE.mul((u1, G))
+    R2 = GE.mul((u2, key))
+    R = R1 + R2
 
-  # Verify if the x-coordinate of R modulo ORDER is equal to sig_r
-  return sig_r == int(R.x) % GE.ORDER
+    # Verify if the x-coordinate of R modulo ORDER is equal to sig_r
+    return sig_r == int(R.x) % GE.ORDER
+
+def verify_keys(keys):
+    # YOUR CODE HERE
 `,
   validate: async (answer) => {
-    // NO VALIDATION REQUIRED - THIS ISN'T EDITABLE BY THE USER
-    return [true, undefined]
+    if (
+      answer ===
+      '049d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee06630636b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6'
+    ) {
+      return [
+        true,
+        'The signature is valid for this public key Vanderpoole used this public key, this was not satoshi!',
+      ]
+    }
+    if (answer === 'True') {
+      return [
+        false,
+        'Make sure you are returning the public key Vanderpoole used and not a boolean value',
+      ]
+    }
+    return [false, 'That is not quite right, try again.']
   },
   constraints: [
     {
-      range: [1, 1, 1, 1],
+      range: [1, 1, 51, 1],
       allowMultiline: true,
     },
   ],
@@ -168,22 +229,11 @@ const config: EditorConfig = {
 export default function ValidateSignature4({ lang }) {
   const t = useTranslations(lang)
   const { currentLanguage } = useDataContext()
-  const [objectPosition, setObjectPosition] = useState<string | undefined>()
-  const isMediumScreen = useMediaQuery({ width: 1024 })
   const [language, setLanguage] = useState(getLanguageString(currentLanguage))
-  const [isLoading, setIsLoading] = useState(true)
 
   const handleSelectLanguage = (language: string) => {
     setLanguage(language)
   }
-
-  useEffect(() => {
-    if (isMediumScreen) {
-      setObjectPosition('object-left')
-    } else {
-      setObjectPosition('object-bottom')
-    }
-  }, [isMediumScreen])
 
   return (
     <ScriptingChallenge
@@ -192,26 +242,15 @@ export default function ValidateSignature4({ lang }) {
       lessonKey={getLessonKey('chapter-5', 'validate-signature-4')}
       successMessage={t('chapter_five.validate_signature_four.success')}
       onSelectLanguage={handleSelectLanguage}
-      editorOptions={readOnlyOptions}
     >
       <LessonInfo>
         <Title>{t('chapter_five.validate_signature_four.heading')}</Title>
         <Text className="mt-4 font-nunito text-xl text-white">
           {t(`chapter_five.validate_signature_four.paragraph_one`)}
         </Text>
-        <CodeExample
-          className="mt-4"
-          code={`mvG1WcMQV4cFq1aEN6uijg8FSCfcxtMEqS`}
-          language="shell"
-        />
         <Text className="mt-4 font-nunito text-xl text-white">
           {t(`chapter_five.validate_signature_four.paragraph_two`)}
         </Text>
-        <CodeExample
-          className="mt-4"
-          code={`049d57ded01d3a7652a957cf86fd4c3d2a76e76e83d3c965e1dca45f1ee06630636b8bcbc3df3fbc9669efa2ccd5d7fa5a89fe1c0045684189f01ea915b8a746a6`}
-          language="shell"
-        />
         <Text className="mt-4 font-nunito text-xl text-white">
           {t(`chapter_five.validate_signature_four.paragraph_three`)}
         </Text>
