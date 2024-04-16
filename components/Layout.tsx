c35365dd7a7556e4d5069798f4c3592df0fb15b7
daviroo <david.borthwick@gmail.com>
2024-04-16 16:31:18 +0100

    chore: Now init features when app loads

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ import SignUpModal from 'components/Modals/SignUp'
 import { useAtom } from 'jotai'
 import { useEffect } from 'react'
 import { useAuthFunctions } from 'state/AuthFunctions'
+import { useFeatureFunctions } from 'state/FeatureFunctions'
 import { useModalFunctions } from 'state/ModalFunctions'
 import { Modal, modalsAtom } from 'state/state'
 
@@ -13,10 +14,12 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
   const [modals] = useAtom(modalsAtom)
   const { close } = useModalFunctions()
   const { check } = useAuthFunctions()
+  const { init: initFeatures } = useFeatureFunctions()
 
   // Check if the user is authenticated
   useEffect(() => {
     check()
+    initFeatures()
   }, [])
 
   return (
