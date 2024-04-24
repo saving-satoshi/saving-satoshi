46636efbe3cacf4f2d0f223f2173c5b82919fef0
benalleng <benalleng@gmail.com>
2024-04-24 13:45:02 -0400

    js

--- content/lessons/chapter-8/building-blocks-3.tsx
+++ content/lessons/chapter-8/building-blocks-3.tsx
@@ -17,57 +17,31 @@ export default function BuildingBlocks3({ lang }) {
 
   const javascript = {
     program: `//BEGIN VALIDATION BLOCK
-const addr_pfajurn = 'bc1qgghq08syehkym52ueu9nl5x8gth23vr8hurv9dyfcmhaqk4lrlgs28epwj';
-const value_uzglwbxj = 100000000;
-const output_otcqjgpd = Output.from_options(addr_pfajurn, value_uzglwbxj);
-const isCorrect_uflkrpot = output_otcqjgpd.serialize().toString('hex') === '00e1f50500000000220020422e079e04cdec4dd15ccf0b3fd0c742eea8b067bf06c2b489c6efd05abf1fd1'
-console.log(isCorrect_uflkrpot && 'true')
-;
 console.log("KILL")`,
     defaultFunction: {
       name: 'privateKeyToPublicKey',
       args: ['privateKey'],
     },
-    defaultCode: `const assert = require('assert');
-const bech32 = require('@savingsatoshi/bech32js');
-// Use the bech32 library to find the version and data components from the address
-// See the library source code for the exact definition
-// https://github.com/saving-satoshi/bech32js/blob/main/bech32.js
+    defaultCode: `const Bitcoin = require('@0tuedon/bitcoin_rpcjs')
+// https://github.com/saving-satoshi/bitcoin_rpcjs/blob/master/bitcoin_rpc.js
 
-class Output {
-  constructor() {
-    this.value = 0;
-    this.witness_version = 0;
-    this.witness_data = Buffer.alloc(0);
-  }
-
-  static from_options(addr, value) {
-    assert(Number.isInteger(value));
-    const self = new this();
-    // YOUR CODE HERE
-    return self;
-  }
-
-  serialize() {
-    // YOUR CODE HERE
-  }
-}
+console.log(Bitcoin.rpc())
 `,
     validate: async (answer: string) => {
       if (answer) {
-        if (answer === 'true') {
+        if (answer === '3007592928481984.23') {
           return [true, '']
         } else {
-          return [false, 'recheck your methods']
+          return [null, '']
         }
       } else {
-        return [false, "can't find a return in both of the methods"]
+        return [null, '']
       }
     },
   }
 
   const python = {
-    program: `
+    program: `# BEGIN VALIDATION BLOCK
 print("KILL")`,
     defaultFunction: {
       name: 'privatekey_to_publickey',
