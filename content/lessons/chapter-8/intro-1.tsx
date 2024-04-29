ecdfcc8a69d2e327c1e8c2544162b7e4ff988489
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-04-29 17:41:27 +0100

    Chapter 8 Lesson 2 (#919)
    * chore: copy and setup for lesson 2
    
    * fix: validation for chapter 8
    
    * Apply suggestions from code review
    
    use height as param
    
    ---------
    
    Co-authored-by: Ben Allen <108441023+benalleng@users.noreply.github.com>

--- content/lessons/chapter-8/intro-1.tsx
+++ content/lessons/chapter-8/intro-1.tsx
@@ -5,6 +5,7 @@ import { Introduction, Text } from 'ui'
 
 export const metadata = {
   title: 'chapter_eight.intro_one.title',
+  navigation_title: 'chapter_eight.intro_one.nav_title',
   image: '/assets/images/chapter-8-intro-1.jpg',
   key: 'CH8INT1',
 }
--- content/lessons/chapter-8/intro-3.tsx
+++ content/lessons/chapter-8/intro-3.tsx
@@ -5,6 +5,7 @@ import { Introduction, Text } from 'ui'
 
 export const metadata = {
   title: 'chapter_eight.intro_one.title',
+  navigation_title: 'chapter_eight.intro_three.nav_title',
   image: '/assets/images/chapter-8-intro-3.jpg',
   key: 'CH8INT3',
 }
