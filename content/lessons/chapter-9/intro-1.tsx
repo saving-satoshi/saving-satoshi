ee04587ac62a3b9af22add8d958d7ceb620cc41e
benalleng <benalleng@gmail.com>
2024-06-24 19:40:21 -0400

    feat: add chapter 9 intro pages

--- content/lessons/chapter-9/intro-1.tsx
+++ content/lessons/chapter-9/intro-1.tsx
@@ -4,9 +4,10 @@ import { useTranslations } from 'hooks'
 import { Introduction, Text } from 'ui'
 
 export const metadata = {
-  title: 'chapter_eight.intro_one.title',
-  navigation_title: 'chapter_eight.intro_one.nav_title',
-  image: '/assets/images/chapter-8-intro-1.jpg',
+  title: 'chapter_nine.intro_one.title',
+  navigation_title: 'chapter_nine.intro_one.nav_title',
+  image: '/assets/images/chapter-9-intro-1.jpg',
+  theme: 'bg-[#401d4e]',
   key: 'CH9INT1',
 }
 
@@ -16,10 +17,13 @@ export default function Intro1({ lang }) {
   return (
     <Introduction lang={lang} imagePosition="object-center">
       <Text className="text-lg md:text-xl">
-        {t('chapter_eight.intro_one.paragraph_one')}
+        {t('chapter_nine.intro_one.paragraph_one')}
       </Text>
-      <Text className="mt-2 text-lg md:text-xl">
-        {t('chapter_eight.intro_one.paragraph_two')}
+      <Text className="mt-4 text-lg md:text-xl">
+        {t('chapter_nine.intro_one.paragraph_two')}
+      </Text>
+      <Text className="mt-4 text-lg md:text-xl">
+        {t('chapter_nine.intro_one.paragraph_three')}
       </Text>
     </Introduction>
   )
