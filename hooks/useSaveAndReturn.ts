6d2de447eebc6214bb0f0952a7ba60c6a92df87f
benalleng <benalleng@gmail.com>
2024-07-25 17:13:34 -0400

    fix: saveAndReturn now works on both creating an account and on an existing account

--- hooks/useSaveAndReturn.ts
+++ hooks/useSaveAndReturn.ts
@@ -1,17 +1,30 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useLocalizedRoutes } from 'hooks'
+import { useLocalizedRoutes, usePathData } from 'hooks'
 import { useAtomValue } from 'jotai'
-import { currentChapterAtom } from 'state/progressState'
+import {
+  currentChapterAtom,
+  syncedCourseProgressAtom,
+} from 'state/progressState'
 
 export default function useSaveAndReturn() {
   const { chaptersUrl } = useLocalizedRoutes()
   const router = useRouter()
-  const chapterId = useAtomValue(currentChapterAtom)
+  const { chapterId } = usePathData()
+  const chapterIdAtom = useAtomValue(currentChapterAtom)
+  const courseProgress = useAtomValue(syncedCourseProgressAtom)
 
   const saveAndReturn = async () => {
-    router.push(`${chaptersUrl}#chapter-${chapterId + 1}`)
+    //needs to have different behavior from logging in vs a continue button as the progress is updated at different points of the render
+    router.push(
+      `${chaptersUrl}#chapter-${
+        courseProgress.chapters.find((chapter) => !chapter.completed)?.id !==
+        Number(chapterId?.substring(chapterId?.length - 1))
+          ? courseProgress.chapters.find((chapter) => !chapter.completed)?.id
+          : chapterIdAtom + 1
+      }`
+    )
   }
 
   return saveAndReturn
