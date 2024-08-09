cfa648e3a8523ce914c243e003c52536783f80f2
benalleng <benalleng@gmail.com>
2024-08-09 15:35:22 -0400

    revert: validation check

--- ui/lesson/OpCodeChallenge/OpRunner.tsx
+++ ui/lesson/OpCodeChallenge/OpRunner.tsx
@@ -6,7 +6,6 @@ import { StatusBar, useLessonContext } from 'ui'
 import { EditorRange, LessonView } from 'types'
 import {
   MainState,
-  State,
   OpRunnerTypes,
   StackType,
   T,
@@ -335,44 +334,28 @@ const OpRunner = ({
     setStartedTyping(true)
   }
 
-  const checkSuccessState = (
-    tokens: T,
-    state: MainState | State,
-    stack: StackType
-  ) => {
+  const checkSuccessState = (tokens: T, state, stack: StackType) => {
     const filterToStringArray = tokens.map((token) => token.value)
     const containsEveryScript = answerScript.every((token) =>
       filterToStringArray.includes(token)
     )
     const errorMessage = (error) => !!error.error?.message
 
-    const doesStackValidate = (
-      state: MainState | State,
-      stack: StackType
-    ): boolean => {
-      if (Array.isArray(state)) {
-        if (
-          stack?.length === 1 &&
-          !!state[0] &&
-          (state[state.length - 1]?.stack[0] === 1 ||
-            state[state.length - 1]?.stack[0] === true) &&
-          !state.some((s) => s.stack.some(errorMessage))
-        ) {
-          return true
-        }
-      } else {
-        if (
-          stack?.length === 1 &&
-          (state.stack[0] === 1 || state.stack[0] === true) &&
-          !state.stack.some(errorMessage)
-        ) {
-          return true
-        }
+    const doesStackValidate = () => {
+      if (
+        stack?.length === 1 &&
+        !!state &&
+        state[state.length - 1] &&
+        (state[state.length - 1]?.stack[0] == 1 ||
+          state[state.length - 1]?.stack[0] === true) &&
+        !state?.some(errorMessage)
+      ) {
+        return true
       }
       return false
     }
 
-    if (containsEveryScript && doesStackValidate(state, stack)) {
+    if (containsEveryScript && doesStackValidate()) {
       setSuccess(true)
     } else if (success !== true) {
       setSuccess(2)
