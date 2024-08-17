1b2608675954ddca45ad64b8d7f8438145875bab
Stacie Waleyko <1823216+satsie@users.noreply.github.com>
2024-08-16 21:15:39 -0400

    Chapter 4: fix secp256k1 lib 404 (#1052)

--- content/lessons/chapter-4/public-key-3.tsx
+++ content/lessons/chapter-4/public-key-3.tsx
@@ -39,7 +39,7 @@ console.log("KILL")`,
 // Return that curve point (also known as a group element)
 // which will be an instance of secp256k1.GE
 // See the library source code for the exact definition
-// https://github.com/saving-satoshi/challenges/blob/master/chapter4/javascript/lib/secp256k1.js
+// https://github.com/saving-satoshi/secp256k1js/blob/main/secp256k1.js
 const G = secp256k1.G
 
 function privateKeyToPublicKey(privateKey) {
@@ -85,7 +85,7 @@ print("KILL")`,
 # Return that curve point (also known as a group element)
 # which will be an instance of secp256k1.GE
 # See the library source code for the exact definition
-# https://github.com/saving-satoshi/challenges/blob/master/chapter4/python/lib/secp256k1.py
+# https://github.com/saving-satoshi/secp256k1py/blob/main/secp256k1py/secp256k1.py
 G = SECP256K1.FAST_G
 
 def privatekey_to_publickey(private_key):
