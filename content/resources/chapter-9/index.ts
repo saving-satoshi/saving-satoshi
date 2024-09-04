c217788cda483204b67c4ad5d259a1988f9c9913
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-09-04 22:46:34 +0100

    feat: setup different help modal on each challenge

---
+++ content/resources/chapter-9/index.ts
@@ -0,0 +1,23 @@
+import * as OpcodesFive from './opcodes-5'
+import * as OpcodesSix from './opcodes-6'
+import * as OpcodesSeven from './opcodes-7'
+import * as OpcodesEight from './opcodes-8'
+import * as OpcodesNine from './opcodes-9'
+import * as OpcodesTen from './opcodes-10'
+import * as ProposalTwo from './proposal-2'
+import * as ProposalThree from './proposal-3'
+import * as ProposalFour from './proposal-4'
+
+const Chapter9 = {
+  'opcodes-5': OpcodesFive,
+  'opcodes-6': OpcodesSix,
+  'opcodes-7': OpcodesSeven,
+  'opcodes-8': OpcodesEight,
+  'opcodes-9': OpcodesNine,
+  'opcodes-10': OpcodesTen,
+  'proposal-2': ProposalTwo,
+  'proposal-3': ProposalThree,
+  'proposal-4': ProposalFour,
+}
+
+export default Chapter9
---
+++ content/resources/chapter-9/proposal-3.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useTranslations } from 'hooks'
+import { useState } from 'react'
+import { Text, ResourcePage, ToggleSwitch } from 'ui'
+
+export default function ProposalThree({ lang }) {
+  const t = useTranslations(lang)
+  const [challengeIsToggled, setChallengeIsToggled] = useState(false)
+
+  const challengeToggleSwitch = () => {
+    setChallengeIsToggled(!challengeIsToggled)
+  }
+
+  return (
+    <ResourcePage
+      lang={lang}
+      readingResources={<></>}
+      codeResources={
+        <>
+          <Text>{t('help_page.solution_one')}</Text>
+          <div className="flex flex-row items-center gap-2">
+            <ToggleSwitch
+              checked={challengeIsToggled}
+              onChange={challengeToggleSwitch}
+            />
+            <Text>{t('help_page.spoilers_confirm')}</Text>
+          </div>
+        </>
+      }
+    />
+  )
+}
