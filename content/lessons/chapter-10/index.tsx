6f8ea327cc714e7c4090e82700c1f79bbda478d2
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-09-13 17:40:18 +0100

    Chap 10 scaffold (#1081)
    * feat: created all lessons and intro
    
    * feat: updated meta data on chapters folder

---
+++ content/lessons/chapter-10/index.tsx
@@ -0,0 +1,31 @@
+import * as Intro1 from './intro-1'
+import * as Intro2 from './intro-2'
+import * as MakingAPayment1 from './making-a-payment-1'
+import * as MakingAPayment2 from './making-a-payment-2'
+import * as MakingAPayment3 from './making-a-payment-3'
+import * as MakingAPayment4 from './making-a-payment-4'
+import * as MakingAPayment5 from './making-a-payment-5'
+import * as OpeningAChannel1 from './opening-a-channel-1'
+import * as OpeningAChannel2 from './opening-a-channel-2'
+import * as OpeningAChannel3 from './opening-a-channel-3'
+import * as OpeningAChannel4 from './opening-a-channel-4'
+import * as OpeningAChannel5 from './opening-a-channel-5'
+import * as Outro1 from './outro-1'
+
+const Chapter10 = {
+  'intro-1': Intro1,
+  'intro-2': Intro2,
+  'opening-a-channel-1': OpeningAChannel1,
+  'opening-a-channel-2': OpeningAChannel2,
+  'opening-a-channel-3': OpeningAChannel3,
+  'opening-a-channel-4': OpeningAChannel4,
+  'opening-a-channel-5': OpeningAChannel5,
+  'making-a-payment-1': MakingAPayment1,
+  'making-a-payment-2': MakingAPayment2,
+  'making-a-payment-3': MakingAPayment3,
+  'making-a-payment-4': MakingAPayment4,
+  'making-a-payment-5': MakingAPayment5,
+  'outro-1': Outro1,
+}
+
+export default Chapter10
