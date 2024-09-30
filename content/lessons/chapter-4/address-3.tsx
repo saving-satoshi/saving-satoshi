12ddc12b89b000b053294ee7346c2cf5c128d032
Ben Allen <108441023+benalleng@users.noreply.github.com>
2024-09-30 12:27:59 -0400

    fix: broken scripting challenge link comment (#1101)

--- content/lessons/chapter-4/address-3.tsx
+++ content/lessons/chapter-4/address-3.tsx
@@ -77,7 +77,7 @@ ${
 
 // Insert checksum and metadata, encode using bech32 and return a string
 // See the library source code for the exact API.
-// https://github.com/saving-satoshi/challenges/blob/master/chapter4/javascript/lib/bech32.js
+// https://github.com/saving-satoshi/bech32js/blob/main/bech32.js
 function hashToAddress(hash) {
 
 }
