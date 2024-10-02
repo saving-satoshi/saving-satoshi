d335ee96e8a9b5f95bdad0b06ebac5bc5bd373cb
benalleng <benalleng@gmail.com>
2024-10-02 15:27:49 -0400

    fix: className is optional

--- ui/common/Title.tsx
+++ ui/common/Title.tsx
@@ -1,4 +1,4 @@
-export default function Title({ className, children }) {
+export default function Title({ className = '', children }) {
   return (
     <h1 className={`font-nunito text-2xl font-bold text-white ${className}`}>
       {children}
