fb4264ba4dae698afe33ac40f040e362362a56c3
Anh La <37478019+Anh-La@users.noreply.github.com>
2025-04-21 09:37:07 +0930

    Update index.ts
    added vietnamese

--- i18n/locales/index.ts
+++ i18n/locales/index.ts
@@ -5,6 +5,7 @@ import hi from './hi'
 import de from './de'
 import pt from './pt'
 import fr from './fr'
+import vi from './'
 
 const locales = {
   en,
@@ -14,6 +15,7 @@ const locales = {
   de,
   pt,
   fr,
+  vi,
 }
 
 export default locales
