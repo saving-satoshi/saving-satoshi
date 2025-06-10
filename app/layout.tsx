c5c50e62c6cf8b1b9d244dc1f1928ec696e96f9f
Christoph Ono <chri@sto.ph>
2025-06-10 03:22:32 +0200

    Favicon update & cleanup (#1273)
    Redoing all the favicons in this one.
    
    - There are various outdated sizes that are not needed anymore in 2025 (per https://realfavicongenerator.net)
    - Match the larger icons to the new preview image Tuedon is adding
    - Make small icon sizes a simple "S" in your Audiowide font. More appropriate for small sizes where rendered images just look like a mess of pixels

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,89 +11,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head id="head">
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="Explore the mysteries of Satoshi and learn about bitcoin along the way."
         />
+        <meta name="apple-mobile-web-app-title" content="Saving Satoshi" />
         <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/apple-icon-57x57.png"
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
         />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-icon-152x152.png"
-        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-icon-180x180.png"
+          href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          sizes="16x16"
-          href="/favicon.ico"
-          type="image/x-icon"
-        />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="font-cbrush">
         <Fonts />
