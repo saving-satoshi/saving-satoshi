b79d8e0058efab66204b3a062d1c057762ba4dca
Shigeru Minami <218467216+shigeru-minami@users.noreply.github.com>
2025-06-30 13:47:26 +0900

    Translate about section to Japanese

--- i18n/locales/ja.ts
+++ i18n/locales/ja.ts
@@ -18,7 +18,14 @@ const translations = {
     poweroff: `章の選択へ戻る`,
     loading: `読み込み中`,
     bitcoin_dev_project: `Bitcoin開発プロジェクト`,
-  }
+  },
+  ///ABOUT PAGE
+about: {
+  title: `このプロジェクトについて`,
+  mobile_title: `Saving Satoshi（サトシを救え）とは`,
+  subtitle: `2139年。最後のビットコイン採掘まであと2週間。数か月にわたり、サトシ・スクエアではカウントダウンの時計が時を刻んでいる。`,
+  intro: `世界がこの歴史的な瞬間を待ちわびる中、ネットワークが突然停止する。<br><br>あなたの時計「Everything Watch」には、「サトシ・ナカモト」と名乗る誰かから、WhiskerWareブランドのホロキャットが届く。（他のeホログラムと似ているが、これは猫の形をしている。）あなたが鼻をちょんと押すと、ホロキャットが現れる。`,
 }
 
+  
 export default translations
