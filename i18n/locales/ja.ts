cbacdbe5a67ce51c5322190fc05c5b3951205968
Shigeru Minami <218467216+shigeru-minami@users.noreply.github.com>
2025-06-30 12:54:19 +0900

    Translate shared section to Japanese

--- i18n/locales/ja.ts
+++ i18n/locales/ja.ts
@@ -1,7 +1,24 @@
 const translations = {
-  hello_world: {
-    heading: 'こんにちは、世界！',
-  },
+  shared: {
+    next: `続ける`,
+    start: `開始`,
+    copy: `コピー`,
+    info: `情報`,
+    copy_acknowledged: `コピーしました！`,
+    about: `このプロジェクトについて`,
+    chapter: `章`,
+    chapters: `章一覧`,
+    challenge: `チャレンジ`,
+    challenges: `チャレンジ一覧`,
+    coming_soon: `近日公開`,
+    start_chapter: `章を始める`,
+    back: `戻る`,
+    end: `終了`,
+    close: `閉じる`,
+    poweroff: `章の選択へ戻る`,
+    loading: `読み込み中`,
+    bitcoin_dev_project: `Bitcoin開発プロジェクト`,
+  }
 }
 
 export default translations
