333dd8849210f003b55dfa9764f709aa6991a0d8
0tuedon <90271995+0tuedon@users.noreply.github.com>
2025-09-22 19:27:10 +0100

    fix: adds className props to accordion component

--- ui/common/Accordion.tsx
+++ ui/common/Accordion.tsx
@@ -1,23 +1,27 @@
 import clsx from 'clsx'
 import React, { useState } from 'react'
 import { AccordionIcon } from 'shared/icons'
-import ArrowLeftIcon from 'shared/icons/ArrowRight'
 
 const Accordion = ({
   title,
   defaultOpen,
   children,
+  className,
 }: {
   title: string
   defaultOpen: boolean
   children: React.ReactNode
+  className?: string
 }) => {
   const [open, setOpen] = useState(defaultOpen)
   const handleClick = () => {
     setOpen((prev) => !prev)
   }
   return (
-    <div onClick={handleClick} className={'bg-[#00000033] px-5'}>
+    <div
+      onClick={handleClick}
+      className={clsx('bg-[#00000033] px-5', className)}
+    >
       <div className="flex w-full cursor-pointer items-center justify-between py-3">
         <p className="font-nunito text-xl">{title}</p>
         <AccordionIcon className={'rotate-180'} />
