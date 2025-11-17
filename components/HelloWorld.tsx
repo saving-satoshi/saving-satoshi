b5f38d8e3b6cd217b8039e77a7d0771181758924
jerryzli99 <84209711+jerryzli99@users.noreply.github.com>
2025-11-17 12:15:42 -0800

    Implement HelloWorld component with translations

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -1,1 +1,9 @@
+// components/HelloWorld.tsx
+import { useTranslation, useLang } from 'react-i18next'
 
+export default function HelloWorldComponent() {
+  const lang = useLang()
+  const t = useTranslation(lang)
+
+  return <h1>{t('hello_world.heading')}</h1>
+}
