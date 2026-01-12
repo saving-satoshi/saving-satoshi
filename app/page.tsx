88bd13a01174390aa4b9dcf20601181fceddcd48
Stacie W <1823216+satsie@users.noreply.github.com>
2026-01-11 23:14:51 -0500

    Remove BOSS 2026 deadline banner (#1432)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from 'components/Hero'
 import Footer from 'components/Footer'
 import defaultMetadata from 'config/metadata'
 import Umami from 'components/Util/Tracking'
-import DeadlineBanner from 'components/DeadlineBanner'
 
 export const metadata = {
   ...defaultMetadata,
@@ -12,7 +11,7 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col">
-      <Umami /> <DeadlineBanner />
+      <Umami />
       <div className="bg-gradient-to-b from-[#00000040] to-transparent pb-28 pt-[70px]">
         <Topbar />
       </div>
